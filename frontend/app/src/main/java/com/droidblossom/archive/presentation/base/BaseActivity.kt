deb404859350d2325dba6d87b9eb39656b4635d2
hangunhee39 <ghee0831@naver.com>
2024-01-01 15:35:37 +0900

    feat : 디자인 시스템 & 초기 세팅

---
+++ frontend/app/src/main/java/com/droidblossom/archive/presentation/base/BaseActivity.kt
@@ -0,0 +1,35 @@
+package com.droidblossom.archive.presentation.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import kotlinx.coroutines.Job
+
+abstract class BaseActivity<VM: BaseViewModel, V: ViewDataBinding>(@LayoutRes val layoutResource :Int): AppCompatActivity() {
+
+    abstract val viewModel: VM
+    private lateinit var fetchJob: Job
+
+    private var _binding: V? = null
+    protected val binding : V get() = _binding!!
+
+    abstract fun observeData()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = DataBindingUtil.setContentView(this,layoutResource)
+        binding.lifecycleOwner=this
+        setContentView(binding.root)
+        fetchJob = viewModel.fetchData()
+        observeData()
+    }
+
+    override fun onDestroy() {
+        if(fetchJob.isActive){
+            fetchJob.cancel()
+        }
+        super.onDestroy()
+    }
+}
