473757d96c80c53b7ca6bdc5152e8d539c1d7cce
comst19 <comst19@naver.com>
2024-01-02 23:40:38 +0900

    feat: navigation 연결

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/base/BaseActivity.kt
@@ -0,0 +1,37 @@
+package com.droidblossom.archive.presentation.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import kotlinx.coroutines.Job
+
+abstract class BaseActivity<VM: BaseViewModel?, V: ViewDataBinding>(@LayoutRes val layoutResource :Int): AppCompatActivity() {
+
+    abstract val viewModel: VM?
+    private var fetchJob: Job? = null
+
+    private var _binding: V? = null
+    protected val binding: V get() = _binding!!
+
+    abstract fun observeData()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = DataBindingUtil.setContentView(this, layoutResource)
+        binding.lifecycleOwner = this
+        setContentView(binding.root)
+        fetchJob = viewModel?.fetchData()
+        observeData()
+    }
+
+    override fun onDestroy() {
+        fetchJob?.let {
+            if (it.isActive) {
+                it.cancel()
+            }
+        }
+        super.onDestroy()
+    }
+}
