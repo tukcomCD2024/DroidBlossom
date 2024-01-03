1108bf04e101b68d4441e21f6b355e7dfc60d8b8
GaBaljaintheroom <junsu1222@naver.com>
2024-01-03 23:06:42 +0900

    feat: 캡슐 스킨 엔티티 생성

---
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/capsuleskin/repository/CapsuleSkinRepository.java
@@ -0,0 +1,8 @@
+package site.timecapsulearchive.core.domain.capsuleskin.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import site.timecapsulearchive.core.domain.capsuleskin.entity.CapsuleSkin;
+
+public interface CapsuleSkinRepository extends JpaRepository<CapsuleSkin, Long> {
+
+}
