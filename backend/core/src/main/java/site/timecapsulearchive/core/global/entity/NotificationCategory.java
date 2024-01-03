09b22fc98ef482ecaaa53b0defae2c79d8c390cd
GaBaljaintheroom <junsu1222@naver.com>
2024-01-03 23:14:30 +0900

    feat: 알림 엔티티 생성

---
+++ backend/core/src/main/java/site/timecapsulearchive/core/global/entity/NotificationCategory.java
@@ -0,0 +1,36 @@
+package site.timecapsulearchive.core.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class NotificationCategory extends BaseEntity {
+
+    @Id
+    @Column(nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String categoryName;
+
+    @Column(nullable = false)
+    private String categoryDescription;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "nofitication_id", nullable = false)
+    private Notification notification;
+
+}
+
