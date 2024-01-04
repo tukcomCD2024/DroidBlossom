7445b5d0f645b9a4c000bcc59d762a40e8a5d216
seokho-1116 <seokho7799@gmail.com>
2024-01-04 12:59:54 +0900

    refactor : 패키지 구조 변경
    - secretcapsule -> capsule 패키지 하위로 이동
    - publiccapsule -> capsule 패키지 하위로 이동
    - groupcapsule -> capsule 패키지 하위로 이동

---
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/capsule/dto/secret_c/response/SecretCapsuleDetailResponse.java
@@ -0,0 +1,51 @@
+package site.timecapsulearchive.core.domain.capsule.dto.secret_c.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import java.time.ZonedDateTime;
+import java.util.List;
+import org.springframework.validation.annotation.Validated;
+
+@Schema(description = "비밀 캡슐 상세 정보")
+@Validated
+public record SecretCapsuleDetailResponse(
+    @JsonProperty("capsuleSkinUrl")
+    @Schema(description = "캡슐 스킨 url")
+    String capsuleSkinUrl,
+
+    @JsonProperty("dueDate")
+    @Schema(description = "개봉일")
+    ZonedDateTime dueDate,
+
+    @JsonProperty("nickname")
+    @Schema(description = "생성자 닉네임")
+    String nickname,
+
+    @JsonProperty("createdDate")
+    @Schema(description = "생성일")
+    ZonedDateTime createdDate,
+
+    @JsonProperty("address")
+    @Schema(description = "캡슐 생성 주소")
+    String address,
+
+    @JsonProperty("title")
+    @Schema(description = "제목")
+    String title,
+
+    @JsonProperty("content")
+    @Schema(description = "내용")
+    String content,
+
+    @JsonProperty("mediaUrls")
+    @Schema(description = "미디어 url들")
+    @Valid
+    List<String> mediaUrls,
+
+    @JsonProperty("isOpened")
+    @Schema(description = "개봉 여부")
+    Boolean isOpened
+) {
+
+}
---
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/capsule/entity/GroupCapsuleOpen.java
@@ -0,0 +1,36 @@
+package site.timecapsulearchive.core.domain.capsule.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import site.timecapsulearchive.core.domain.capsule.entity.Capsule;
+import site.timecapsulearchive.core.domain.member.entity.Member;
+import site.timecapsulearchive.core.global.entity.BaseEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "GROUP_CAPSULE_OPEN")
+public class GroupCapsuleOpen extends BaseEntity {
+    @Id
+    @Column(name = "group_capsule_open_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "capsule_id", nullable = false)
+    private Capsule capsule;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+}
