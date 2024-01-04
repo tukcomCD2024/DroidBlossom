e0278e44eb10e9a08f02b2cf9a0992801c264120
seokho-1116 <seokho7799@gmail.com>
2024-01-04 11:36:27 +0900

    refactor : 알림 관련 엔티티 위치 조정
    - notification 프로젝트 하위로 위치 조정
    - BaseEntity, JpaAuditingConfig 추가
    - Notification의 Member 연관관계를 삭제하고 memberId 키 참조로 변경

---
+++ backend/notification/src/main/java/site/timecapsulearchive/notification/entity/BaseEntity.java
@@ -0,0 +1,20 @@
+package site.timecapsulearchive.notification.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.ZonedDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@EntityListeners(value = {AuditingEntityListener.class})
+@MappedSuperclass
+public abstract class BaseEntity {
+    @CreatedDate
+    private ZonedDateTime createdAt;
+
+    @LastModifiedDate
+    private ZonedDateTime updatedAt;
+}
--- backend/notification/src/main/java/site/timecapsulearchive/notification/repository/NotificationRepository.java
+++ backend/notification/src/main/java/site/timecapsulearchive/notification/repository/NotificationRepository.java
@@ -1,7 +1,7 @@
-package site.timecapsulearchive.core.global.repository;
+package site.timecapsulearchive.notification.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import site.timecapsulearchive.core.global.entity.Notification;
+import site.timecapsulearchive.notification.entity.Notification;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
