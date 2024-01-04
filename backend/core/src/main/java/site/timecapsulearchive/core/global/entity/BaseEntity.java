ed4dce8933a04a713f7433761b27204033edecca
seokho-1116 <seokho7799@gmail.com>
2024-01-04 11:04:49 +0900

    refactor : 시간 타입 변경
    - LocalDateTime -> ZonedDateTime

--- backend/core/src/main/java/site/timecapsulearchive/core/global/entity/BaseEntity.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/global/entity/BaseEntity.java
@@ -2,7 +2,7 @@ package site.timecapsulearchive.core.global.entity;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -12,12 +12,9 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(value = {AuditingEntityListener.class})
 @MappedSuperclass
 public abstract class BaseEntity {
-
     @CreatedDate
-    private LocalDateTime createdAt;
+    private ZonedDateTime createdAt;
 
     @LastModifiedDate
-    private LocalDateTime updatedAt;
-
-
+    private ZonedDateTime updatedAt;
 }
