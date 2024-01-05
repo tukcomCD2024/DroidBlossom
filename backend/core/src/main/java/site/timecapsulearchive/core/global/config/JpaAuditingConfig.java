4c49e8a8f1415c95b2594bbd438a4a79641f7b6b
seokho-1116 <seokho7799@gmail.com>
2024-01-05 15:43:38 +0900

    fix : audit config 수정
    - ZonedDateTime으로 audit할 수 있도록 수정
    - 참고링크
     - https://github.com/spring-projects/spring-data-commons/issues/880

--- backend/core/src/main/java/site/timecapsulearchive/core/global/config/JpaAuditingConfig.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/global/config/JpaAuditingConfig.java
@@ -1,11 +1,19 @@
 package site.timecapsulearchive.core.global.config;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Optional;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing
+@EnableJpaAuditing(dateTimeProviderRef = "dateTimeProvider")
 public class JpaAuditingConfig {
-
+    @Bean
+    public DateTimeProvider dateTimeProvider() {
+        return () -> Optional.of(ZonedDateTime.now(ZoneId.of("UTC")));
+    }
 }
 
