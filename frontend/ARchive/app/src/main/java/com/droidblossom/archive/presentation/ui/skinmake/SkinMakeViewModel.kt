823a1e158f3455fad11d0f673016b5c66cf6806f
comst19 <comst19@naver.com>
2024-01-08 21:19:34 +0900

    feat: SkinMakeViewModel 구현

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/skinmake/SkinMakeViewModel.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/skinmake/SkinMakeViewModel.kt
@@ -1,6 +1,13 @@
 package com.droidblossom.archive.presentation.ui.skinmake
 
+import android.net.Uri
 import com.droidblossom.archive.presentation.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import javax.inject.Inject
 
-class SkinMakeViewModel : BaseViewModel() {
+@HiltViewModel
+class SkinMakeViewModel @Inject constructor(): BaseViewModel() {
+
+    val imgUri = MutableStateFlow<Uri?>(null)
 }
