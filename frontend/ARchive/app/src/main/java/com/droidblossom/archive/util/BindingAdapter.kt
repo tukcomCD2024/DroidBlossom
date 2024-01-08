2a833280a85cc2b856efe21827136b4624e10a90
comst19 <comst19@naver.com>
2024-01-08 20:03:40 +0900

    feat: BindingAdapter에 ImageView.setImage 기능 추가

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/util/BindingAdapter.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/util/BindingAdapter.kt
@@ -1,15 +1,24 @@
 package com.droidblossom.archive.util
 
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.telephony.PhoneNumberFormattingTextWatcher
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 
-@BindingAdapter("bind:imageUrl")
-fun ImageView.setImage(imageUrl: String?) {
+@BindingAdapter(value = ["bind:imageUrl", "bind:placeholder"], requireAll = false)
+fun ImageView.setImage(imageUrl: Uri?, placeholder: Drawable?) {
     Glide.with(this.context)
         .load(imageUrl)
         .transition(DrawableTransitionOptions.withCrossFade(300))
+        .apply {
+            if (placeholder != null) {
+                placeholder(placeholder)
+            }
+        }
         .into(this)
 }
