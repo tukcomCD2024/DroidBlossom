1f917406551bc2cf8cee4d5d69e611c3aea55655
comst19 <comst19@naver.com>
2024-01-08 21:12:24 +0900

    feat: 인증 번호 입력, 삭제 시 포커스 이전 로직

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/auth/CertificationFragment.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/auth/CertificationFragment.kt
@@ -58,9 +58,54 @@ class CertificationFragment : BaseFragment<AuthViewModel, FragmentCertificationB
                 viewModel.initTimer()
             }
 
+            setupAutoFocusOnLength(null, certificationNumberEditText1, certificationNumberEditText2)
+            setupAutoFocusOnLength(certificationNumberEditText1, certificationNumberEditText2, certificationNumberEditText3)
+            setupAutoFocusOnLength(certificationNumberEditText2, certificationNumberEditText3, certificationNumberEditText4)
+            setupAutoFocusOnLength(certificationNumberEditText3, certificationNumberEditText4, null)
         }
     }
 
-    override fun observeData() {}
+    override fun observeData() {
+
+        lifecycleScope.launch{
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.doneEvent.filter { it == AuthViewModel.AuthFlowEvent.CERTIFICATION_TO_SIGNUPSUCCESS }
+                    .collect { event ->
+                        if(navController.currentDestination?.id != R.id.signUpSuccessFragment) {
+                            navController.navigate(R.id.action_certificationFragment_to_signUpSuccessFragment)
+                        }
+                    }
+            }
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModel.certificationNumber
+                    .filter { it.length == 4 }
+                    .collect { certificationNum ->
+                        // 길이가 4일 때의 처리 로직
+                        viewModel.certificationToSignUpSuccess()
+                    }
+            }
+        }
+
+    }
+
+    private fun setupAutoFocusOnLength(previousEditText: EditText?, currentEditText: EditText, nextEditText: EditText?) {
+        currentEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (s != null) {
+                    if (s.length == 1) {
+                        nextEditText?.requestFocus()
+                    } else if (s.isEmpty()) {
+                        previousEditText?.requestFocus()
+                    }
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+    }
 
 }
