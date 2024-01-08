7a7fc1eb0ebf6bf6d7296dcd8439474f76b806cf
comst19 <comst19@naver.com>
2024-01-08 21:11:26 +0900

    feat: Phone Number Certification -> Sign-Up Success 로직 구현

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/auth/CertificationFragment.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/auth/CertificationFragment.kt
@@ -20,21 +20,46 @@ import com.droidblossom.archive.R
 import com.droidblossom.archive.databinding.FragmentCertificationBinding
 import com.droidblossom.archive.presentation.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class CertificationFragment : BaseFragment<AuthViewModel, FragmentCertificationBinding>(R.layout.fragment_certification) {
 
+
+    lateinit var navController: NavController
     override val viewModel : AuthViewModel by activityViewModels()
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.initTimer()
+        if (binding.certificationNumberEditText1.requestFocus()) {
+            val imm =
+                requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(binding.certificationNumberEditText1, InputMethodManager.SHOW_IMPLICIT)
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.vm = viewModel
+        navController = Navigation.findNavController(view)
 
+        viewModel.startTimer()
         initView()
     }
 
-    private fun initView(){}
+    private fun initView(){
+
+        with(binding){
+            resendBtn.setOnClickListener {
+                // 인증번호 재전송
+                viewModel.initTimer()
+            }
+
+        }
+    }
 
     override fun observeData() {}
 
