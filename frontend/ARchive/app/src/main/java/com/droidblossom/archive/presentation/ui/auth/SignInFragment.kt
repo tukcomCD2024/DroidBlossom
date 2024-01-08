314360b2f425c11017f7b23ba572335d602d3f36
comst19 <comst19@naver.com>
2024-01-08 21:03:32 +0900

    feat: Sign-In -> Sign-Up 로직 구현

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/auth/SignInFragment.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/auth/SignInFragment.kt
@@ -6,24 +6,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.droidblossom.archive.R
 import com.droidblossom.archive.databinding.FragmentSignInBinding
 import com.droidblossom.archive.presentation.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class SignInFragment : BaseFragment<AuthViewModel, FragmentSignInBinding>(R.layout.fragment_sign_in) {
+class SignInFragment : BaseFragment<AuthViewModel,FragmentSignInBinding>(R.layout.fragment_sign_in) {
+
+
+    lateinit var navController: NavController
 
     override val viewModel : AuthViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        navController = Navigation.findNavController(view)
+        binding.kakaoLoginBtn.setOnClickListener {
+            viewModel.signInToSignUp()
+        }
 
-        initView()
+        binding.googleLoginBtn.setOnClickListener {
+            viewModel.signInToSignUp()
+        }
     }
 
-    private fun initView(){}
+    override fun observeData() {
 
-    override fun observeData() {}
+        lifecycleScope.launch{
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.doneEvent.filter { it == AuthViewModel.AuthFlowEvent.SIGNIN_TO_SIGNUP }
+                    .collect { event ->
+                        if(navController.currentDestination?.id != R.id.signUpFragment) {
+                            navController.navigate(R.id.action_signInFragment_to_signUpFragment)
+                        }
+                    }
+            }
+        }
+
+    }
 }
