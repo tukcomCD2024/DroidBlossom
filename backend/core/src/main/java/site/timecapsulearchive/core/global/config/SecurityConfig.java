f88008119189b1a2115dccb6fe655ea36ca7def3
GaBaljaintheroom <junsu1222@naver.com>
2024-01-10 22:52:16 +0900

    feat : SecurityConfig에 oauth2Login 추가

--- backend/core/src/main/java/site/timecapsulearchive/core/global/config/SecurityConfig.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/global/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package site.timecapsulearchive.core.global.config;
 
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,10 +13,15 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import site.timecapsulearchive.core.domain.auth.service.CustomOAuth2UserService;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOauth2UserService;
+
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
@@ -35,7 +42,11 @@ public class SecurityConfig {
             )
             .authorizeHttpRequests(authz -> authz
                 .anyRequest().permitAll()
-            );
+            )
+            .oauth2Login(oauth2login ->
+                oauth2login.userInfoEndpoint(
+                    userInfoEndpointConfig -> userInfoEndpointConfig.userService(
+                        customOauth2UserService)));
 
         return http.build();
     }
