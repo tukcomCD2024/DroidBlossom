4599f92d53fcaa1f4eaaf5e1f9f0c35d582a944e
GaBaljaintheroom <junsu1222@naver.com>
2024-01-10 22:52:52 +0900

    feat : MemberRepository 메서드 추가

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/member/repository/MemberRepository.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/member/repository/MemberRepository.java
@@ -1,8 +1,14 @@
 package site.timecapsulearchive.core.domain.member.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import jakarta.validation.constraints.Email;
+import java.util.Optional;
+import org.springframework.data.repository.Repository;
+import site.timecapsulearchive.core.domain.auth.entity.SocialType;
 import site.timecapsulearchive.core.domain.member.entity.Member;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends Repository<Member, Long> {
 
+    Optional<Member> findBySocialTypeAndEmail(SocialType socialType, @Email String email);
+
+    Member save(Member createMember);
 }
