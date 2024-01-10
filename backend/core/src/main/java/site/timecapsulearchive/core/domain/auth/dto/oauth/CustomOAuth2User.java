1a9308f751654b172bfb02abf11806d2ea05c388
GaBaljaintheroom <junsu1222@naver.com>
2024-01-10 22:51:47 +0900

    feat : CustomOAuth2User 생성
    - 반환할 사용자 정보

---
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/auth/dto/oauth/CustomOAuth2User.java
@@ -0,0 +1,21 @@
+package site.timecapsulearchive.core.domain.auth.dto.oauth;
+
+import java.util.Collection;
+import java.util.Map;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import site.timecapsulearchive.core.domain.auth.entity.Role;
+
+public class CustomOAuth2User extends DefaultOAuth2User {
+
+    private String email;
+    private Role role;
+
+    public CustomOAuth2User(
+        Collection<? extends GrantedAuthority> authorities,
+        Map<String, Object> attributes, String nameAttributeKey, String email, Role role) {
+        super(authorities, attributes, nameAttributeKey);
+        this.email = email;
+        this.role = role;
+    }
+}
