91e8a07ad383d3bb798ab2bf1cb69403c85fde4a
hangunhee39 <ghee0831@naver.com>
2024-01-12 01:09:14 +0900

    feat : home VM 작업 / ARCH-124
    - FAB animation 추가
    - 캡슐 필터 데이터바인딩 작업
    - 데이터바인딩, VM 작업

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/HomeFragment.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/HomeFragment.kt
@@ -1,16 +1,39 @@
 package com.droidblossom.archive.presentation.ui.home
 
+import android.animation.ObjectAnimator
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.droidblossom.archive.R
 import com.droidblossom.archive.databinding.FragmentHomeBinding
 import com.droidblossom.archive.presentation.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.NonCancellable.start
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<HomeViewModelImpl, FragmentHomeBinding>(R.layout.fragment_home) {
 
-    override val viewModel: HomeViewModelImpl by viewModels()
+    override val viewModel: HomeViewModelImpl by viewModels<HomeViewModelImpl>()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.vm = viewModel
+
+    }
     override fun observeData() {
+        //FAB 상태
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModel.filterCapsuleSelect.collect(
+
+                )
+            }
+        }
     }
 }
--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/HomeViewModelImpl.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/HomeViewModelImpl.kt
@@ -1,7 +1,11 @@
 package com.droidblossom.archive.presentation.ui.home
 
+import androidx.lifecycle.viewModelScope
 import com.droidblossom.archive.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -9,5 +13,69 @@ class HomeViewModelImpl @Inject constructor(
 
 ) : BaseViewModel(), HomeViewModel {
 
+    private val _filterCapsuleSelect: MutableStateFlow<HomeViewModel.CapsuleFilter> =
+        MutableStateFlow(HomeViewModel.CapsuleFilter.ALL)
+    override val filterCapsuleSelect: StateFlow<HomeViewModel.CapsuleFilter>
+        get() = _filterCapsuleSelect
+
+    private val _isClickedFAB: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    override val isClickedFAB: StateFlow<Boolean>
+        get() = _isClickedFAB
+    private val _existsNotification: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    override val existsNotification: StateFlow<Boolean>
+        get() = _existsNotification
+    private val _followLocation: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    override val followLocation: StateFlow<Boolean>
+        get() = _followLocation
+
+
+    override fun selectPublic() {
+        viewModelScope.launch {
+            if (filterCapsuleSelect.value == HomeViewModel.CapsuleFilter.PUBLIC)
+                _filterCapsuleSelect.emit(HomeViewModel.CapsuleFilter.ALL)
+            else
+                _filterCapsuleSelect.emit(HomeViewModel.CapsuleFilter.PUBLIC)
+        }
+    }
+
+    override fun selectGroup() {
+        viewModelScope.launch {
+            if (filterCapsuleSelect.value == HomeViewModel.CapsuleFilter.GROUP)
+                _filterCapsuleSelect.emit(HomeViewModel.CapsuleFilter.ALL)
+            else
+                _filterCapsuleSelect.emit(HomeViewModel.CapsuleFilter.GROUP) }
+    }
+
+    override fun selectSecret() {
+        viewModelScope.launch {
+            if (filterCapsuleSelect.value == HomeViewModel.CapsuleFilter.SECRET)
+                _filterCapsuleSelect.emit(HomeViewModel.CapsuleFilter.ALL)
+            else
+                _filterCapsuleSelect.emit(HomeViewModel.CapsuleFilter.SECRET) }
+    }
+
+    override fun selectHotPlace() {
+        viewModelScope.launch {
+            if (filterCapsuleSelect.value == HomeViewModel.CapsuleFilter.HOT)
+                _filterCapsuleSelect.emit(HomeViewModel.CapsuleFilter.ALL)
+            else
+                _filterCapsuleSelect.emit(HomeViewModel.CapsuleFilter.HOT) }
+    }
+
+    override fun clickFollowBtn() {
+        viewModelScope.launch {
+            _followLocation.emit(!followLocation.value)
+        }
+    }
+
+    override fun clickNotificationBtn() {
+        viewModelScope.launch {
+            _existsNotification.emit(!existsNotification.value)
+        }
+    }
+
+    override fun clickFAB() {
+        viewModelScope.launch { _isClickedFAB.emit(!isClickedFAB.value) }
+    }
 
 }
