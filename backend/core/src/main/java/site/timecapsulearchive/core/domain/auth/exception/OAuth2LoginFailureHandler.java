ae241b2ecf59ace00936c22800f6263fe56ae657
GaBaljaintheroom <junsu1222@naver.com>
2024-01-12 13:01:03 +0900

    feat : Oauth 실패 핸들러 작성

---
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/auth/exception/OAuth2LoginFailureHandler.java
@@ -0,0 +1,29 @@
+package site.timecapsulearchive.core.domain.auth.exception;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OAuth2LoginFailureHandler implements AuthenticationFailureHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+        AuthenticationException exception) throws IOException {
+
+        //TODO: ErrorResponse로 변경 필요
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write("소셜 로그인 실패! 서버 로그를 확인해주세요.");
+        log.info("소셜 로그인에 실패했습니다. 에러 메시지 : {}", exception.getMessage());
+    }
+}
