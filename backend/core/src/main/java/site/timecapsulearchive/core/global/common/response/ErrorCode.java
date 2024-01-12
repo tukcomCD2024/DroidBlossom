1450c728406f3ff953c2a1e2c7f4f8e538c92fa9
GaBaljaintheroom <junsu1222@naver.com>
2024-01-12 18:36:50 +0900

    feat : 에러 코드 추가

--- backend/core/src/main/java/site/timecapsulearchive/core/global/common/response/ErrorCode.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/global/common/response/ErrorCode.java
@@ -6,8 +6,16 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    INVALID_TOKEN_EXCEPTION("A001", "jwt 토큰이 유효하지 않습니다."),
-    ALREADY_RE_ISSUED_TOKEN_EXCEPTION("A002", "이미 액세스 토큰 재발급에 사용된 리프레시 토큰입니다.");
+
+    //global
+    INTERNAL_SERVER_ERROR("G001", "서버에 오류가 발생하였습니다."),
+
+    //jwt
+    INVALID_TOKEN_EXCEPTION("J001", "jwt 토큰이 유효하지 않습니다."),
+    ALREADY_RE_ISSUED_TOKEN_EXCEPTION("J002", "이미 액세스 토큰 재발급에 사용된 리프레시 토큰입니다."),
+
+    //ouath
+    OAUTH2_NOT_AUTHENTICATED_EXCEPTION("O001", "OAuth2 인증에 실패하였습니다.");
 
     private final String code;
     private final String message;
