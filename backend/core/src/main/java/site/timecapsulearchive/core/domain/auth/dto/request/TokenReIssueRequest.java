ef1324f74b20654d411337a42e028214faca9b43
hong seokho <seokho7799@gmail.com>
2024-01-17 05:15:44 +0900

    fix : swagger 수정
    - swagger config 수정
    - Schema 오타 수정

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/auth/dto/request/TokenReIssueRequest.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/auth/dto/request/TokenReIssueRequest.java
@@ -3,7 +3,7 @@ package site.timecapsulearchive.core.domain.auth.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.validation.annotation.Validated;
 
-@Schema(description = "임시 인증 토큰")
+@Schema(description = "토큰 재발급 요청")
 @Validated
 public record TokenReIssueRequest(
 
