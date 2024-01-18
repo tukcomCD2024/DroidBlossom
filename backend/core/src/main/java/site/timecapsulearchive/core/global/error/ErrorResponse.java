16142c4f1d4eb43281d94597d7605e5549b37d09
GaBaljaintheroom <junsu1222@naver.com>
2024-01-18 16:05:40 +0900

    feat : 에러 응답 필드 형태 추가

--- backend/core/src/main/java/site/timecapsulearchive/core/global/error/ErrorResponse.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/global/error/ErrorResponse.java
@@ -3,10 +3,11 @@ package site.timecapsulearchive.core.global.error;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import org.springframework.validation.BindingResult;
 
 @Schema(description = "에러 발생 시 응답")
 public record ErrorResponse(
-
     @Schema(description = "에러 코드")
     String code,
 
@@ -14,18 +15,40 @@ public record ErrorResponse(
     String message,
 
     @Schema(description = "에러 리스트 ex) 필드 에러들")
-    List<Error> result
+    List<Error> errors
 ) {
 
-    public static ErrorResponse create(String code, String message) {
-        return new ErrorResponse(code, message, Collections.emptyList());
+    public static ErrorResponse create(final ErrorCode errorCode) {
+        return new ErrorResponse(
+            errorCode.getCode(),
+            errorCode.getMessage(),
+            Collections.emptyList()
+        );
     }
 
-    private record Error(
+    public static ErrorResponse create(final ErrorCode errorCode,
+        final BindingResult bindingResult) {
+        return new ErrorResponse(
+            errorCode.getCode(),
+            errorCode.getMessage(),
+            Error.from(bindingResult)
+        );
+    }
+
+    public record Error(
         String field,
         String value,
         String reason
     ) {
+
+        public static List<Error> from(final BindingResult bindingResult) {
+            return bindingResult.getFieldErrors().stream()
+                .map(error -> new Error(
+                    error.getField(),
+                    String.valueOf(error.getRejectedValue()),
+                    Objects.requireNonNullElse(error.getDefaultMessage(), "")
+                )).toList();
+        }
 
     }
 }
