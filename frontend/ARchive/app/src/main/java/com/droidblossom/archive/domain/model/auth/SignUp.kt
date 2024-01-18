fd2c4f98fe9c50a80dfec5e54207e1f82def814f
comst19 <comst19@naver.com>
2024-01-18 18:34:23 +0900

    SignUp model의 socialType 타입 변경

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/model/auth/SignUp.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/model/auth/SignUp.kt
@@ -1,17 +1,19 @@
 package com.droidblossom.archive.domain.model.auth
 
 import com.droidblossom.archive.data.dto.auth.request.SignUpRequestDto
+import com.droidblossom.archive.presentation.ui.auth.AuthViewModel
+import com.droidblossom.archive.util.SocialUtils
 
 data class SignUp(
     val authId: String,
     val email: String,
     val profileUrl: String,
-    val socialType: String
+    val socialType: AuthViewModel.Social
 ){
     fun toDto() = SignUpRequestDto(
         authId = this.authId,
         email = this.email,
         profileUrl = this.profileUrl,
-        socialType = this.socialType
+        socialType = SocialUtils.enumToString(this.socialType)
     )
 }
