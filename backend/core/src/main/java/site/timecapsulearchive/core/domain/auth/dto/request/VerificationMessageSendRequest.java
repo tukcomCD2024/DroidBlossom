761d16a379f2eec043f9b73eb4540273acbf8a16
GaBaljaintheroom <junsu1222@naver.com>
2024-01-18 16:26:50 +0900

    feat : 핸드폰 번호 regex 추가

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/auth/dto/request/VerificationMessageSendRequest.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/auth/dto/request/VerificationMessageSendRequest.java
@@ -1,13 +1,13 @@
 package site.timecapsulearchive.core.domain.auth.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import org.springframework.validation.annotation.Validated;
+import jakarta.validation.constraints.Pattern;
 
 @Schema(description = "인증 문자 요청")
-@Validated
 public record VerificationMessageSendRequest(
 
     @Schema(description = "핸드폰 번호")
+    @Pattern(regexp = "^01(?:0|1|[6-9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$", message = "10 ~ 11 자리의 숫자만 입력 가능합니다.")
     String phone
 ) {
 
