7f76518718a5311259ab811d84779cf56145a94d
hong seokho <seokho7799@gmail.com>
2024-01-19 11:53:12 +0900

    feat : request, response 추가
    - 문자 인증 request 추가
    - 문자 인증 response 추가
    Open #29

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/auth/dto/request/VerificationMessageSendRequest.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/auth/dto/request/VerificationMessageSendRequest.java
@@ -1,14 +1,19 @@
 package site.timecapsulearchive.core.domain.auth.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 @Schema(description = "인증 문자 요청")
 public record VerificationMessageSendRequest(
 
-    @Schema(description = "핸드폰 번호")
-    @Pattern(regexp = "^01(?:0|1|[6-9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$", message = "10 ~ 11 자리의 숫자만 입력 가능합니다.")
-    String phone
+    @Schema(description = "수신자 핸드폰 번호 ex)01012341234")
+    @Pattern(regexp = "^01(?:0|1|[6-9])?(\\d{3}|\\d{4})?(\\d{4})$", message = "숫자로만 구성된 전화번호 형식이여만합니다.")
+    String receiver,
+
+    @Schema(description = "앱의 해시 키")
+    @NotBlank(message = "앱의 해시 키는 필수입니다.")
+    String appHashKey
 ) {
 
 }
--- backend/core/src/main/java/site/timecapsulearchive/core/global/common/response/SuccessCode.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/global/common/response/SuccessCode.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum SuccessCode {
     //success handle
-    SUCCESS("00", "요청 처리에 성공했습니다.");
+    SUCCESS("00", "요청 처리에 성공했습니다."),
+    ACCEPTED("01", "요청이 수락되었습니다.");
 
     private final String code;
     private final String message;
