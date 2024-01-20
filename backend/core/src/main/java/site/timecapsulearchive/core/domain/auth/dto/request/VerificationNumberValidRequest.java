465f84dbeb9c9b0393fc7eee809de12e8fd36ce6
GaBaljaintheroom <junsu1222@naver.com>
2024-01-20 14:34:55 +0900

    refact : 필드 valid 추가

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/auth/dto/request/VerificationNumberValidRequest.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/auth/dto/request/VerificationNumberValidRequest.java
@@ -1,12 +1,14 @@
 package site.timecapsulearchive.core.domain.auth.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "인증 번호로 인증 요청")
-public record VerificationMessageValidRequest(
+public record VerificationNumberValidRequest(
 
     @Schema(description = "인증 번호")
-    Long number
+    @NotNull(message = "인증번호는 필수 입니다.")
+    Integer certificationNumber
 ) {
 
 }
