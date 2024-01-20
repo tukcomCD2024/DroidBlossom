79518ac76ce5f78e10db9b4ca2e3c8e586cd87e5
GaBaljaintheroom <junsu1222@naver.com>
2024-01-20 20:10:34 +0900

    refact : 전화번호 필드 추가

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/auth/dto/request/VerificationNumberValidRequest.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/auth/dto/request/VerificationNumberValidRequest.java
@@ -2,13 +2,18 @@ package site.timecapsulearchive.core.domain.auth.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+import site.timecapsulearchive.core.global.common.valid.annotation.Phone;
 
 @Schema(description = "인증 번호로 인증 요청")
 public record VerificationNumberValidRequest(
 
     @Schema(description = "인증 번호")
     @NotNull(message = "인증번호는 필수 입니다.")
-    Integer certificationNumber
+    String certificationNumber,
+
+    @Schema(description = "수신자 핸드폰 번호 ex)01012341234")
+    @Phone
+    String receiver
 ) {
 
 }
