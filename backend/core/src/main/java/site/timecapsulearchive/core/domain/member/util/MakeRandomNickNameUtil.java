f37f431f35310910edb9a159c7c0e7e58cd11071
GaBaljaintheroom <junsu1222@naver.com>
2024-01-20 22:24:12 +0900

    refact : 인덱스 랜덤 숫자 변경

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/member/util/MakeRandomNickNameUtil.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/member/util/MakeRandomNickNameUtil.java
@@ -1,13 +1,13 @@
 package site.timecapsulearchive.core.domain.member.util;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public final class MakeRandomNickNameUtil {
 
     private static final long DIVIDED_UNIT = 1000;
-    private static final int FIRST_VALUE = 0;
+    private static final int NUMBER_RANGE = 52;
 
     private MakeRandomNickNameUtil() {
     }
@@ -16,12 +16,14 @@ public final class MakeRandomNickNameUtil {
     public static String makeRandomNickName() {
         List<String> nick = new ArrayList<>(RandomNickName.NICK.getValue());
         List<String> name = new ArrayList<>(RandomNickName.NAME.getValue());
-        Collections.shuffle(nick);
-        Collections.shuffle(name);
 
         // 0 ~ 999
         String uniqueNumber = String.valueOf(System.currentTimeMillis() % DIVIDED_UNIT);
 
-        return nick.get(FIRST_VALUE) + name.get(FIRST_VALUE) + uniqueNumber;
+        return nick.get(randomNumber()) + name.get(randomNumber()) + uniqueNumber;
+    }
+
+    private static int randomNumber() {
+        return ThreadLocalRandom.current().nextInt(NUMBER_RANGE);
     }
 }
