ee5f4c81c5e68845fa7139f32feb5ae61a7cd412
hong seokho <seokho7799@gmail.com>
2024-01-21 14:14:31 +0900

    fix : mapper에 auth id 파라미터 추가

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/member/dto/mapper/MemberMapper.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/member/dto/mapper/MemberMapper.java
@@ -3,14 +3,19 @@ package site.timecapsulearchive.core.domain.member.dto.mapper;
 import org.springframework.stereotype.Component;
 import site.timecapsulearchive.core.domain.auth.dto.oauth.OAuth2UserInfo;
 import site.timecapsulearchive.core.domain.auth.dto.request.SignUpRequest;
-import site.timecapsulearchive.core.domain.auth.entity.SocialType;
+import site.timecapsulearchive.core.domain.member.entity.SocialType;
 import site.timecapsulearchive.core.domain.member.entity.Member;
 
 @Component
 public class MemberMapper {
 
-    public Member OAuthToEntity(SocialType socialType, OAuth2UserInfo oAuth2UserInfo) {
+    public Member OAuthToEntity(
+        String authId,
+        SocialType socialType,
+        OAuth2UserInfo oAuth2UserInfo
+    ) {
         return Member.builder()
+            .authId(authId)
             .socialType(socialType)
             .email(oAuth2UserInfo.getEmail())
             .profileUrl(oAuth2UserInfo.getImageUrl())
