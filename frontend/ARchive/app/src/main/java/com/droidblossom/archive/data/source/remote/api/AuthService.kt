d07a9f1cebcaaf43c732142e029b56287be8d323
comst19 <comst19@naver.com>
2024-01-23 19:27:27 +0900

    feat: AuthService에 postValidMessageApi추가

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/source/remote/api/AuthService.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/source/remote/api/AuthService.kt
@@ -2,18 +2,23 @@ package com.droidblossom.archive.data.source.remote.api
 
 import com.droidblossom.archive.data.dto.ResponseBody
 import com.droidblossom.archive.data.dto.auth.request.SignUpRequestDto
+import com.droidblossom.archive.data.dto.auth.request.VerificationNumberValidDto
 import com.droidblossom.archive.data.dto.auth.response.TemporaryTokenResponseDto
-import com.droidblossom.archive.domain.model.auth.TemporaryToken
-import com.droidblossom.archive.util.RetrofitResult
+import com.droidblossom.archive.data.dto.auth.response.TokenResponseDto
 import retrofit2.Response
 import retrofit2.http.Body
-import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface AuthService {
+
+    @POST("auth/verification/valid-message")
+    suspend fun postValidMessageApi(
+        @Body request : VerificationNumberValidDto
+    ) : Response<ResponseBody<TokenResponseDto>>
 
     @POST("auth/sign-up")
     suspend fun postSignUpApi(
         @Body request: SignUpRequestDto
     ) : Response<ResponseBody<TemporaryTokenResponseDto>>
+
 }
