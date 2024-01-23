6999a2628982a60b45a04f4ea53b3ffcee8aa0ce
GaBaljaintheroom <junsu1222@naver.com>
2024-01-24 02:16:03 +0900

    feat : S3Api 작성

---
+++ backend/core/src/main/java/site/timecapsulearchive/core/infra/s3/controller/S3Api.java
@@ -0,0 +1,37 @@
+package site.timecapsulearchive.core.infra.s3.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import site.timecapsulearchive.core.global.common.response.ApiSpec;
+import site.timecapsulearchive.core.infra.s3.dto.request.S3PreSignedUrlRequest;
+import site.timecapsulearchive.core.infra.s3.dto.response.S3PreSignedUrlResponse;
+
+public interface S3Api {
+
+    @Operation(
+        summary = "s3 업로드 미리 서명된 주소 요청",
+        description = "캡술 이미지/비디오 업로드할 수 있는 미리 서명된 주소를 응답한다.",
+        security = {@SecurityRequirement(name= "temporary_user_token")},
+        tags = {"s3"}
+    )
+    @ApiResponses(value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "s3업로드 미리 서명된 주소 반환"
+        )
+    })
+    @PostMapping(
+        value = "/presigned_url",
+        consumes = {"application/json"},
+        produces = {"application/json"}
+    )
+    ResponseEntity<ApiSpec<S3PreSignedUrlResponse>> getS3PreSignedUrl(
+        S3PreSignedUrlRequest request
+    );
+
+
+}
