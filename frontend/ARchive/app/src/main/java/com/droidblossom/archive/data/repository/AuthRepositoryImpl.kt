09669564e97e75c4576aa4e0b6961857960468a0
comst19 <comst19@naver.com>
2024-01-23 21:11:22 +0900

    feat: AuthRepositoryImpl, AuthRepository에 validMessageSend, validMessage, reIssue 구현

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/repository/AuthRepositoryImpl.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/repository/AuthRepositoryImpl.kt
@@ -3,11 +3,16 @@ package com.droidblossom.archive.data.repository
 import com.droidblossom.archive.data.dto.ResponseBody
 import com.droidblossom.archive.data.dto.auth.request.SignInRequestDto
 import com.droidblossom.archive.data.dto.auth.request.SignUpRequestDto
+import com.droidblossom.archive.data.dto.auth.request.TokenReIssueRequestDto
+import com.droidblossom.archive.data.dto.auth.request.VerificationMessageSendRequestDto
+import com.droidblossom.archive.data.dto.auth.request.VerificationNumberValidRequestDto
 import com.droidblossom.archive.data.dto.auth.response.TemporaryTokenResponseDto
 import com.droidblossom.archive.data.dto.auth.response.TokenResponseDto
+import com.droidblossom.archive.data.dto.auth.response.VerificationMessageResponseDto
 import com.droidblossom.archive.data.source.remote.api.AuthService
 import com.droidblossom.archive.domain.model.auth.TemporaryToken
 import com.droidblossom.archive.domain.model.auth.Token
+import com.droidblossom.archive.domain.model.auth.VerificationMessageResult
 import com.droidblossom.archive.domain.repository.AuthRepository
 import com.droidblossom.archive.util.RetrofitResult
 import com.droidblossom.archive.util.apiHandler
@@ -16,6 +21,18 @@ import javax.inject.Inject
 class AuthRepositoryImpl @Inject constructor(
     private val api: AuthService
 ) : AuthRepository {
+    override suspend fun validMessageSend(request: VerificationMessageSendRequestDto): RetrofitResult<VerificationMessageResult>{
+        return apiHandler({ api.postValidSendMessageApi(request) }) { response : ResponseBody<VerificationMessageResponseDto> -> response.result.toModel()}
+
+    }
+
+    override suspend fun validMessage(request: VerificationNumberValidRequestDto): RetrofitResult<Token> {
+        return apiHandler({ api.postValidMessageApi(request) }) { response : ResponseBody<TokenResponseDto> -> response.result.toModel()}
+    }
+
+    override suspend fun reIssue(request: TokenReIssueRequestDto): RetrofitResult<Token> {
+        return apiHandler({ api.postReIssueApi(request) }) { response : ResponseBody<TokenResponseDto> -> response.result.toModel()}
+    }
 
 
     override suspend fun authSignIn(request: SignInRequestDto) : RetrofitResult<Token> {
--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/repository/AuthRepository.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/repository/AuthRepository.kt
@@ -3,13 +3,17 @@ package com.droidblossom.archive.domain.repository
 import com.droidblossom.archive.data.dto.auth.request.SignInRequestDto
 import com.droidblossom.archive.data.dto.auth.request.SignUpRequestDto
 import com.droidblossom.archive.data.dto.auth.request.TokenReIssueRequestDto
+import com.droidblossom.archive.data.dto.auth.request.VerificationMessageSendRequestDto
+import com.droidblossom.archive.data.dto.auth.request.VerificationNumberValidRequestDto
 import com.droidblossom.archive.domain.model.auth.TemporaryToken
 import com.droidblossom.archive.domain.model.auth.Token
+import com.droidblossom.archive.domain.model.auth.VerificationMessageResult
 import com.droidblossom.archive.util.RetrofitResult
 
 interface AuthRepository {
 
-
+    suspend fun validMessageSend(request : VerificationMessageSendRequestDto) : RetrofitResult<VerificationMessageResult>
+    suspend fun validMessage(request : VerificationNumberValidRequestDto) : RetrofitResult<Token>
     suspend fun reIssue(request : TokenReIssueRequestDto) : RetrofitResult<Token>
 
     suspend fun authSignIn(request : SignInRequestDto) : RetrofitResult<Token>
