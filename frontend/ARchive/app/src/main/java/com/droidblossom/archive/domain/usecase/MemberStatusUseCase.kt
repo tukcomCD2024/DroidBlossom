050dc8350fd9dbd1b3d9001ed66e00de5e08c5ea
comst19 <comst19@naver.com>
2024-01-24 00:07:42 +0900

    feat: MemberStatusUseCase 추가

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/usecase/MemberStatusUseCase.kt
@@ -0,0 +1,37 @@
+package com.droidblossom.archive.domain.usecase
+
+import android.util.Log
+import com.droidblossom.archive.data.dto.auth.request.VerificationMessageSendRequestDto
+import com.droidblossom.archive.data.dto.member.request.MemberStatusRequestDto
+import com.droidblossom.archive.data.dto.member.response.MemberStatusResponseDto
+import com.droidblossom.archive.domain.model.auth.VerificationMessageResult
+import com.droidblossom.archive.domain.model.member.MemberStatus
+import com.droidblossom.archive.domain.repository.MemberRepository
+import com.droidblossom.archive.util.RetrofitResult
+import com.droidblossom.archive.util.onError
+import com.droidblossom.archive.util.onException
+import com.droidblossom.archive.util.onFail
+import com.droidblossom.archive.util.onSuccess
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class MemberStatusUseCase @Inject constructor(
+    private val repository: MemberRepository
+) {
+    suspend operator fun invoke(request : MemberStatusRequestDto) = flow<RetrofitResult<MemberStatus>>{
+        repository.postMemberStatus(request)
+            .onSuccess {
+                Log.d("후후후", "성공 - 멤버 상태")
+
+            }.onFail {
+                Log.d("후후후", "실패 - 멤버 상태")
+
+            }.onError {
+                Log.d("후후후", "에러 - 멤버 상태")
+
+            }.onException {
+                Log.d("후후후", "예외 - 멤버 상태")
+
+            }
+    }
+}
