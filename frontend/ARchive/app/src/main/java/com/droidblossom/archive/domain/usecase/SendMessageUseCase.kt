01a766d1882f32b71ff58e1026e35d542a079214
comst19 <comst19@naver.com>
2024-01-24 00:07:15 +0900

    feat: MemberStatusUseCase 추가

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/usecase/SendMessageUseCase.kt
@@ -0,0 +1,36 @@
+package com.droidblossom.archive.domain.usecase
+
+import android.util.Log
+import com.droidblossom.archive.data.dto.auth.request.TokenReIssueRequestDto
+import com.droidblossom.archive.data.dto.auth.request.VerificationMessageSendRequestDto
+import com.droidblossom.archive.domain.model.auth.Token
+import com.droidblossom.archive.domain.model.auth.VerificationMessageResult
+import com.droidblossom.archive.domain.repository.AuthRepository
+import com.droidblossom.archive.util.RetrofitResult
+import com.droidblossom.archive.util.onError
+import com.droidblossom.archive.util.onException
+import com.droidblossom.archive.util.onFail
+import com.droidblossom.archive.util.onSuccess
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class SendMessageUseCase @Inject constructor(
+    private val repository : AuthRepository
+) {
+    suspend operator fun invoke(request : VerificationMessageSendRequestDto) = flow<RetrofitResult<VerificationMessageResult>>{
+        repository.authValidMessageSend(request)
+            .onSuccess {
+                Log.d("후후후", "성공 - 인증문자 전송")
+
+            }.onFail {
+                Log.d("후후후", "실패 - 인증문자 전송")
+
+            }.onError {
+                Log.d("후후후", "에러 - 인증문자 전송")
+
+            }.onException {
+                Log.d("후후후", "예외 - 인증문자 전송")
+
+            }
+    }
+}
