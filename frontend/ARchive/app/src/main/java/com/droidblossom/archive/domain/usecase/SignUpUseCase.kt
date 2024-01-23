2e316da6c9611037a2e530480d506a2220908bad
comst19 <comst19@naver.com>
2024-01-24 00:09:37 +0900

    feat: SignUpUseCase 추가

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/usecase/SignUpUseCase.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/usecase/SignUpUseCase.kt
@@ -1,5 +1,6 @@
 package com.droidblossom.archive.domain.usecase
 
+import android.util.Log
 import com.droidblossom.archive.data.dto.auth.request.SignUpRequestDto
 import com.droidblossom.archive.domain.model.auth.TemporaryToken
 import com.droidblossom.archive.domain.repository.AuthRepository
@@ -17,12 +18,16 @@ class SignUpUseCase @Inject constructor(
     suspend operator fun invoke(request: SignUpRequestDto) = flow<RetrofitResult<TemporaryToken>> {
         repository.authSignUp(request)
             .onSuccess {
+                Log.d("후후후", "성공 - 회원가입")
 
             }.onFail {
+                Log.d("후후후", "실패 - 회원가입")
 
             }.onError {
+                Log.d("후후후", "에러 - 회원가입")
 
             }.onException {
+                Log.d("후후후", "예외 - 회원가입")
 
             }
     }
