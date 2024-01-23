d405cc30a400c42db64802204e5d5c5a6cce7bd8
GaBaljaintheroom <junsu1222@naver.com>
2024-01-24 02:23:16 +0900

    feat : S3 Config 설정

---
+++ backend/core/src/main/java/site/timecapsulearchive/core/infra/s3/config/S3Config.java
@@ -0,0 +1,43 @@
+package site.timecapsulearchive.core.infra.s3.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@Configuration
+@RequiredArgsConstructor
+public class S3Config {
+
+    private final S3Properties s3Properties;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client
+            .builder()
+            .credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials()))
+            .region(Region.of(s3Properties.region()))
+            .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner
+            .builder()
+            .credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials()))
+            .region(Region.of(s3Properties.region()))
+            .build();
+    }
+
+    private AwsBasicCredentials awsBasicCredentials() {
+        return AwsBasicCredentials.create(s3Properties.accessKey(), s3Properties.secretKey());
+    }
+
+    public String getBucketName() {
+        return s3Properties.bucket();
+    }
+}
