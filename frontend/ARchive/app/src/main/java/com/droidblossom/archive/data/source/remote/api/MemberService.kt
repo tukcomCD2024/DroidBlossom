a9ae8a64f5090129359e42c6ec29d6f9327a48cc
comst19 <comst19@naver.com>
2024-01-23 19:07:17 +0900

    feat: MemberService에 postMeStatusApi 추가

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/source/remote/api/MemberService.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/source/remote/api/MemberService.kt
@@ -6,6 +6,7 @@ import com.droidblossom.archive.data.dto.member.request.MemberStatusRequestDto
 import com.droidblossom.archive.data.dto.member.response.MemberDetailResponseDto
 import com.droidblossom.archive.data.dto.member.response.MemberStatusResponseDto
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 
@@ -13,7 +14,12 @@ interface MemberService {
 
     @GET("me/")
     suspend fun getMeApi(): Response<ResponseBody<MemberDetailResponseDto>>
-    
+
+    @POST("me/status")
+    suspend fun postMeStatusApi(
+        @Body checkStatusRequestDto: MemberStatusRequestDto
+    ) : Response<ResponseBody<MemberStatusResponseDto>>
+
     @GET("health")
     suspend fun getTextApi() : Response<ResponseBody<HealthResponseDto>>
 }
