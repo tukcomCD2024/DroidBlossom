b49b9580b45141826c99be7daac9465b9f8b87de
GaBaljaintheroom <junsu1222@naver.com>
2024-01-24 22:53:19 +0900

    feat : s3url 형식 생성 기능 추가

--- backend/core/src/main/java/site/timecapsulearchive/core/infra/s3/service/S3Service.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/infra/s3/service/S3Service.java
@@ -3,9 +3,9 @@ package site.timecapsulearchive.core.infra.s3.service;
 import java.time.Duration;
 import java.util.List;
 import org.springframework.stereotype.Service;
+import site.timecapsulearchive.core.domain.capsule.dto.secret_c.FileMetaData;
 import site.timecapsulearchive.core.infra.s3.config.S3Config;
 import site.timecapsulearchive.core.infra.s3.dto.response.S3PreSignedUrlResponse;
-import site.timecapsulearchive.core.infra.s3.exception.InvalidFileTypeException;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
@@ -15,74 +15,66 @@ import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignReques
 public class S3Service {
 
     private static final long PRE_SIGNED_URL_EXPIRATION_TIME = 10;
-
     private final S3Presigner s3Presigner;
     private final String bucketName;
+    private final String location;
 
     public S3Service(S3Config s3config) {
         this.s3Presigner = s3config.s3Presigner();
         this.bucketName = s3config.getBucketName();
+        this.location = s3config.getS3Location();
     }
 
     /**
      * 업로드할 파일 이름들을 받아서 서명된 S3 링크를 반환한다.
-     * @param memberId 업로드할 유저 아이디
-     * @param directory 업로드할 디렉토리
-     * @param fileNames 업로드할 파일 이름
+     *
+     * @param memberId         업로드할 유저 아이디
+     * @param directory        업로드할 디렉토리
+     * @param fileMetaDataList 업로드할 파일 메타 데이터 리스트
      * @return 서명된 S3 링크
      */
     public S3PreSignedUrlResponse getS3PreSignedUrls(
         Long memberId,
         String directory,
-        List<String> fileNames
+        List<FileMetaData> fileMetaDataList
     ) {
         return S3PreSignedUrlResponse.from(
-            fileNames.stream()
-                .map(fileName -> createS3PreSignedUrl(memberId, directory, fileName))
+            fileMetaDataList.stream()
+                .map(fileMetaData -> createS3PreSignedUrl(memberId, directory, fileMetaData))
                 .toList()
         );
     }
 
-    private String createS3PreSignedUrl(Long memberId, String directory, String fileName) {
-        String newFileName = createNewFileName(memberId, directory, fileName);
+    private String createS3PreSignedUrl(Long memberId, String directory,
+        FileMetaData fileMetaData) {
+        String newFileName = createNewFileName(memberId, directory, fileMetaData.fileName());
 
-        PutObjectRequest putObjectRequest = getPutObjectRequest(newFileName);
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+            .bucket(bucketName)
+            .key(newFileName)
+            .contentType(fileMetaData.extension())
+            .build();
 
-        PutObjectPresignRequest putObjectPresignRequest = getPutObjectPreSignRequest(
-            putObjectRequest);
+        PutObjectPresignRequest putObjectPresignRequest = PutObjectPresignRequest.builder()
+            .signatureDuration(Duration.ofMinutes(PRE_SIGNED_URL_EXPIRATION_TIME))
+            .putObjectRequest(putObjectRequest)
+            .build();
 
         PresignedPutObjectRequest presignedPutObjectRequest = s3Presigner.presignPutObject(
             putObjectPresignRequest);
         return String.valueOf(presignedPutObjectRequest.url());
     }
 
-    private String createNewFileName(Long memberId, String directory, String filename) {
-        return directory + "/" + memberId + "-" + filename;
+    public String geS3UrlFormat(Long memberId, String directory, String fileName) {
+        return "https://"
+            + bucketName
+            + ".s3."
+            + location
+            + ".amazonaws.com/"
+            + createNewFileName(memberId, directory, fileName);
     }
 
-    private PutObjectRequest getPutObjectRequest(String fileName) {
-        return PutObjectRequest.builder()
-            .bucket(bucketName)
-            .key(fileName)
-            .contentType(getContentTypeBasedOnExtension(fileName))
-            .build();
+    private String createNewFileName(Long memberId, String directory, String fileName) {
+        return directory + "/" + memberId + "/" + fileName;
     }
-
-    private String getContentTypeBasedOnExtension(String fileName) {
-        if (fileName.endsWith(".jpg") || fileName.endsWith(".jpeg")) {
-            return "image/jpeg";
-        } else if (fileName.endsWith(".mp4")) {
-            return "video/mp4";
-        }
-        throw new InvalidFileTypeException();
-    }
-
-    private PutObjectPresignRequest getPutObjectPreSignRequest(PutObjectRequest objectRequest) {
-        return PutObjectPresignRequest.builder()
-            .signatureDuration(Duration.ofMinutes(PRE_SIGNED_URL_EXPIRATION_TIME))
-            .putObjectRequest(objectRequest)
-            .build();
-    }
-
-
 }
