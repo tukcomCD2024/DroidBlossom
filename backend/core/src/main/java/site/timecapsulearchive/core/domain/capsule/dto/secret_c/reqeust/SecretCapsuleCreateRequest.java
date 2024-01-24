f3d786536481d510303a50c4218a51e8d9748653
GaBaljaintheroom <junsu1222@naver.com>
2024-01-24 23:03:49 +0900

    feat : 비밀 캡슐 생성 요청 DTO 작성

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/capsule/dto/secret_c/reqeust/SecretCapsuleCreateRequest.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/capsule/dto/secret_c/reqeust/SecretCapsuleCreateRequest.java
@@ -1,33 +1,46 @@
 package site.timecapsulearchive.core.domain.capsule.dto.secret_c.reqeust;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import java.time.ZonedDateTime;
 import java.util.List;
-import org.springframework.web.multipart.MultipartFile;
+import site.timecapsulearchive.core.domain.capsule.dto.secret_c.FileMetaData;
+import site.timecapsulearchive.core.domain.capsule.entity.CapsuleType;
 
 @Schema(description = "캡슐 생성 포맷")
 public record SecretCapsuleCreateRequest(
 
-    @Schema(description = "미디어들(이미지, 비디오)")
-    List<MultipartFile> media,
+    @Schema(description = "이미지, 비디오 파일 이름")
+    List<FileMetaData> fileNames,
+
+    @Schema(description = "디렉토리 이름")
+    String directory,
 
     @Schema(description = "캡슐 스킨 아이디")
     Long capsuleSkinId,
 
     @Schema(description = "제목")
+    @NotBlank(message = "캡슐 제목은 필수 입니다.")
     String title,
 
     @Schema(description = "내용")
+    @NotBlank(message = "캡슐 내용은 필수 입니다.")
     String content,
 
     @Schema(description = "현재 경도")
-    Float longitude,
+    @NotBlank(message = "캡슐 경도는 필수 입니다.")
+    Double longitude,
 
     @Schema(description = "현재 위도")
-    Float latitude,
+    @NotBlank(message = "캡슐 위도는 필수 입니다.")
+    Double latitude,
 
     @Schema(description = "개봉일")
-    ZonedDateTime dueDate
+    ZonedDateTime dueDate,
+
+    @Schema(description = "캡슐 타입")
+    @NotBlank(message = "캡슐 타입은 필수 입니다.")
+    CapsuleType capsuleType
 ) {
 
 }
