ee67b29627724beb00aebbd2829967edd2d6f59c
comst19 <comst19@naver.com>
2024-01-24 12:51:50 +0900

    feat: EncryptedSharedPreferences을 이용한 TokenUtil 구현

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/util/TokenUtils.kt
@@ -0,0 +1,42 @@
+package com.droidblossom.archive.util
+
+import android.content.Context
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+
+
+class TokenUtils(context: Context) {
+
+    val masterKeyAlias = MasterKey
+        .Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+        .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+        .build()
+
+    val pref = EncryptedSharedPreferences.create(
+        context,
+        FILE_NAME,
+        masterKeyAlias,
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+    )
+
+    suspend fun saveAccessToken(accessToken: String) {
+        pref.edit().putString("AccessToken", accessToken).apply()
+    }
+
+    fun fetchAccessToken(): String {
+        return pref.getString("AccessToken", "") ?: ""
+    }
+
+    suspend fun saveRefreshToken(refreshToken: String) {
+        pref.edit().putString("RefreshToken", refreshToken).apply()
+    }
+
+    fun fetchRefreshToken(): String {
+        return pref.getString("RefreshToken", "") ?: ""
+    }
+
+    companion object {
+        private const val FILE_NAME = "ARchive_encrypted_pref_file"
+    }
+}
