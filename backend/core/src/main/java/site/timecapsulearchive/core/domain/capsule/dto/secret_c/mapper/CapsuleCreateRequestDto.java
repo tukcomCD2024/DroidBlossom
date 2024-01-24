6f5008a008372e7d3117fe97eb77473bbb486611
GaBaljaintheroom <junsu1222@naver.com>
2024-01-24 23:00:31 +0900

    feat : 캡슐 생성 request dto 작성

---
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/capsule/dto/secret_c/mapper/CapsuleCreateRequestDto.java
@@ -0,0 +1,21 @@
+package site.timecapsulearchive.core.domain.capsule.dto.secret_c.mapper;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+import site.timecapsulearchive.core.domain.capsule.dto.secret_c.FileMetaData;
+import site.timecapsulearchive.core.domain.capsule.entity.CapsuleType;
+
+public record CapsuleCreateRequestDto(
+    Long capsuleSkinId,
+    String title,
+    String content,
+    Double longitude,
+    Double latitude,
+    ZonedDateTime dueDate,
+    List<FileMetaData> fileNames,
+    String directory,
+    CapsuleType capsuleType
+
+) {
+
+}
