0f3d77db469f34457a0d5a3c97cac5ecc78b6aa8
GaBaljaintheroom <junsu1222@naver.com>
2024-01-25 16:01:23 +0900

    feat : hash 암호화 기능 추가

---
+++ backend/core/src/main/java/site/timecapsulearchive/core/global/security/encryption/HashEncryptionManager.java
@@ -0,0 +1,29 @@
+package site.timecapsulearchive.core.global.security.encryption;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import site.timecapsulearchive.core.global.error.exception.EncryptionException;
+
+@Component
+@RequiredArgsConstructor
+public class HashEncryptionManager {
+
+    private static final String HASH_ALGORITHM = "SHA-256";
+
+    private final HashProperties hashProperties;
+
+    public byte[] encrypt(byte[] plaintText) {
+        MessageDigest digest;
+        try {
+             digest = MessageDigest.getInstance(HASH_ALGORITHM);
+        } catch (NoSuchAlgorithmException e) {
+            throw new EncryptionException(e);
+        }
+
+        digest.update(hashProperties.salt().getBytes());
+
+        return digest.digest(plaintText);
+    }
+}
