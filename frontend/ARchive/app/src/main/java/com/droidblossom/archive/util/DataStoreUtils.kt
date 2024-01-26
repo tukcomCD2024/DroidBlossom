65dca7f6a0b51e1f5118a83f250b46ab81273c8c
comst19 <comst19@naver.com>
2024-01-26 14:42:26 +0900

    feat: EncryptionUtils를 이용한 DataStore 추가 - 엑세스, 리프레시 토큰 저장

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/util/DataStoreUtils.kt
@@ -0,0 +1,53 @@
+package com.droidblossom.archive.util
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import java.util.prefs.Preferences
+import javax.inject.Inject
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "my_data_store")
+
+class DataStoreUtils @Inject constructor(private val context: Context) {
+    companion object {
+        private val ACCESS_TOKEN_KEY = stringPreferencesKey("AccessToken")
+        private val REFRESH_TOKEN_KEY = stringPreferencesKey("RefreshToken")
+    }
+
+    suspend fun saveAccessToken(accessToken: String) {
+        val (iv, encryptedData) = EncryptionUtils.encrypt(accessToken)
+        context.dataStore.edit { preferences ->
+            preferences[ACCESS_TOKEN_KEY] = encryptedData.toBase64() + ":" + iv.toBase64()
+        }
+    }
+
+    suspend fun fetchAccessToken(): String {
+        val preferences = context.dataStore.data.first()
+        val combined = preferences[ACCESS_TOKEN_KEY] ?: return ""
+        val (encryptedData, iv) = combined.split(":").map { it.fromBase64() }
+        return EncryptionUtils.decrypt(iv, encryptedData)
+    }
+
+    suspend fun saveRefreshToken(refreshToken: String) {
+        val (iv, encryptedData) = EncryptionUtils.encrypt(refreshToken)
+        context.dataStore.edit { preferences ->
+            preferences[REFRESH_TOKEN_KEY] = encryptedData.toBase64() + ":" + iv.toBase64()
+        }
+    }
+
+    suspend fun fetchRefreshToken(): String {
+        val preferences = context.dataStore.data.first()
+        val combined = preferences[REFRESH_TOKEN_KEY] ?: return ""
+        val (encryptedData, iv) = combined.split(":").map { it.fromBase64() }
+        return EncryptionUtils.decrypt(iv, encryptedData)
+    }
+
+    suspend fun resetTokenData() {
+        context.dataStore.edit { preferences ->
+            preferences.remove(ACCESS_TOKEN_KEY)
+            preferences.remove(REFRESH_TOKEN_KEY)
+        }
+    }
+}
+
+private fun ByteArray.toBase64(): String = android.util.Base64.encodeToString(this, android.util.Base64.NO_WRAP)
+private fun String.fromBase64(): ByteArray = android.util.Base64.decode(this, android.util.Base64.NO_WRAP)
