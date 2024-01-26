4ea561f563aef9697db1d29422a25f1504fa76c5
hangunhee39 <ghee0831@naver.com>
2024-01-27 00:25:54 +0900

    feat : S3Service

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/source/remote/api/S3Service.kt
@@ -0,0 +1,17 @@
+package com.droidblossom.archive.data.source.remote.api
+
+import com.droidblossom.archive.data.dto.ResponseBody
+import com.droidblossom.archive.data.dto.s3.request.S3UrlRequestDto
+import com.droidblossom.archive.data.dto.s3.response.S3UrlResponseDto
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+
+interface S3Service {
+
+    @GET("file/upload-url")
+    suspend fun getUploadUrls(
+        @Body request : S3UrlRequestDto
+    ) : Response<ResponseBody<S3UrlResponseDto>>
+
+}
