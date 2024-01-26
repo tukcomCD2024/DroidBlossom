46d172c144258b2b898e96355a67315c781f1ee7
hangunhee39 <ghee0831@naver.com>
2024-01-26 13:56:44 +0900

    feat : capsulePage Dto/model

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/dto/secret/response/SecretCapsulePageResponseDto.kt
@@ -0,0 +1,15 @@
+package com.droidblossom.archive.data.dto.secret.response
+
+import com.droidblossom.archive.domain.model.secret.SecretCapsulePage
+
+data class SecretCapsulePageResponseDto(
+    val capsules: List<SecretCapsuleSummeryDto>,
+    val hasNext: Boolean,
+    val hasPrevious: Boolean
+){
+    fun toModel() = SecretCapsulePage(
+        capsules = this.capsules.map { it.toModel() },
+        hasNext = this.hasNext,
+        hasPrevious = this.hasPrevious,
+    )
+}
