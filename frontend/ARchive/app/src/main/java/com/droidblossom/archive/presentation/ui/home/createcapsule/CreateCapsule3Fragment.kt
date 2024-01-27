c3a42019330bf2c13da8cad1422ebc971eae1aec
hangunhee39 <ghee0831@naver.com>
2024-01-28 01:09:57 +0900

    feat : createCapsule3 Event

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/createcapsule/CreateCapsule3Fragment.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/createcapsule/CreateCapsule3Fragment.kt
@@ -3,11 +3,14 @@ package com.droidblossom.archive.presentation.ui.home.createcapsule
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.droidblossom.archive.R
 import com.droidblossom.archive.databinding.FragmentCreateCapsule3Binding
 import com.droidblossom.archive.presentation.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class CreateCapsule3Fragment :
@@ -20,6 +23,27 @@ class CreateCapsule3Fragment :
         binding.vm = viewModel
     }
     override fun observeData() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModel.create3Events.collect{
+                    when(it){
+                        CreateCapsuleViewModel.Create3Event.ClickDate -> {
+                            //날짜 Dialog 디자인?
+                        }
+                        CreateCapsuleViewModel.Create3Event.ClickFinish -> {
+                            //캡슐 생성 api 추가
+                            requireActivity().finish()
+                        }
+                        CreateCapsuleViewModel.Create3Event.ClickImgUpLoad -> {
+
+                        }
+                        CreateCapsuleViewModel.Create3Event.ClickLocation -> {
+                            //위치 목록 dialog or page ?
+                        }
+                    }
+                }
+            }
+        }
     }
 
 }
