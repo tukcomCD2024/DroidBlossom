4c6ce9a883112a6d1541ba70a51a67ddfbc1a2fc
hangunhee39 <ghee0831@naver.com>
2024-01-27 23:21:16 +0900

    feat: CreateCapsule ViewModel Evnet 추가

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/createcapsule/CreateCapsuleViewModel.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/createcapsule/CreateCapsuleViewModel.kt
@@ -1,6 +1,25 @@
 package com.droidblossom.archive.presentation.ui.home.createcapsule
 
+import com.droidblossom.archive.presentation.ui.auth.AuthViewModel
+
 interface CreateCapsuleViewModel {
     var isGroupCapsuleCreate : Boolean
 
+
+    sealed class Create1Event {
+        object NavigateTo2 : Create1Event()
+    }
+
+    sealed class Create2Event {
+        object NavigateTo3 : Create2Event()
+
+        object SearchSkin : Create2Event()
+    }
+    sealed class Create3Event {
+        object Finish : Create3Event()
+
+        object ClickTime : Create3Event()
+        object ClickDate : Create3Event()
+        object ClickImgUpLoad : Create3Event()
+    }
 }
