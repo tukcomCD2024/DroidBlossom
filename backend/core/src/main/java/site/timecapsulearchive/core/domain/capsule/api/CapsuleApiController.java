dd5f833887285e55635afa4a2980d1d11ea13ac5
GaBaljaintheroom <junsu1222@naver.com>
2024-01-27 14:07:50 +0900

    feat : 내 주변 캡슐 조회 Controller 작성

---
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/capsule/api/CapsuleApiController.java
@@ -0,0 +1,57 @@
+package site.timecapsulearchive.core.domain.capsule.api;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import site.timecapsulearchive.core.domain.capsule.CoordinateRangeRequestDto;
+import site.timecapsulearchive.core.domain.capsule.dto.response.ImagesPageResponse;
+import site.timecapsulearchive.core.domain.capsule.dto.response.MyCapsulePageResponse;
+import site.timecapsulearchive.core.domain.capsule.dto.response.NearbyCapsulePageResponse;
+import site.timecapsulearchive.core.domain.capsule.entity.CapsuleType;
+import site.timecapsulearchive.core.domain.capsule.service.CapsuleService;
+import site.timecapsulearchive.core.global.common.response.ApiSpec;
+import site.timecapsulearchive.core.global.common.response.SuccessCode;
+
+@RestController
+@RequestMapping("/capsules")
+@RequiredArgsConstructor
+public class CapsuleApiController implements CapsuleApi {
+
+    private final CapsuleService capsuleService;
+
+    @Override
+    public ResponseEntity<MyCapsulePageResponse> findCapsulesByMemberId(Long size, Long capsuleId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<ImagesPageResponse> findImages(Long size, Long capsuleId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<ApiSpec<NearbyCapsulePageResponse>> getNearByCapsules(
+        @AuthenticationPrincipal Long memberId,
+        @NotNull @Valid @RequestParam(value = "latitude") double latitude,
+        @NotNull @Valid @RequestParam(value = "longitude") double longitude,
+        @NotNull @Valid @RequestParam(value = "distance") double distance,
+        @NotNull @Valid @RequestParam(value = "capsule_type", required = false, defaultValue = "ALL"
+        ) CapsuleType capsuleType
+    ) {
+        return ResponseEntity.ok(
+            ApiSpec.success(
+                SuccessCode.SUCCESS,
+                capsuleService.findCapsuleByCurrentLocationAndCapsuleType(
+                    memberId,
+                    CoordinateRangeRequestDto.from(latitude, longitude, distance),
+                    capsuleType
+                )
+            )
+        );
+    }
+}
