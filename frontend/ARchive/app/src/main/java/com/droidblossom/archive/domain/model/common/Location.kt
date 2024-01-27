065a94cda6d316a27ea29426e3c6e6e510f74f11
hangunhee39 <ghee0831@naver.com>
2024-01-28 00:24:11 +0900

    faet: CreateCapsuleViewModel

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/model/common/Location.kt
@@ -0,0 +1,6 @@
+package com.droidblossom.archive.domain.model.common
+
+data class Location(
+    val longitude : Double,
+    val latitude : Double
+)
--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/createcapsule/CreateCapsuleViewModel.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/createcapsule/CreateCapsuleViewModel.kt
@@ -1,9 +1,32 @@
 package com.droidblossom.archive.presentation.ui.home.createcapsule
 
+import com.droidblossom.archive.domain.model.common.FileName
+import com.droidblossom.archive.domain.model.common.Location
 import com.droidblossom.archive.presentation.ui.auth.AuthViewModel
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 
 interface CreateCapsuleViewModel {
-    var isGroupCapsuleCreate : Boolean
+    var isGroupCapsuleCreate: Boolean
+    val capsuleType: StateFlow<CapsuleType>
+
+    //Create1
+    val create1Events: SharedFlow<Create1Event>
+    val groupId: StateFlow<Int>
+
+    //Create2
+    val create2Events: SharedFlow<Create2Event>
+    val skinId: StateFlow<Int>
+
+    //Create3
+    val create3Event: SharedFlow<Create3Event>
+    val capsuleTitle: StateFlow<String>
+    val capsuleContent: StateFlow<String>
+    val capsuleLocationName: StateFlow<String>
+    val capsuleDueDate: StateFlow<String>
+    val capsuleLocation : StateFlow<Location>
+    val capsuleImg : StateFlow<List<FileName>>
+    val capsuleImgUrls: StateFlow<List<String>>
 
 
     sealed class Create1Event {
@@ -15,11 +38,15 @@ interface CreateCapsuleViewModel {
 
         object SearchSkin : Create2Event()
     }
-    sealed class Create3Event {
-        object Finish : Create3Event()
 
-        object ClickTime : Create3Event()
+    sealed class Create3Event {
+        object ClickFinish : Create3Event()
+        object ClickLocation : Create3Event()
         object ClickDate : Create3Event()
         object ClickImgUpLoad : Create3Event()
+    }
+
+    enum class CapsuleType(val title: String) {
+        SECRET("SECRET"), GROUP("GROUP"), OPEN("OPEN")
     }
 }
