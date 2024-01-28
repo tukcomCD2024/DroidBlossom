5424c46aec8ba20318990266e8f30ebf9e74bdb1
hong seokho <seokho7799@gmail.com>
2024-01-28 15:34:25 +0900

    style : 구글 스타일 적용

--- backend/core/src/main/java/site/timecapsulearchive/core/infra/map/MapApiService.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/infra/map/MapApiService.java
@@ -6,8 +6,9 @@ public interface MapApiService {
 
     /**
      * 경도와 위도를 받아서 주소를 반환한다.
+     *
      * @param longitude 경도
-     * @param latitude 위도
+     * @param latitude  위도
      * @return 위도와 경도로부터 반환된 주소
      */
     Address reverseGeoCoding(Double longitude, Double latitude);
