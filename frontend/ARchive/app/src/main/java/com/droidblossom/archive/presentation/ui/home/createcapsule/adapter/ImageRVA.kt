139803ff02028377359b7cefc2e820cc16df467d
hangunhee39 <ghee0831@naver.com>
2024-01-29 03:23:22 +0900

    feat: viewPager (ListAdapter) 데이터 꼬임 해결

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/createcapsule/adapter/ImageRVA.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/createcapsule/adapter/ImageRVA.kt
@@ -3,23 +3,29 @@ package com.droidblossom.archive.presentation.ui.home.createcapsule.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.droidblossom.archive.R
 import com.droidblossom.archive.databinding.ItemPostImageBinding
 import com.droidblossom.archive.domain.model.common.Dummy
 
-class ImageRVA(val onClick: () -> Unit, val flowData : (List<Dummy>)->Unit) : ListAdapter<Dummy, ImageRVA.ItemViewHolder>(differ) {
+class ImageRVA(val onClick: () -> Unit, val flowData: (List<Dummy>) -> Unit) :
+    ListAdapter<Dummy, ImageRVA.ItemViewHolder>(differ) {
 
     inner class ItemViewHolder(
         private val binding: ItemPostImageBinding
     ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(data: Dummy) {
-            if (data.last){
-                binding.root.setOnClickListener{
+            if (data.last) {
+                binding.root.setOnClickListener {
                     onClick()
                 }
-            }else {
+                binding.postImg.setImageResource(R.color.Transparency)
+                binding.plusImg.isVisible = true
+                binding.plusT.isVisible = true
+            } else {
                 binding.postImg.setImageURI(data.string)
                 binding.plusImg.isGone = true
                 binding.plusT.isGone = true
@@ -30,8 +36,8 @@ class ImageRVA(val onClick: () -> Unit, val flowData : (List<Dummy>)->Unit) : Li
         }
     }
 
-    fun removeItem(position: Int){
-        val currentList =  currentList.toMutableList()
+    fun removeItem(position: Int) {
+        val currentList = currentList.toMutableList()
         currentList.removeAt(position)
         submitList(currentList)
         flowData(currentList)
@@ -57,7 +63,7 @@ class ImageRVA(val onClick: () -> Unit, val flowData : (List<Dummy>)->Unit) : Li
     companion object {
         val differ = object : DiffUtil.ItemCallback<Dummy>() {
             override fun areItemsTheSame(oldItem: Dummy, newItem: Dummy): Boolean {
-                return oldItem.string == newItem.string
+                return oldItem.last == newItem.last
             }
 
             override fun areContentsTheSame(oldItem: Dummy, newItem: Dummy): Boolean {
