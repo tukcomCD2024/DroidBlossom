a1aeac2035fc5611b2d9c9abbdd2d965a3f2b142
hangunhee39 <ghee0831@naver.com>
2024-01-29 14:04:51 +0900

    feat: 스킨 리사이클러뷰 단일 선택 구현

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/createcapsule/CreateCapsule2Fragment.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/createcapsule/CreateCapsule2Fragment.kt
@@ -12,7 +12,10 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.droidblossom.archive.R
 import com.droidblossom.archive.databinding.FragmentCreateCapsule2Binding
+import com.droidblossom.archive.domain.model.common.Skin
 import com.droidblossom.archive.presentation.base.BaseFragment
+import com.droidblossom.archive.presentation.ui.home.createcapsule.adapter.ImageRVA
+import com.droidblossom.archive.presentation.ui.home.createcapsule.adapter.SkinRVA
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -24,38 +27,53 @@ class CreateCapsule2Fragment :
     lateinit var navController: NavController
     private lateinit var callback: OnBackPressedCallback
 
+    private val skinRVA by lazy {
+        SkinRVA { viewModel.changeSkin(it) }
+    }
+
     //그룹캡슐이 아닐 경우 바로 엑티비티 닫기
     override fun onAttach(context: Context) {
         super.onAttach(context)
         callback = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
-                if (!viewModel.isGroupCapsuleCreate){
+                if (!viewModel.isGroupCapsuleCreate) {
                     requireActivity().finish()
-                }else{
+                } else {
                     super.remove()
                 }
             }
         }
         requireActivity().onBackPressedDispatcher.addCallback(this, callback)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.vm = viewModel
         navController = Navigation.findNavController(view)
+        binding.recycleView.adapter = skinRVA
     }
 
     override fun observeData() {
         viewLifecycleOwner.lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED){
-                viewModel.create2Events.collect{
-                    when(it){
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.create2Events.collect {
+                    when (it) {
                         CreateCapsuleViewModel.Create2Event.NavigateTo3 -> {
                             navController.navigate(R.id.action_createCapsule2Fragment_to_createCapsule3Fragment)
                         }
+
                         CreateCapsuleViewModel.Create2Event.SearchSkin -> {
 
                         }
                     }
+                }
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.skins.collect {
+                    skinRVA.submitList(it)
                 }
             }
         }
