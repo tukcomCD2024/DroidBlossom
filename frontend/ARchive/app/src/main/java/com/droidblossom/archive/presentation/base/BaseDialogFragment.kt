7ef0c5514c844695b29ef97555a171fc337e29b6
hangunhee39 <ghee0831@naver.com>
2024-01-31 15:44:20 +0900

    faet: baseDialogFragment

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/base/BaseDialogFragment.kt
@@ -0,0 +1,39 @@
+package com.droidblossom.archive.presentation.base
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.DialogFragment
+import com.droidblossom.archive.R
+
+abstract class BaseDialogFragment<V : ViewDataBinding>(@LayoutRes val layoutResource: Int) :
+    DialogFragment() {
+    private var _binding: V? = null
+    protected val binding: V get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DataBindingUtil.inflate(
+            layoutInflater,
+            layoutResource,
+            null,
+            false
+        )
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#99000000")))
+    }
+}
