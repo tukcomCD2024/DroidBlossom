86a80f99deea5ac6c617b1ec67810a794fa6f9e4
comst19 <comst19@naver.com>
2024-02-01 15:41:36 +0900

    feat: CapsulePreviewDialog 구현

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/dialog/CapsulePreviewDialogFragment.kt
@@ -0,0 +1,23 @@
+package com.droidblossom.archive.presentation.ui.home.dialog
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import com.droidblossom.archive.R
+import com.droidblossom.archive.databinding.FragmentCapsulePreviewDialogBinding
+import com.droidblossom.archive.presentation.base.BaseDialogFragment
+
+class CapsulePreviewDialogFragment : BaseDialogFragment<FragmentCapsulePreviewDialogBinding>(R.layout.fragment_capsule_preview_dialog) {
+    
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    private fun initObserver(){
+
+    }
+}
