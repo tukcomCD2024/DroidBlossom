ccabd77204f10c192145256481d23f3d6c1d4cbf
comst19 <comst19@naver.com>
2024-02-01 15:45:51 +0900

    feat: Dialog 위치 조정, 배경 삭제

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/dialog/CapsulePreviewDialogFragment.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/dialog/CapsulePreviewDialogFragment.kt
@@ -11,7 +11,27 @@ import com.droidblossom.archive.databinding.FragmentCapsulePreviewDialogBinding
 import com.droidblossom.archive.presentation.base.BaseDialogFragment
 
 class CapsulePreviewDialogFragment : BaseDialogFragment<FragmentCapsulePreviewDialogBinding>(R.layout.fragment_capsule_preview_dialog) {
-    
+
+    override fun onStart() {
+        super.onStart()
+        val dialog = dialog
+        if (dialog != null) {
+            val width = ViewGroup.LayoutParams.MATCH_PARENT
+            val height = ViewGroup.LayoutParams.WRAP_CONTENT
+            dialog.window?.setLayout(width, height)
+            dialog.window?.setDimAmount(0f)
+            dialog.window?.setGravity(Gravity.BOTTOM)
+
+            val marginBottomDp = 120f
+            val density = resources.displayMetrics.density
+            val marginBottomPx = (marginBottomDp * density).toInt()
+
+            val params = dialog.window?.attributes
+            params?.y = marginBottomPx
+            dialog.window?.attributes = params
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
