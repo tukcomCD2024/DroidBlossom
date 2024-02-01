5ee5b890eb18ee930e39162554ffd19a25efe3c2
hong seokho <seokho7799@gmail.com>
2024-02-01 11:33:13 +0900

    feat : 레포지토리 추가
    - 캡슐 스킨 목록 조회 쿼리 추가

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/capsuleskin/dto/response/CapsuleSkinSummaryResponse.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/capsuleskin/dto/response/CapsuleSkinSummaryResponse.java
@@ -1,20 +1,21 @@
 package site.timecapsulearchive.core.domain.capsuleskin.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.ZonedDateTime;
 
 @Schema(description = "캡슐 스킨 요약 정보")
 public record CapsuleSkinSummaryResponse(
     @Schema(description = "캡슐 스킨 아이디")
-
     Long id,
 
     @Schema(description = "캡슐 스킨 url")
-
     String skinUrl,
 
     @Schema(description = "캡슐 스킨 이름")
+    String name,
 
-    String name
+    @Schema(description = "캡슐 스킨 생성일")
+    ZonedDateTime createdAt
 ) {
 
 }
---
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/capsuleskin/repository/CapsuleSkinQueryRepository.java
@@ -0,0 +1,62 @@
+package site.timecapsulearchive.core.domain.capsuleskin.repository;
+
+import static site.timecapsulearchive.core.domain.capsuleskin.entity.QCapsuleSkin.capsuleSkin;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.ZonedDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.stereotype.Repository;
+import site.timecapsulearchive.core.domain.capsuleskin.dto.response.CapsuleSkinSummaryResponse;
+
+@Repository
+@RequiredArgsConstructor
+public class CapsuleSkinQueryRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    public Slice<CapsuleSkinSummaryResponse> findCapsuleSkinSliceByCreatedAtAndMemberId(
+        Long memberId,
+        int size,
+        ZonedDateTime createdAt
+    ) {
+        List<CapsuleSkinSummaryResponse> capsuleSkins = selectCapsuleSkinSummaryResponse(
+            memberId,
+            size,
+            createdAt
+        );
+
+        boolean hasNext = canMoreRead(size, capsuleSkins.size());
+        if (hasNext) {
+            capsuleSkins.remove(size);
+        }
+
+        return new SliceImpl<>(capsuleSkins, Pageable.ofSize(size), hasNext);
+    }
+
+    private List<CapsuleSkinSummaryResponse> selectCapsuleSkinSummaryResponse(Long memberId, int size,
+        ZonedDateTime createdAt) {
+        return jpaQueryFactory
+            .select(
+                Projections.constructor(
+                    CapsuleSkinSummaryResponse.class,
+                    capsuleSkin.id,
+                    capsuleSkin.imageUrl,
+                    capsuleSkin.skinName,
+                    capsuleSkin.createdAt
+                )
+            )
+            .from(capsuleSkin)
+            .where(capsuleSkin.member.id.eq(memberId).and(capsuleSkin.createdAt.lt(createdAt)))
+            .limit(size + 1)
+            .fetch();
+    }
+
+    private boolean canMoreRead(int size, int capsuleSize) {
+        return capsuleSize > size;
+    }
+}
