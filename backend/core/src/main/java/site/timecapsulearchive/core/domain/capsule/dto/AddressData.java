d1aee85bf1c64717a155ea1ced77549c489cdb05
GaBaljaintheroom <junsu1222@naver.com>
2024-02-01 15:40:16 +0900

    feat : AddressData 생성

---
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/capsule/dto/AddressData.java
@@ -0,0 +1,26 @@
+package site.timecapsulearchive.core.domain.capsule.dto;
+
+import lombok.Builder;
+
+@Builder
+public record AddressData(
+    String fullRoadAddressName,
+    String province,
+
+    String city,
+
+    String subDistinct,
+
+    String roadName,
+
+    String mainBuildingNumber,
+
+    String subBuildingNumber,
+
+    String buildingName,
+
+    String zipCode
+
+) {
+
+}
