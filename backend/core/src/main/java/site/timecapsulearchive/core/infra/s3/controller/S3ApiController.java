6da0b978bc1af13676c879c98aebc73ac33c0406
hong seokho <seokho7799@gmail.com>
2024-02-02 22:02:03 +0900

    fix : s3 url 생성 api 수정

--- backend/core/src/main/java/site/timecapsulearchive/core/infra/s3/controller/S3ApiController.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/infra/s3/controller/S3ApiController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import site.timecapsulearchive.core.global.common.response.ApiSpec;
 import site.timecapsulearchive.core.global.common.response.SuccessCode;
+import site.timecapsulearchive.core.infra.s3.dto.mapper.S3ApiMapper;
 import site.timecapsulearchive.core.infra.s3.dto.request.S3PreSignedUrlRequest;
+import site.timecapsulearchive.core.infra.s3.dto.S3PreSignedUrlDto;
 import site.timecapsulearchive.core.infra.s3.dto.response.S3PreSignedUrlResponse;
 import site.timecapsulearchive.core.infra.s3.service.S3Service;
 
@@ -19,20 +21,22 @@ import site.timecapsulearchive.core.infra.s3.service.S3Service;
 public class S3ApiController implements S3Api {
 
     private final S3Service s3Service;
+    private final S3ApiMapper mapper;
 
     @Override
     public ResponseEntity<ApiSpec<S3PreSignedUrlResponse>> getS3PreSignedUrl(
         @AuthenticationPrincipal final Long memberId,
         @RequestBody @Valid final S3PreSignedUrlRequest request
     ) {
+        S3PreSignedUrlDto s3PreSignedUrlsDto = s3Service.getS3PreSignedUrls(
+            memberId,
+            mapper.s3PreSignedUrlRequestToDto(request)
+        );
+
         return ResponseEntity.ok(
             ApiSpec.success(
                 SuccessCode.SUCCESS,
-                s3Service.getS3PreSignedUrls(
-                    memberId,
-                    request.directory(),
-                    request.fileMetaDataList()
-                )
+                S3PreSignedUrlResponse.of(s3PreSignedUrlsDto.preSignedUrls())
             )
         );
     }
