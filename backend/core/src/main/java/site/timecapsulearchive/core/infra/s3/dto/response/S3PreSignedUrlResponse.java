bed39f869313ad1e531b340a53820a7e4d5babe1
hong seokho <seokho7799@gmail.com>
2024-02-02 21:51:44 +0900

    feat : response 추가

--- backend/core/src/main/java/site/timecapsulearchive/core/infra/s3/dto/response/S3PreSignedUrlResponse.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/infra/s3/dto/response/S3PreSignedUrlResponse.java
@@ -1,10 +1,16 @@
 package site.timecapsulearchive.core.infra.s3.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
-public record S3PreSignedUrlResponse(List<String> preSignedUrls) {
+@Schema(description = "미리 서명된 s3 url 응답")
+public record S3PreSignedUrlResponse(
 
-    public static S3PreSignedUrlResponse from(List<String> preSignedUrls) {
+    @Schema(description = "미리 서명된 s3 url들")
+    List<String> preSignedUrls
+) {
+
+    public static S3PreSignedUrlResponse of(List<String> preSignedUrls) {
         return new S3PreSignedUrlResponse(preSignedUrls);
     }
 }
