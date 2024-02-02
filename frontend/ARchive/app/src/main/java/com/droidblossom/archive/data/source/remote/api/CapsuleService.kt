7a160676a40e6b6c7480c24514ece9ba05045a09
hangunhee39 <ghee0831@naver.com>
2024-02-02 12:57:47 +0900

    faet : capsules/full-address api 설정

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/source/remote/api/CapsuleService.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/source/remote/api/CapsuleService.kt
@@ -1,6 +1,7 @@
 package com.droidblossom.archive.data.source.remote.api
 
 import com.droidblossom.archive.data.dto.ResponseBody
+import com.droidblossom.archive.data.dto.capsule.response.AddressDataDto
 import com.droidblossom.archive.data.dto.capsule.response.NearbyCapsuleResponseDto
 import retrofit2.Response
 import retrofit2.http.GET
@@ -15,4 +16,10 @@ interface CapsuleService {
         @Query("distance") distance : Double,
         @Query("capsule_type") capsule_type : String
     ) : Response<ResponseBody<NearbyCapsuleResponseDto>>
+
+    @GET("capsules/full-address")
+    suspend fun getAddressApi(
+        @Query("latitude") latitude : Double,
+        @Query("longitude") longitude : Double,
+    ) : Response<ResponseBody<AddressDataDto>>
 }
