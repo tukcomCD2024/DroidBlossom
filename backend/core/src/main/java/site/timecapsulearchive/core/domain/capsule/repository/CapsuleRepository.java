c35cf35c5cf748cc0b84c082086b2edf4e10d5b8
GaBaljaintheroom <junsu1222@naver.com>
2024-02-04 02:23:00 +0900

    feat : 캡슐 조회 쿼리 작성

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/capsule/repository/CapsuleRepository.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/capsule/repository/CapsuleRepository.java
@@ -1,9 +1,18 @@
 package site.timecapsulearchive.core.domain.capsule.repository;
 
+import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 import site.timecapsulearchive.core.domain.capsule.entity.Capsule;
 
 public interface CapsuleRepository extends Repository<Capsule, Long> {
 
     Capsule save(Capsule capsule);
+
+    @Query("select c from Capsule c where c.id = :capsuleId and c.member.id = :memberId")
+    Optional<Capsule> findCapsuleByMemberIdAndCapsuleId(
+        @Param("memberId") Long memberId,
+        @Param("capsuleId") Long capsuleId
+    );
 }
