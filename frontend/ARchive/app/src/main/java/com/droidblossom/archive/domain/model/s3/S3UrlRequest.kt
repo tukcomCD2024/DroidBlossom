7a7da9ca170939863eff9d6a87ec6447b7cf7bfc
comst19 <comst19@naver.com>
2024-02-04 01:11:16 +0900

    chore: S3UrlRequest(dto, model) 프로퍼티 변경

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/model/s3/S3UrlRequest.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/model/s3/S3UrlRequest.kt
@@ -1,14 +1,15 @@
 package com.droidblossom.archive.domain.model.s3
 
 import com.droidblossom.archive.data.dto.s3.request.S3UrlRequestDto
-import com.droidblossom.archive.domain.model.common.FileName
 
 data class S3UrlRequest(
     val directory: String,
-    val fileMetaDataList: List<FileName>
+    val imageUrls: List<String>,
+    val videoUrls: List<String>
 ){
     fun toDto() = S3UrlRequestDto(
         directory = this.directory,
-        fileMetaDataList = this.fileMetaDataList.map { it.toDto() }
+        imageUrls = this.imageUrls.ifEmpty { null },
+        videoUrls = this.videoUrls.ifEmpty { null }
     )
 }
