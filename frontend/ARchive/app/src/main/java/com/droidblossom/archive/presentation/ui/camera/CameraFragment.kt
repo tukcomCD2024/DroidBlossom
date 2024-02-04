1c8c929ab4e7a53cc5ee8c0364dfc4716b32efc6
hangunhee39 <ghee0831@naver.com>
2024-02-04 19:57:49 +0900

    feat : camera 에 newIntent , TAG 추가

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/camera/CameraFragment.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/camera/CameraFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.droidblossom.archive.R
+import com.droidblossom.archive.presentation.ui.skin.SkinFragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -38,23 +39,9 @@ class CameraFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_camera, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CameraFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CameraFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    companion object{
+
+        const val TAG = "CAMERA"
+        fun newIntent()= CameraFragment()
     }
 }
