4e21cf1366c7693064080af6e62b5c19d5490393
hangunhee39 <ghee0831@naver.com>
2024-02-04 23:13:01 +0900

    faet : camera MV

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/camera/CameraViewModel.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/camera/CameraViewModel.kt
@@ -1,4 +1,11 @@
 package com.droidblossom.archive.presentation.ui.camera
 
+import com.droidblossom.archive.domain.model.common.CapsuleMarker
+import kotlinx.coroutines.flow.StateFlow
+
 interface CameraViewModel {
+
+    val capsuleList:StateFlow<List<CapsuleMarker>>
+
+    fun getCapsules(latitude: Double, longitude: Double,): List<CapsuleMarker>
 }
--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/camera/CameraViewModelImpl.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/camera/CameraViewModelImpl.kt
@@ -1,11 +1,38 @@
 package com.droidblossom.archive.presentation.ui.camera
 
+import android.util.Log
+import androidx.lifecycle.viewModelScope
+import com.droidblossom.archive.domain.model.common.CapsuleMarker
+import com.droidblossom.archive.domain.usecase.capsule.NearbyCapsulesUseCase
 import com.droidblossom.archive.presentation.base.BaseViewModel
+import com.droidblossom.archive.presentation.ui.home.HomeFragment
+import com.droidblossom.archive.util.onFail
+import com.droidblossom.archive.util.onSuccess
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CameraViewModelImpl@Inject constructor(
-
+    private val nearbyCapsulesUseCase: NearbyCapsulesUseCase
 ) : BaseViewModel(), CameraViewModel {
+
+    private val _capsuleList = MutableStateFlow(listOf<CapsuleMarker>())
+    override val capsuleList: StateFlow<List<CapsuleMarker>>
+        get() = _capsuleList
+
+    override fun getCapsules(latitude: Double, longitude: Double) : List<CapsuleMarker> {
+        viewModelScope.launch {
+            nearbyCapsulesUseCase(latitude,longitude,1.0,"ALL").collect{ result->
+                result.onSuccess {
+                   _capsuleList.emit(it.capsules)
+                }.onFail {
+
+                }
+            }
+        }
+        return capsuleList.value
+    }
 }
