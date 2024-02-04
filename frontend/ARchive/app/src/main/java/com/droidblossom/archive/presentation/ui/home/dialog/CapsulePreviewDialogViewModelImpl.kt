f84749670686a992e4b5fe857896404143989a99
comst19 <comst19@naver.com>
2024-02-04 18:31:41 +0900

    feat: CapsulePreviewDialogViewModel 생성 후 의존성 주입

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/dialog/CapsulePreviewDialogViewModelImpl.kt
@@ -0,0 +1,11 @@
+package com.droidblossom.archive.presentation.ui.home.dialog
+
+import com.droidblossom.archive.presentation.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CapsulePreviewDialogViewModelImpl @Inject constructor(
+
+): BaseViewModel(), CapsulePreviewDialogViewModel{
+}
