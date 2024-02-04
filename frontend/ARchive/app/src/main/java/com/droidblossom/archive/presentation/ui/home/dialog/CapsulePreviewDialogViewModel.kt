b8253d3ed6745f8fc41ce54884df566fe5a64dcf
comst19 <comst19@naver.com>
2024-02-05 07:33:01 +0900

    feat: UI 구현에 필요한 변수 선언

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/dialog/CapsulePreviewDialogViewModel.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/dialog/CapsulePreviewDialogViewModel.kt
@@ -2,8 +2,16 @@ package com.droidblossom.archive.presentation.ui.home.dialog
 
 import com.droidblossom.archive.domain.model.secret.SecretCapsuleSummary
 import kotlinx.coroutines.flow.StateFlow
+import java.util.Calendar
 
 interface CapsulePreviewDialogViewModel {
 
     val secretCapsuleSummary : StateFlow<SecretCapsuleSummary>
+    val startTime: StateFlow<Calendar?>
+    val endTime: StateFlow<Calendar?>
+    val totalTime: StateFlow<Int?>
+    val initialProgress: StateFlow<Int?>
+    val timerState: StateFlow<String>
+
+    fun calculateCapsuleOpenTime(createdAt: String, dueDate: String)
 }
