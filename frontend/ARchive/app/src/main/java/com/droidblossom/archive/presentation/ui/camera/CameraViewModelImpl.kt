7bc815b4b263b53db76ed8c65de95806bcd3fc08
comst19 <comst19@naver.com>
2024-02-04 19:01:35 +0900

    feat: CameraViewModel 생성

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/camera/CameraViewModelImpl.kt
@@ -0,0 +1,11 @@
+package com.droidblossom.archive.presentation.ui.camera
+
+import com.droidblossom.archive.presentation.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CameraViewModelImpl@Inject constructor(
+
+) : BaseViewModel(), CameraViewModel {
+}
