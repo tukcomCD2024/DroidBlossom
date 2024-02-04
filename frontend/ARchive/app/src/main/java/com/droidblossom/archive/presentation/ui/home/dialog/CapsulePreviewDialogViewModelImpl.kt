a27d366c4c9d757151b9b9de96e7ca393144c569
comst19 <comst19@naver.com>
2024-02-05 07:36:21 +0900

    feat: UI용 Format함수 선언

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/dialog/CapsulePreviewDialogViewModelImpl.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/dialog/CapsulePreviewDialogViewModelImpl.kt
@@ -10,9 +10,14 @@ import com.droidblossom.archive.util.onException
 import com.droidblossom.archive.util.onFail
 import com.droidblossom.archive.util.onSuccess
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 import javax.inject.Inject
 
 @HiltViewModel
@@ -23,12 +28,29 @@ class CapsulePreviewDialogViewModelImpl @Inject constructor(
     private val _secretCapsuleSummary = MutableStateFlow(SecretCapsuleSummary("","","","","",false,""))
     override val secretCapsuleSummary: StateFlow<SecretCapsuleSummary>
         get() = _secretCapsuleSummary
-    fun getSecretCapsuleSummary(capsuleId : Int){
+
+    private val _startTime = MutableStateFlow<Calendar?>(null)
+    private val _endTime = MutableStateFlow<Calendar?>(null)
+
+    override val startTime: StateFlow<Calendar?> = _startTime
+    override val endTime: StateFlow<Calendar?> = _endTime
+
+    private val _totalTime = MutableStateFlow<Int?>(0)
+    override val totalTime: StateFlow<Int?> = _totalTime.asStateFlow()
+
+    private val _initialProgress = MutableStateFlow<Int?>(0)
+    override val initialProgress: StateFlow<Int?> = _initialProgress.asStateFlow()
+
+    private val _timerState = MutableStateFlow("0일 00시간 00분")
+    override val timerState: StateFlow<String> = _timerState.asStateFlow()
+
+    fun getSecretCapsuleSummary(capsuleId: Int) {
         viewModelScope.launch {
-            secretCapsuleSummaryUseCase(capsuleId).collect{result ->
+            secretCapsuleSummaryUseCase(capsuleId).collect { result ->
                 result.onSuccess {
                     _secretCapsuleSummary.emit(it)
-                    Log.d("getSecretCapsuleSummary","$it")
+                    calculateCapsuleOpenTime(it.createdAt, it.dueDate)
+                    Log.d("getSecretCapsuleSummary", "$it")
                 }.onFail {
 
                 }.onException {
@@ -38,7 +60,30 @@ class CapsulePreviewDialogViewModelImpl @Inject constructor(
                 }
             }
         }
+    }
 
+    override fun calculateCapsuleOpenTime(createdAt: String, dueDate: String) {
+        if (dueDate.isEmpty()) return
+        viewModelScope.launch {
+            val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX", Locale.getDefault())
+
+            val startTimeCalendar = Calendar.getInstance().apply {
+                time = dateFormat.parse(createdAt) ?: throw IllegalArgumentException("Invalid createdAt format")
+            }
+
+            val endTimeCalendar = Calendar.getInstance().apply {
+                time = dateFormat.parse(dueDate) ?: throw IllegalArgumentException("Invalid dueDate format")
+            }
+
+            _startTime.emit(startTimeCalendar)
+            _endTime.emit(endTimeCalendar)
+        }
     }
     
+    private fun getTime(millis: Long): String {
+        val hours = (millis / (1000 * 60 * 60)) % 24
+        val minutes = (millis / (1000 * 60)) % 60
+        return String.format("%02d시간 %02d분", hours, minutes)
+    }
+
 }
