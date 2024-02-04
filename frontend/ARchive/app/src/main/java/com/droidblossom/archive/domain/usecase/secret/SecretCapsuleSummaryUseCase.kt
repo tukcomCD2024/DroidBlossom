35814cacc3903aea6d85c7025890c1f24e4602db
comst19 <comst19@naver.com>
2024-02-04 22:11:34 +0900

    feat: SecretCapsuleSummaryUseCase 추가

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/usecase/secret/SecretCapsuleSummaryUseCase.kt
@@ -0,0 +1,30 @@
+package com.droidblossom.archive.domain.usecase.secret
+
+import android.util.Log
+import com.droidblossom.archive.domain.repository.SecretRepository
+import com.droidblossom.archive.util.onException
+import com.droidblossom.archive.util.onFail
+import com.droidblossom.archive.util.onSuccess
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class SecretCapsuleSummaryUseCase @Inject constructor(
+    private val repository : SecretRepository
+) {
+
+    suspend operator fun invoke(capsuleId :Int) =
+        flow {
+            try {
+                emit(repository.getSecretCapsuleSummary(capsuleId).onSuccess {
+
+                }.onFail {
+
+                }.onException {
+                    throw Exception(it)
+                })
+            } catch (e: Exception) {
+                Log.d("예외확인", "$e")
+                e.printStackTrace()
+            }
+        }
+}
