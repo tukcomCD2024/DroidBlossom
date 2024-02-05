212e230e59388e32d2c2043a9834ec189e223cb9
hangunhee39 <ghee0831@naver.com>
2024-02-05 19:37:23 +0900

    feat : 비밀 캡슐 상세 정보 api 연결

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/capsule/CapsuleDetailViewModel.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/capsule/CapsuleDetailViewModel.kt
@@ -1,4 +1,9 @@
 package com.droidblossom.archive.presentation.ui.capsule
 
+import com.droidblossom.archive.domain.model.secret.SecretCapsuleDetail
+import kotlinx.coroutines.flow.StateFlow
+
 interface CapsuleDetailViewModel {
+    val capsuleDetail :StateFlow<SecretCapsuleDetail>
+    fun getSecretCapsuleDetail(id:Long)
 }
--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/capsule/CapsuleDetailViewModelImpl.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/capsule/CapsuleDetailViewModelImpl.kt
@@ -1,11 +1,32 @@
 package com.droidblossom.archive.presentation.ui.capsule
 
+import androidx.lifecycle.viewModelScope
+import com.droidblossom.archive.domain.model.secret.SecretCapsuleDetail
+import com.droidblossom.archive.domain.usecase.secret.SecretCapsuleDetailUseCase
 import com.droidblossom.archive.presentation.base.BaseViewModel
+import com.droidblossom.archive.util.onFail
+import com.droidblossom.archive.util.onSuccess
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class CapsuleDetailViewModelImpl @Inject constructor(
-
+    private val secretCapsuleDetailUseCase: SecretCapsuleDetailUseCase
 ): BaseViewModel(), CapsuleDetailViewModel {
+    private val _capsuleDetail = MutableStateFlow(SecretCapsuleDetail())
+    override val capsuleDetail: StateFlow<SecretCapsuleDetail>
+        get() = _capsuleDetail
 
+    override fun getSecretCapsuleDetail(id: Long) {
+        viewModelScope.launch {
+            secretCapsuleDetailUseCase(id).collect{ result ->
+                result.onSuccess { detail ->
+                    _capsuleDetail.emit(detail)
+                }.onFail {
 
+                }
+            }
+        }
+    }
 }
