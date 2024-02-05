e566937f3464edf55b1da73e5ddb337c627d026b
comst19 <comst19@naver.com>
2024-02-06 03:18:13 +0900

    feat: postCapsuleSkinsApi

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/source/remote/api/CapsuleSkinService.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/source/remote/api/CapsuleSkinService.kt
@@ -2,8 +2,14 @@ package com.droidblossom.archive.data.source.remote.api
 
 import com.droidblossom.archive.data.dto.ResponseBody
 import com.droidblossom.archive.data.dto.capsule_skin.response.CapsuleSkinsPageResponseDto
+import com.droidblossom.archive.data.dto.common.CapsuleSkinSummaryResponseDto
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
 import retrofit2.http.Query
 
 interface CapsuleSkinService {
@@ -13,4 +19,12 @@ interface CapsuleSkinService {
         @Query("size") size : Int,
         @Query("createdAt") createdAt: String
     ) : Response<ResponseBody<CapsuleSkinsPageResponseDto>>
+
+    @Multipart
+    @POST("capsule-skins")
+    suspend fun postCapsuleSkinsApi(
+        @Part("name") name: String,
+        @Part skinImage: MultipartBody.Part,
+        @Part("motionName") motionName: String
+    ): Response<ResponseBody<CapsuleSkinSummaryResponseDto>>
 }
