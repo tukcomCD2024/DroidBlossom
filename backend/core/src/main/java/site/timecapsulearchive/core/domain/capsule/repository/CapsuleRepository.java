7982541579f58c78d4765ce82410f1418518d235
GaBaljaintheroom <junsu1222@naver.com>
2024-02-05 11:13:32 +0900

    fix : repository Transactional 삭제

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/capsule/repository/CapsuleRepository.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/capsule/repository/CapsuleRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 import site.timecapsulearchive.core.domain.capsule.entity.Capsule;
 
 public interface CapsuleRepository extends Repository<Capsule, Long> {
@@ -19,7 +18,6 @@ public interface CapsuleRepository extends Repository<Capsule, Long> {
     );
 
     @Modifying(clearAutomatically = true)
-    @Transactional
     @Query("UPDATE Capsule c SET c.isOpened = true WHERE c.id = :capsuleId and c.member.id = :memberId")
     void updateIsOpenedTrue(
         @Param("memberId") Long memberId,
