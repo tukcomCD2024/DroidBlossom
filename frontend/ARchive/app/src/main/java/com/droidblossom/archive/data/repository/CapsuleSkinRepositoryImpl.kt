41e27a1a82411942fc168f6782a2b8a3cc664658
comst19 <comst19@naver.com>
2024-02-06 03:46:38 +0900

    feat: postCapsuleSkinMake

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/repository/CapsuleSkinRepositoryImpl.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/repository/CapsuleSkinRepositoryImpl.kt
@@ -1,13 +1,19 @@
 package com.droidblossom.archive.data.repository
 
 import com.droidblossom.archive.data.dto.ResponseBody
+import com.droidblossom.archive.data.dto.capsule_skin.request.CapsuleSkinsMakeRequestDto
 import com.droidblossom.archive.data.dto.capsule_skin.request.CapsuleSkinsPageRequestDto
 import com.droidblossom.archive.data.dto.capsule_skin.response.CapsuleSkinsPageResponseDto
+import com.droidblossom.archive.data.dto.common.CapsuleSkinSummaryResponseDto
 import com.droidblossom.archive.data.source.remote.api.CapsuleSkinService
 import com.droidblossom.archive.domain.model.capsule_skin.CapsuleSkinsPageResponse
+import com.droidblossom.archive.domain.model.common.CapsuleSkinSummary
 import com.droidblossom.archive.domain.repository.CapsuleSkinRepository
 import com.droidblossom.archive.util.RetrofitResult
 import com.droidblossom.archive.util.apiHandler
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
 import javax.inject.Inject
 
 class CapsuleSkinRepositoryImpl @Inject constructor(
@@ -16,6 +22,12 @@ class CapsuleSkinRepositoryImpl @Inject constructor(
     
     override suspend fun getCapsuleSkinPage(request: CapsuleSkinsPageRequestDto): RetrofitResult<CapsuleSkinsPageResponse> {
         return apiHandler({ api.getCapsuleSkinsPageApi(request.size, request.createdAt) }) { response: ResponseBody<CapsuleSkinsPageResponseDto> -> response.result.toModel() }
+    }
+
+    override suspend fun postCapsuleSkinMake(request: CapsuleSkinsMakeRequestDto): RetrofitResult<CapsuleSkinSummary> {
+        val fileRequestBody = request.skinImage.asRequestBody("image/jpeg".toMediaType())
+        val skinImagePart = MultipartBody.Part.createFormData("skinImage", request.skinImage.name, fileRequestBody)
+        return apiHandler({api.postCapsuleSkinsApi(request.name, skinImagePart, request.motionName) }) { response: ResponseBody<CapsuleSkinSummaryResponseDto> -> response.result.toModel()}
     }
 
 }
--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/repository/CapsuleSkinRepository.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/repository/CapsuleSkinRepository.kt
@@ -1,10 +1,14 @@
 package com.droidblossom.archive.domain.repository
 
+import com.droidblossom.archive.data.dto.capsule_skin.request.CapsuleSkinsMakeRequestDto
 import com.droidblossom.archive.data.dto.capsule_skin.request.CapsuleSkinsPageRequestDto
 import com.droidblossom.archive.domain.model.capsule_skin.CapsuleSkinsPageResponse
+import com.droidblossom.archive.domain.model.common.CapsuleSkinSummary
 import com.droidblossom.archive.util.RetrofitResult
 
 interface CapsuleSkinRepository {
 
     suspend fun getCapsuleSkinPage(request: CapsuleSkinsPageRequestDto) : RetrofitResult<CapsuleSkinsPageResponse>
+
+    suspend fun postCapsuleSkinMake(request: CapsuleSkinsMakeRequestDto) : RetrofitResult<CapsuleSkinSummary>
 }
