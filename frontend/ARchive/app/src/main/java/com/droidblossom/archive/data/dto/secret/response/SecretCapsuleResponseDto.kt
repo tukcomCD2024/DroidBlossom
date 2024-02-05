2f5f5015598c9049df89935f0d2de0200e05e821
hangunhee39 <ghee0831@naver.com>
2024-02-05 11:27:01 +0900

    feat : SecretCapsuleResponse Dto

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/dto/secret/response/SecretCapsuleResponseDto.kt
@@ -0,0 +1,26 @@
+package com.droidblossom.archive.data.dto.secret.response
+
+import com.droidblossom.archive.domain.model.common.MyCapsule
+
+data class SecretCapsuleResponseDto(
+    val address: String,
+    val capsuleId: Int,
+    val capsuleSkinUrl: String,
+    val content: String,
+    val createdAt: String,
+    val dueDate: String,
+    val images: List<String>,
+    val isOpened: Boolean,
+    val nickname: String,
+    val title: String,
+    val videos: List<String>
+){
+    fun toModel()=MyCapsule(
+        capsuleId = this.capsuleId,
+        capsuleSkinUrl = this.capsuleSkinUrl,
+        createdAt = this.createdAt,
+        dueDate = this.dueDate,
+        isOpened = this.isOpened,
+        title =this.title
+    )
+}
