b05f8f369f7d715ad6732a2a649503031008538f
comst19 <comst19@naver.com>
2024-02-06 04:13:16 +0900

    chore: CapsuleSkinsSearchPageRequest(model,dto)에서 프로퍼티 타입 변경

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/dto/capsule_skin/request/CapsuleSkinsSearchPageRequestDto.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/dto/capsule_skin/request/CapsuleSkinsSearchPageRequestDto.kt
@@ -1,7 +1,7 @@
 package com.droidblossom.archive.data.dto.capsule_skin.request
 
 data class CapsuleSkinsSearchPageRequestDto(
-    val capsule_skin_name : Int,
+    val capsule_skin_name : String,
     val size : Int,
     val capsule_skin_id : Int
 )
--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/model/capsule_skin/CapsuleSkinsSearchPageRequest.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/model/capsule_skin/CapsuleSkinsSearchPageRequest.kt
@@ -3,7 +3,7 @@ package com.droidblossom.archive.domain.model.capsule_skin
 import com.droidblossom.archive.data.dto.capsule_skin.request.CapsuleSkinsSearchPageRequestDto
 
 data class CapsuleSkinsSearchPageRequest(
-    val capsule_skin_name : Int,
+    val capsule_skin_name : String,
     val size : Int,
     val capsule_skin_id : Int
 ){
