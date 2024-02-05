86d7da95ceeeee4b3646cf8d90d64aefe04e55cd
comst19 <comst19@naver.com>
2024-02-06 04:08:28 +0900

    feat: CapsuleSkinsSearchPageRequest(model, dto)

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/dto/capsule_skin/request/CapsuleSkinsSearchPageRequestDto.kt
@@ -0,0 +1,7 @@
+package com.droidblossom.archive.data.dto.capsule_skin.request
+
+data class CapsuleSkinsSearchPageRequestDto(
+    val capsule_skin_name : Int,
+    val size : Int,
+    val capsule_skin_id : Int
+)
