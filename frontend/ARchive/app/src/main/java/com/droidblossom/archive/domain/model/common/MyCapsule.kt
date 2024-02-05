d6b96cfcb659fc706c60340ce06025008eb48b56
hangunhee39 <ghee0831@naver.com>
2024-02-05 11:27:13 +0900

    faet : MyCapsule Model

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/model/common/MyCapsule.kt
@@ -0,0 +1,10 @@
+package com.droidblossom.archive.domain.model.common
+
+data class MyCapsule (
+    val capsuleId: Int,
+    val capsuleSkinUrl: String,
+    val createdAt: String,
+    val dueDate: String,
+    val isOpened: Boolean,
+    val title: String,
+)
