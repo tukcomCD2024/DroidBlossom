25e58afdde4ce05f32e81b4df0cbc676ece244bf
comst19 <comst19@naver.com>
2024-02-06 04:02:06 +0900

    feat: getCapsuleSkinSearchApi

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/source/remote/api/CapsuleSkinService.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/source/remote/api/CapsuleSkinService.kt
@@ -2,6 +2,7 @@ package com.droidblossom.archive.data.source.remote.api
 
 import com.droidblossom.archive.data.dto.ResponseBody
 import com.droidblossom.archive.data.dto.capsule_skin.response.CapsuleSkinsPageResponseDto
+import com.droidblossom.archive.data.dto.capsule_skin.response.CapsuleSkinsSearchPageResponseDto
 import com.droidblossom.archive.data.dto.common.CapsuleSkinSummaryResponseDto
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
@@ -27,4 +28,11 @@ interface CapsuleSkinService {
         @Part skinImage: MultipartBody.Part,
         @Part("motionName") motionName: String
     ): Response<ResponseBody<CapsuleSkinSummaryResponseDto>>
+
+    @GET("capsule-skins/search")
+    suspend fun getCapsuleSkinSearchApi(
+        @Query("capsule_skin_name") capsule_skin_name : String,
+        @Query("size") size : Int,
+        @Query("capsule_skin_id") capsule_skin_id : Int
+    ) : Response<ResponseBody<CapsuleSkinsSearchPageResponseDto>>
 }
