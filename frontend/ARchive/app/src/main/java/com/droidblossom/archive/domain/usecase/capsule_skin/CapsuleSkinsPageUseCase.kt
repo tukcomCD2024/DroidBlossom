47fbff82ed36d1d0990b0eaa30d392c38f7c1d9c
comst19 <comst19@naver.com>
2024-02-06 03:02:35 +0900

    feat: CapsuleSkinsPageUseCase

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/usecase/capsule_skin/CapsuleSkinsPageUseCase.kt
@@ -0,0 +1,31 @@
+package com.droidblossom.archive.domain.usecase.capsule_skin
+
+import android.util.Log
+import com.droidblossom.archive.data.dto.capsule_skin.request.CapsuleSkinsPageRequestDto
+import com.droidblossom.archive.domain.repository.CapsuleSkinRepository
+import com.droidblossom.archive.util.onException
+import com.droidblossom.archive.util.onFail
+import com.droidblossom.archive.util.onSuccess
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class CapsuleSkinsPageUseCase @Inject constructor(
+    private val repository: CapsuleSkinRepository
+){
+
+    suspend operator fun invoke(request: CapsuleSkinsPageRequestDto) =
+        flow{
+            try {
+                emit(repository.getCapsuleSkinPage(request).onSuccess {
+
+                }.onFail {
+
+                }.onException {
+                    throw Exception(it)
+                })
+            }catch (e : Exception){
+                Log.d("예외확인", "$e")
+                e.printStackTrace()
+            }
+        }
+}
