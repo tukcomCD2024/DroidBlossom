f1daa11e61a71e1775c83ebed5d3a68ea91d0912
comst19 <comst19@naver.com>
2024-02-06 02:33:45 +0900

    feat: CapsuleSkinSummary(model, dto)

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/dto/common/CapsuleSkinSummaryResponseDto.kt
@@ -0,0 +1,17 @@
+package com.droidblossom.archive.data.dto.common
+
+import com.droidblossom.archive.domain.model.common.CapsuleSkinSummary
+
+data class CapsuleSkinSummaryResponseDto (
+    val id : Int,
+    val skinUrl : String,
+    val name : String,
+    val createdAt : String
+){
+    fun toModel() = CapsuleSkinSummary(
+        id = this.id,
+        skinUrl = this.skinUrl,
+        name = this.name,
+        createdAt = this.createdAt
+    )
+}
---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/model/common/CapsuleSkinSummary.kt
@@ -0,0 +1,8 @@
+package com.droidblossom.archive.domain.model.common
+
+data class CapsuleSkinSummary (
+    val id : Int,
+    val skinUrl : String,
+    val name : String,
+    val createdAt : String
+)
