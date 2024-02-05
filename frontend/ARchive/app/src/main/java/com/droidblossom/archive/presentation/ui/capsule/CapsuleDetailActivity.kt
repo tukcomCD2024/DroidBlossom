836fa1980bcd8bdaa650c26af0851c3b90559789
hangunhee39 <ghee0831@naver.com>
2024-02-05 19:37:44 +0900

    feat : 상세페이지 데이터 바인딩

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/capsule/CapsuleDetailActivity.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/capsule/CapsuleDetailActivity.kt
@@ -9,6 +9,7 @@ import androidx.databinding.DataBindingUtil.setContentView
 import com.droidblossom.archive.R
 import com.droidblossom.archive.databinding.ActivityCapsuleDetailBinding
 import com.droidblossom.archive.presentation.base.BaseActivity
+import com.droidblossom.archive.presentation.ui.home.HomeFragment
 import com.droidblossom.archive.presentation.ui.home.createcapsule.CreateCapsuleActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -27,8 +28,13 @@ class CapsuleDetailActivity : BaseActivity<CapsuleDetailViewModelImpl, ActivityC
 
     companion object {
         const val CAPSULE_DETAIL = "capsule_detail"
+        const val CAPSULE_ID = "capsule_id"
+        const val CAPSULE_TYPE = "capsule_type"
 
-        fun newIntent(context: Context) =
-            Intent(context, CapsuleDetailActivity::class.java)
+        fun newIntent(context: Context , capsuleId : Long, capsuleType : HomeFragment.CapsuleType) =
+            Intent(context, CapsuleDetailActivity::class.java).apply {
+                putExtra(CAPSULE_ID, capsuleId)
+                putExtra(CAPSULE_TYPE, capsuleType)
+            }
     }
 }
