995acb663d83d560aeeb83081c8650fa1274615b
GaBaljaintheroom <junsu1222@naver.com>
2024-02-06 14:56:43 +0900

    refact : 이미지 확장자 regex 수정

--- backend/core/src/main/java/site/timecapsulearchive/core/global/common/valid/ImageExtensionValidator.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/global/common/valid/ImageExtensionValidator.java
@@ -7,7 +7,7 @@ import site.timecapsulearchive.core.global.common.valid.annotation.Image;
 
 public class ImageExtensionValidator implements ConstraintValidator<Image, String> {
 
-    private static final String AVAILABLE_IMAGE_EXTENSION_REGEX = "^.*\\.jpeg$";
+    private static final String AVAILABLE_IMAGE_EXTENSION_REGEX = "^\\S+\\.jpeg$";
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
