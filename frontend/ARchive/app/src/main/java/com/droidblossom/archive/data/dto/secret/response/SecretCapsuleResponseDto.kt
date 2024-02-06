d6821c83404731e7d2d7ee66482be03cd4e338f5
hangunhee39 <ghee0831@naver.com>
2024-02-06 16:16:47 +0900

    fix : SecretCapsuleResponseDto 변경

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/dto/secret/response/SecretCapsuleResponseDto.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/dto/secret/response/SecretCapsuleResponseDto.kt
@@ -3,23 +3,18 @@ package com.droidblossom.archive.data.dto.secret.response
 import com.droidblossom.archive.domain.model.common.MyCapsule
 
 data class SecretCapsuleResponseDto(
-    val address: String,
     val capsuleId: Long,
-    val capsuleSkinUrl: String,
-    val content: String?,
-    val createdDate: String?,
+    val SkinUrl: String,
+    val createdAt: String,
     val dueDate: String?,
-    val images: List<String>?,
     val isOpened: Boolean,
-    val nickname: String,
     val title: String,
-    val videos: List<String>?,
-    val capsuleType : String,
+    val capsuleType : String?,
 ){
     fun toModel()=MyCapsule(
         capsuleId = this.capsuleId,
-        capsuleSkinUrl = this.capsuleSkinUrl,
-        createdDate = this.createdDate ?: "",
+        capsuleSkinUrl = this.SkinUrl,
+        createdDate = this.createdAt,
         dueDate = this.dueDate,
         isOpened = this.isOpened,
         title =this.title,
