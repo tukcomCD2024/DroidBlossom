3d35d80159c9d5afd96ff0d3c32e4fa355112412
hangunhee39 <ghee0831@naver.com>
2024-02-07 17:27:23 +0900

    feat : fragmentSkin 에서 Skin 페이징으로 받기

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/skin/SkinFragment.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/skin/SkinFragment.kt
@@ -7,33 +7,85 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.droidblossom.archive.R
 import com.droidblossom.archive.databinding.FragmentSkinBinding
 import com.droidblossom.archive.presentation.base.BaseFragment
 import com.droidblossom.archive.presentation.ui.home.HomeFragment
+import com.droidblossom.archive.presentation.ui.skin.adapter.MySkinRVA
 import com.droidblossom.archive.presentation.ui.skin.skinmake.SkinMakeActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class SkinFragment : BaseFragment<SkinViewModelImpl,FragmentSkinBinding>(R.layout.fragment_skin) {
+class SkinFragment : BaseFragment<SkinViewModelImpl, FragmentSkinBinding>(R.layout.fragment_skin) {
     override val viewModel: SkinViewModelImpl by viewModels<SkinViewModelImpl>()
-    override fun observeData() {
 
+    private val mySkinRVA by lazy {
+        MySkinRVA()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.vm = viewModel
-
+        viewModel.getSkinList()
+        initRVA()
         binding.fab.setOnClickListener {
             SkinMakeActivity.goSkinMake(requireContext())
         }
     }
 
-    companion object{
+    private fun initRVA() {
+        binding.skinRV.adapter = mySkinRVA
+        binding.skinRV.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                val lastVisibleItemPosition =
+                    (recyclerView.layoutManager as LinearLayoutManager?)!!.findLastCompletelyVisibleItemPosition()
+                val totalItemViewCount = recyclerView.adapter!!.itemCount - 1
+
+                if (newState == 2 && !recyclerView.canScrollVertically(1)
+                    && lastVisibleItemPosition == totalItemViewCount
+                ) {
+                    viewModel.getSkinList()
+                }
+            }
+        })
+    }
+
+    override fun observeData() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.skinEvents.collect { event ->
+                    when (event) {
+                        is SkinViewModel.SkinEvent.ShowToastMessage -> {
+                            showToastMessage(event.message)
+                        }
+
+                        SkinViewModel.SkinEvent.ToSkinMake -> {
+                            SkinMakeActivity.goSkinMake(requireContext())
+                        }
+                    }
+                }
+            }
+        }
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.skins.collect { skins ->
+                    mySkinRVA.submitList(skins)
+                }
+            }
+        }
+    }
+
+    companion object {
 
         const val TAG = "SKIN"
-        fun newIntent()= SkinFragment()
+        fun newIntent() = SkinFragment()
     }
 
 }
--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/skin/SkinViewModel.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/skin/SkinViewModel.kt
@@ -1,4 +1,25 @@
 package com.droidblossom.archive.presentation.ui.skin
 
+import com.droidblossom.archive.domain.model.common.CapsuleSkinSummary
+import com.droidblossom.archive.presentation.ui.home.createcapsule.CreateCapsuleViewModel
+import com.droidblossom.archive.util.DateUtils
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+
 interface SkinViewModel {
+    val skinEvents : SharedFlow<SkinEvent>
+
+    val skins: StateFlow<List<CapsuleSkinSummary>>
+    val lastCreatedSkinTime: StateFlow<String>
+    val hasNextSkins: StateFlow<Boolean>
+    fun getSkinList()
+    fun goSkinMake()
+
+    sealed class SkinEvent {
+        object ToSkinMake : SkinEvent()
+        data class ShowToastMessage(val message : String) : SkinEvent()
+
+    }
+
 }
