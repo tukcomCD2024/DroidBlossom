2b65bbb7ea4b911ac86bbaf80ecd913559e8c957
hangunhee39 <ghee0831@naver.com>
2024-02-07 17:26:40 +0900

    faet : MySkin RV

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/skin/adapter/MySkinRVA.kt
@@ -0,0 +1,50 @@
+package com.droidblossom.archive.presentation.ui.skin.adapter
+
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.droidblossom.archive.databinding.ItemMySkinBinding
+import com.droidblossom.archive.domain.model.common.CapsuleSkinSummary
+
+class MySkinRVA() : ListAdapter<CapsuleSkinSummary, MySkinRVA.ItemViewHolder>(differ) {
+
+    inner class ItemViewHolder(
+        private val binding: ItemMySkinBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: CapsuleSkinSummary) {
+            binding.item = data
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ItemViewHolder {
+        return ItemViewHolder(
+            ItemMySkinBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        val differ = object : DiffUtil.ItemCallback<CapsuleSkinSummary>() {
+            override fun areItemsTheSame(oldItem: CapsuleSkinSummary, newItem: CapsuleSkinSummary): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: CapsuleSkinSummary, newItem: CapsuleSkinSummary): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
