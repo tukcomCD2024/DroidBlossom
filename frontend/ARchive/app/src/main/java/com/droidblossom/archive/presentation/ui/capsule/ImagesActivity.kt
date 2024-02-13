3464734b4063a3e2d683ced2ca628c283d33f036
hangunhee39 <ghee0831@naver.com>
2024-02-13 13:25:56 +0900

    feat: 이미지 디테일 vp

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/capsule/ImagesActivity.kt
@@ -0,0 +1,64 @@
+package com.droidblossom.archive.presentation.ui.capsule
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil.setContentView
+import androidx.viewpager2.widget.ViewPager2
+import com.droidblossom.archive.R
+import com.droidblossom.archive.databinding.ActivityImagesBinding
+import com.droidblossom.archive.databinding.ActivityVideoBinding
+import com.droidblossom.archive.domain.model.common.ImageUrl
+import com.droidblossom.archive.presentation.ui.capsule.VideoActivity.Companion.VIDEO
+import com.droidblossom.archive.presentation.ui.capsule.adapter.ImageDetailRVA
+import com.droidblossom.archive.presentation.ui.capsule.adapter.ImageUrlRVA
+
+class ImagesActivity : AppCompatActivity() {
+    lateinit var binding: ActivityImagesBinding
+
+    private val imageVP by lazy {
+        ImageDetailRVA()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityImagesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initVP()
+    }
+
+    private fun initVP(){
+        binding.vp.adapter = imageVP
+
+        val images = (intent.getStringArrayExtra(IMAGES) ?: emptyArray()).map { uriString ->
+            ImageUrl(uriString)
+        }
+        imageVP.submitList(images)
+        binding.totalT.text = images.size.toString()
+        binding.backBtn.setOnClickListener {
+            finish()
+        }
+        binding.currentT.text = intent.getIntExtra(CURRENT,1).toString()
+        binding.vp.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            @SuppressLint("SetTextI18n")
+            override fun onPageSelected(position: Int) {
+                binding.currentT.text = "${position + 1}/"
+            }
+        })
+    }
+
+
+    companion object {
+        const val IMAGES = "images"
+        const val CURRENT = "current"
+
+        fun newIntent(context: Context, imageUrls : Array<String> ,current : Int) =
+            Intent(context, ImagesActivity::class.java).apply {
+                putExtra(IMAGES, imageUrls)
+                putExtra(CURRENT, current)
+            }
+    }
+}
