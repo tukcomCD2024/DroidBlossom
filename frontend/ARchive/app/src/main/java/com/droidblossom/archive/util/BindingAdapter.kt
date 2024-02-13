7d5a16f3719d95b922d0972c05897ccdef7967cc
hangunhee39 <ghee0831@naver.com>
2024-02-13 12:17:58 +0900

    feat: 타임캡슐 디자인 변경

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/util/BindingAdapter.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/util/BindingAdapter.kt
@@ -5,18 +5,17 @@ import android.annotation.SuppressLint
 import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.telephony.PhoneNumberFormattingTextWatcher
+import android.view.View
+import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
 import de.hdodenhof.circleimageview.CircleImageView
-import retrofit2.http.Url
-import java.net.URL
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -115,4 +114,11 @@ fun ImageView.setImageResource(resource: Int?) {
     resource?.let {
         this.setImageResource(it)
     }
+}
+
+@BindingAdapter("bind:layout_height_bind")
+fun setLayoutHeight(view: View, height: Float) {
+    val layoutParams: ViewGroup.LayoutParams = view.layoutParams
+    layoutParams.height = height.toInt()
+    view.layoutParams = layoutParams
 }
