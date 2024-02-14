e39102c4b02b3131593eb97706067ed7e8988496
GaBaljaintheroom <junsu1222@naver.com>
2024-02-14 17:49:52 +0900

    feat : 캡슐 스킨 생성 mapper 작성

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/capsuleskin/data/mapper/CapsuleSkinMapper.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/capsuleskin/data/mapper/CapsuleSkinMapper.java
@@ -1,13 +1,23 @@
 package site.timecapsulearchive.core.domain.capsuleskin.data.mapper;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import site.timecapsulearchive.core.domain.capsuleskin.data.dto.CapsuleSkinCreateDto;
+import site.timecapsulearchive.core.domain.capsuleskin.data.dto.CapsuleSkinMessageDto;
 import site.timecapsulearchive.core.domain.capsuleskin.data.dto.CapsuleSkinSummaryDto;
+import site.timecapsulearchive.core.domain.capsuleskin.data.reqeust.CapsuleSkinCreateRequest;
 import site.timecapsulearchive.core.domain.capsuleskin.data.response.CapsuleSkinSummaryResponse;
 import site.timecapsulearchive.core.domain.capsuleskin.data.response.CapsuleSkinsSliceResponse;
+import site.timecapsulearchive.core.domain.capsuleskin.entity.CapsuleSkin;
+import site.timecapsulearchive.core.domain.member.entity.Member;
+import site.timecapsulearchive.core.infra.s3.manager.S3UrlGenerator;
 
 @Component
+@RequiredArgsConstructor
 public class CapsuleSkinMapper {
+
+    private final S3UrlGenerator s3UrlGenerator;
 
     public CapsuleSkinsSliceResponse capsuleSkinSliceToResponse(
         final List<CapsuleSkinSummaryDto> content,
@@ -28,6 +38,39 @@ public class CapsuleSkinMapper {
             .skinUrl(skin.skinUrl())
             .name(skin.name())
             .createdAt(skin.createdAt())
+            .build();
+    }
+
+    public CapsuleSkinCreateDto createRequestToDto(final CapsuleSkinCreateRequest request) {
+        return new CapsuleSkinCreateDto(
+            request.skinName(),
+            request.imageUrl(),
+            request.directory(),
+            request.motionName()
+        );
+    }
+
+    public CapsuleSkin createDtoToEntity(CapsuleSkinCreateDto dto, Member member) {
+        return CapsuleSkin.builder()
+            .skinName(dto.skinName())
+            .imageUrl(
+                s3UrlGenerator.generateFileName(member.getId(), dto.directory(), dto.imageUrl()))
+            .motionName(dto.motionName())
+            .member(member)
+            .build();
+    }
+
+    public CapsuleSkinMessageDto createDtoToMessageDto(
+        final Long memberId,
+        final String memberName,
+        final CapsuleSkinCreateDto dto
+    ) {
+        return CapsuleSkinMessageDto.builder()
+            .memberId(memberId)
+            .memberName(memberName)
+            .skinName(dto.skinName())
+            .imageUrl(dto.imageUrl())
+            .motionName(dto.motionName())
             .build();
     }
 }
