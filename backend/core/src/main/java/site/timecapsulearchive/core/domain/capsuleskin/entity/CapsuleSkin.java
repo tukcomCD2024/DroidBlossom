4b81297d5f500a0224228e8a181e5c1e6f39c6f8
GaBaljaintheroom <junsu1222@naver.com>
2024-02-14 21:04:27 +0900

    refact : 캡슐 스킨 속성 수정

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/capsuleskin/entity/CapsuleSkin.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/capsuleskin/entity/CapsuleSkin.java
@@ -2,6 +2,8 @@ package site.timecapsulearchive.core.domain.capsuleskin.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -34,17 +36,24 @@ public class CapsuleSkin extends BaseEntity {
     private String imageUrl;
 
     @Column(name = "motion_name")
-    private String motionName;
+    @Enumerated(EnumType.STRING)
+    private Motion motionName;
+
+    @Column(name = "retarget")
+    @Enumerated(EnumType.STRING)
+    private Retarget retarget;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
     @Builder
-    private CapsuleSkin(String skinName, String imageUrl, String motionName, Member member) {
+    private CapsuleSkin(String skinName, String imageUrl, Motion motionName, Member member,
+        Retarget retarget) {
         this.skinName = skinName;
         this.imageUrl = imageUrl;
         this.motionName = motionName;
         this.member = member;
+        this.retarget = retarget;
     }
 }
