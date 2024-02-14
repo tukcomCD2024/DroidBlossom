22dc1bec6ba1fdceb83450368a7417e28e30301d
hong seokho <seokho7799@gmail.com>
2024-02-15 08:59:24 +0900

    feat : 데이터베이스 모델, Enum 추가
    - Member, CapsuleSkin entity 추가
    - Retarget, Motion Enum 추가

---
+++ backend/AnimatedDrawings/model/retarget.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class Retarget(Enum):
+    CMU = 'examples/config/retarget/cmu1_pfp.yaml'
+    FAIR = 'examples/config/retarget/fair1_ppf.yaml'
+    ROKOKO = 'examples/config/retarget/mixamo_fff.yaml'
