84a6a28d7d40fa994c6b6a0ca6cfc966a0052ca1
GaBaljaintheroom <junsu1222@naver.com>
2024-02-14 17:52:49 +0900

    feat : 캡슐 스킨 생성 Service 작성

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/capsuleskin/service/CapsuleSkinService.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/capsuleskin/service/CapsuleSkinService.java
@@ -1,20 +1,34 @@
 package site.timecapsulearchive.core.domain.capsuleskin.service;
 
+
 import java.time.ZonedDateTime;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
+import site.timecapsulearchive.core.domain.capsuleskin.data.dto.CapsuleSkinCreateDto;
 import site.timecapsulearchive.core.domain.capsuleskin.data.dto.CapsuleSkinSummaryDto;
 import site.timecapsulearchive.core.domain.capsuleskin.data.mapper.CapsuleSkinMapper;
+import site.timecapsulearchive.core.domain.capsuleskin.data.response.CapsuleSkinStatusResponse;
 import site.timecapsulearchive.core.domain.capsuleskin.data.response.CapsuleSkinsSliceResponse;
+import site.timecapsulearchive.core.domain.capsuleskin.entity.CapsuleSkin;
 import site.timecapsulearchive.core.domain.capsuleskin.repository.CapsuleSkinQueryRepository;
+import site.timecapsulearchive.core.domain.capsuleskin.repository.CapsuleSkinRepository;
+import site.timecapsulearchive.core.domain.member.entity.Member;
+import site.timecapsulearchive.core.domain.member.exception.MemberNotFoundException;
+import site.timecapsulearchive.core.domain.member.repository.MemberRepository;
+import site.timecapsulearchive.core.global.config.RabbitmqConfig;
 
 @Service
 @RequiredArgsConstructor
 public class CapsuleSkinService {
 
+    private final CapsuleSkinRepository capsuleSkinRepository;
     private final CapsuleSkinQueryRepository capsuleSkinQueryRepository;
+    private final MemberRepository memberRepository;
     private final CapsuleSkinMapper capsuleSkinMapper;
+    private final RabbitmqConfig rabbitmqConfig;
+    private final RabbitTemplate rabbitTemplate;
 
     public CapsuleSkinsSliceResponse findCapsuleSkinSliceByCreatedAtAndMemberId(
         final Long memberId,
@@ -28,5 +42,31 @@ public class CapsuleSkinService {
         );
 
         return capsuleSkinMapper.capsuleSkinSliceToResponse(slice.getContent(), slice.hasNext());
+    }
+
+    public CapsuleSkinStatusResponse sendCapsuleSkinCreateMessage(
+        final Long memberId,
+        final CapsuleSkinCreateDto dto
+    ) {
+        Member foundMember = memberRepository.findMemberById(memberId)
+            .orElseThrow(MemberNotFoundException::new);
+
+        if (isNotExistMotionName(dto)) {
+            CapsuleSkin capsuleSkin = capsuleSkinMapper.createDtoToEntity(dto, foundMember);
+
+            capsuleSkinRepository.save(capsuleSkin);
+            return CapsuleSkinStatusResponse.success();
+        }
+
+        rabbitTemplate.convertAndSend(
+            rabbitmqConfig.getCapsuleSkinExchangeName(),
+            rabbitmqConfig.getRoutingKey(),
+            capsuleSkinMapper.createDtoToMessageDto(memberId, foundMember.getNickname(), dto)
+        );
+        return CapsuleSkinStatusResponse.sendMessage();
+    }
+
+    private boolean isNotExistMotionName(CapsuleSkinCreateDto dto) {
+        return dto.motionName() == null || dto.motionName().isEmpty();
     }
 }
