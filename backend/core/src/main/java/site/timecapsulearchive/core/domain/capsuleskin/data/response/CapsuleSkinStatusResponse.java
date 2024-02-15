bd9e3e9a06e170a68021b89d42fe7b0d855a598d
GaBaljaintheroom <junsu1222@naver.com>
2024-02-15 10:59:11 +0900

    refact : 응답 상태 필드 추가

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/capsuleskin/data/response/CapsuleSkinStatusResponse.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/capsuleskin/data/response/CapsuleSkinStatusResponse.java
@@ -1,14 +1,17 @@
 package site.timecapsulearchive.core.domain.capsuleskin.data.response;
 
+import org.springframework.http.HttpStatus;
+
 public record CapsuleSkinStatusResponse(
+    HttpStatus status,
     String result
 ) {
 
     public static CapsuleSkinStatusResponse success() {
-        return new CapsuleSkinStatusResponse("캡슐 스킨 생성 성공!");
+        return new CapsuleSkinStatusResponse(HttpStatus.OK ,"캡슐 스킨 생성 성공!");
     }
 
     public static CapsuleSkinStatusResponse sendMessage() {
-        return new CapsuleSkinStatusResponse("캡슐 스킨 메세지 전송 성공!");
+        return new CapsuleSkinStatusResponse(HttpStatus.ACCEPTED, "캡슐 스킨 메세지 전송 성공!");
     }
 }
