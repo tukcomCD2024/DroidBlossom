d1b62279d65805966a69b65c51fe84b9de059679
hong seokho <seokho7799@gmail.com>
2024-02-18 19:09:49 +0900

    fix : 경로 수정
    - config들 경로 수정

--- backend/AnimatedDrawings/application/model/retarget.py
+++ backend/AnimatedDrawings/application/model/retarget.py
@@ -2,6 +2,6 @@ from enum import Enum
 
 
 class Retarget(Enum):
-    CMU = 'examples/config/retarget/cmu1_pfp.yaml'
-    FAIR = 'examples/config/retarget/fair1_ppf.yaml'
-    ROKOKO = 'examples/config/retarget/mixamo_fff.yaml'
+    CMU = '../examples/config/retarget/cmu1_pfp.yaml'
+    FAIR = '../examples/config/retarget/fair1_ppf.yaml'
+    ROKOKO = '../examples/config/retarget/mixamo_fff.yaml'
