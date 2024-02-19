e100f5485e1d9be9f2a20951172def1952eafecd
comst19 <comst19@naver.com>
2024-02-19 21:50:10 +0900

    refact: CapsuleSkinRepository 수정

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/repository/CapsuleSkinRepository.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/repository/CapsuleSkinRepository.kt
@@ -3,6 +3,7 @@ package com.droidblossom.archive.domain.repository
 import com.droidblossom.archive.data.dto.capsule_skin.request.CapsuleSkinsMakeRequestDto
 import com.droidblossom.archive.data.dto.capsule_skin.request.CapsuleSkinsPageRequestDto
 import com.droidblossom.archive.data.dto.capsule_skin.request.CapsuleSkinsSearchPageRequestDto
+import com.droidblossom.archive.domain.model.capsule_skin.CapsuleSkinsMakeResponse
 import com.droidblossom.archive.domain.model.capsule_skin.CapsuleSkinsPageResponse
 import com.droidblossom.archive.domain.model.capsule_skin.CapsuleSkinsSearchPageResponse
 import com.droidblossom.archive.domain.model.common.CapsuleSkinSummary
@@ -12,7 +13,7 @@ interface CapsuleSkinRepository {
 
     suspend fun getCapsuleSkinPage(request: CapsuleSkinsPageRequestDto) : RetrofitResult<CapsuleSkinsPageResponse>
 
-    suspend fun postCapsuleSkinMake(request: CapsuleSkinsMakeRequestDto) : RetrofitResult<CapsuleSkinSummary>
+    suspend fun postCapsuleSkinMake(request: CapsuleSkinsMakeRequestDto) : RetrofitResult<CapsuleSkinsMakeResponse>
 
     suspend fun deleteCapsuleSkin(capsuleSKinId : Long ) : RetrofitResult<String>
 
