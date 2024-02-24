655073e52ed2104f94415cc89175aee604a3bb78
GaBaljaintheroom <junsu1222@naver.com>
2024-02-24 09:51:40 +0900

    feat : 알림 전송 쿼리 생성

--- backend/notification/src/main/java/site/timecapsulearchive/notification/repository/NotificationRepository.java
+++ backend/notification/src/main/java/site/timecapsulearchive/notification/repository/NotificationRepository.java
@@ -3,6 +3,7 @@ package site.timecapsulearchive.notification.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.timecapsulearchive.notification.entity.Notification;
 
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
+public interface NotificationRepository extends Repository<Notification, Long> {
 
+    void save(Notification entity);
 }
