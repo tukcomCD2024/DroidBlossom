461f40a3b98f310c5488bc9cbbfad14aaca14e9a
hangunhee39 <ghee0831@naver.com>
2024-02-25 17:08:06 +0900

    feat : 알림 리스트 가져오기 api repository 추가

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/repository/MemberRepositoryImpl.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/repository/MemberRepositoryImpl.kt
@@ -8,10 +8,12 @@ import com.droidblossom.archive.data.dto.member.request.MemberStatusRequestDto
 import com.droidblossom.archive.data.dto.member.request.NotificationEnabledRequestDto
 import com.droidblossom.archive.data.dto.member.response.MemberDetailResponseDto
 import com.droidblossom.archive.data.dto.member.response.MemberStatusResponseDto
+import com.droidblossom.archive.data.dto.member.response.NotificationResponseDto
 import com.droidblossom.archive.data.source.remote.api.MemberService
 import com.droidblossom.archive.domain.model.auth.Health
 import com.droidblossom.archive.domain.model.member.MemberDetail
 import com.droidblossom.archive.domain.model.member.MemberStatus
+import com.droidblossom.archive.domain.model.member.NotificationPage
 import com.droidblossom.archive.domain.repository.MemberRepository
 import com.droidblossom.archive.util.RetrofitResult
 import com.droidblossom.archive.util.apiHandler
@@ -25,18 +27,32 @@ class MemberRepositoryImpl @Inject constructor(
     }
 
     override suspend fun postMemberStatus(request: MemberStatusRequestDto): RetrofitResult<MemberStatus> {
-        return apiHandler({ api.postMeStatusApi(request) }) { response : ResponseBody<MemberStatusResponseDto> -> response.result.toModel() }
+        return apiHandler({ api.postMeStatusApi(request) }) { response: ResponseBody<MemberStatusResponseDto> -> response.result.toModel() }
     }
 
     override suspend fun patchNotificationEnabled(request: NotificationEnabledRequestDto): RetrofitResult<String> {
-        return apiHandler({api.patchNotificationEnabled(request)}){response : ResponseBody<String> -> response.result.toModel()}
+        return apiHandler({ api.patchNotificationEnabled(request) }) { response: ResponseBody<String> -> response.result.toModel() }
     }
 
     override suspend fun patchFcmToken(request: FcmTokenRequsetDto): RetrofitResult<String> {
-        return apiHandler({api.patchFcmToken(request)}){response : ResponseBody<String> -> response.result.toModel()}
+        return apiHandler({ api.patchFcmToken(request) }) { response: ResponseBody<String> -> response.result.toModel() }
+    }
+
+    override suspend fun getNotifications(
+        size: Int,
+        createdAt: String
+    ): RetrofitResult<NotificationPage> {
+        return apiHandler({
+            api.getNotifications(
+                size = size,
+                createdAt = createdAt
+            )
+        }) { response: ResponseBody<NotificationResponseDto> -> response.result.toModel() }
     }
 
     override suspend fun getText(): RetrofitResult<Health> {
         return apiHandler({ api.getTextApi() }) { response: ResponseBody<HealthResponseDto> -> response.result.toModel() }
     }
+
+
 }
--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/repository/MemberRepository.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/repository/MemberRepository.kt
@@ -3,9 +3,11 @@ package com.droidblossom.archive.domain.repository
 import com.droidblossom.archive.data.dto.member.request.FcmTokenRequsetDto
 import com.droidblossom.archive.data.dto.member.request.MemberStatusRequestDto
 import com.droidblossom.archive.data.dto.member.request.NotificationEnabledRequestDto
+import com.droidblossom.archive.data.dto.member.response.NotificationResponseDto
 import com.droidblossom.archive.domain.model.auth.Health
 import com.droidblossom.archive.domain.model.member.MemberDetail
 import com.droidblossom.archive.domain.model.member.MemberStatus
+import com.droidblossom.archive.domain.model.member.NotificationPage
 import com.droidblossom.archive.util.RetrofitResult
 
 interface MemberRepository {
@@ -17,6 +19,8 @@ interface MemberRepository {
     suspend fun patchNotificationEnabled(request: NotificationEnabledRequestDto): RetrofitResult<String>
 
     suspend fun patchFcmToken(request: FcmTokenRequsetDto): RetrofitResult<String>
+
+    suspend fun getNotifications(size:Int, createdAt: String) : RetrofitResult<NotificationPage>
 
     suspend fun getText() : RetrofitResult<Health>
 }
