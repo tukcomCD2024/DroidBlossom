5c2eef43ab98d634e34c3fca76e5db31d99c36b2
hong seokho <seokho7799@gmail.com>
2024-03-11 19:11:15 +0900

    fix : 알림 전송 url 수정

--- backend/core/src/main/java/site/timecapsulearchive/core/infra/notification/data/mapper/NotificationMapper.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/infra/notification/data/mapper/NotificationMapper.java
@@ -1,21 +1,30 @@
 package site.timecapsulearchive.core.infra.notification.data.mapper;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import site.timecapsulearchive.core.domain.capsuleskin.data.dto.CapsuleSkinCreateDto;
 import site.timecapsulearchive.core.infra.notification.data.dto.request.CreatedCapsuleSkinNotificationRequest;
+import site.timecapsulearchive.core.infra.s3.manager.S3PreSignedUrlManager;
+import site.timecapsulearchive.core.infra.s3.manager.S3UrlGenerator;
 
 @Component
+@RequiredArgsConstructor
 public class NotificationMapper {
 
+    private final S3PreSignedUrlManager s3PreSignedUrlManager;
+    private final S3UrlGenerator s3UrlGenerator;
 
-    public CreatedCapsuleSkinNotificationRequest toRequest(Long memberId, CapsuleSkinCreateDto dto) {
+    public CreatedCapsuleSkinNotificationRequest toRequest(Long memberId,
+        CapsuleSkinCreateDto dto) {
         return new CreatedCapsuleSkinNotificationRequest(
             memberId,
             "SUCCESS_MAKE_CAPSULE_SKIN",
             dto.skinName(),
-            "캡슐, 스킨 생성이 완료되었습니다",
+            "캡슐 스킨 생성이 완료되었습니다",
             dto.skinName() + "이 생성되었습니다. ARchive에서 확인해보세요!",
-            dto.imageUrl()
+            s3PreSignedUrlManager.getS3PreSignedUrlForGet(
+                s3UrlGenerator.generateFileName(memberId, dto.directory(), dto.skinName())
+            )
         );
     }
 }
