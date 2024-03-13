b1f7184efcfe6a6eb8039490aba2403f23ba2335
GaBaljaintheroom <junsu1222@naver.com>
2024-03-13 20:53:52 +0900

    feat: 친구 요청 알림 전송 기능 구현

--- backend/core/src/main/java/site/timecapsulearchive/core/infra/notification/manager/NotificationManager.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/infra/notification/manager/NotificationManager.java
@@ -1,6 +1,5 @@
 package site.timecapsulearchive.core.infra.notification.manager;
 
-import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -11,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 import site.timecapsulearchive.core.domain.capsuleskin.data.dto.CapsuleSkinCreateDto;
 import site.timecapsulearchive.core.global.error.ErrorCode;
 import site.timecapsulearchive.core.infra.notification.data.dto.request.CreatedCapsuleSkinNotificationRequest;
+import site.timecapsulearchive.core.infra.notification.data.dto.request.FriendReqNotificationRequest;
 import site.timecapsulearchive.core.infra.notification.data.mapper.NotificationMapper;
 import site.timecapsulearchive.core.infra.sms.exception.ExternalApiException;
 
@@ -19,22 +19,35 @@ import site.timecapsulearchive.core.infra.sms.exception.ExternalApiException;
 @RequiredArgsConstructor
 public class NotificationManager {
 
-    private static final String NOTIFICATION_SERVER_URL = "https://notification.archive-timecapsule.kro.kr/api/notification/capsule_skin/send";
     private final RestTemplate restTemplate;
     private final NotificationMapper notificationMapper;
 
-    public void sendCreatedSkinMessage(
-        final Long memberId,
-        final CapsuleSkinCreateDto dto
-    ) {
-        final CreatedCapsuleSkinNotificationRequest request = notificationMapper.toRequest(memberId, dto);
+    public void sendCreatedSkinMessage(final Long memberId, final CapsuleSkinCreateDto dto) {
+        final CreatedCapsuleSkinNotificationRequest request = notificationMapper.capsuleSkinDtoToMessage(memberId, dto);
 
         try {
             final HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
 
             restTemplate.postForEntity(
-                NOTIFICATION_SERVER_URL,
+                NotificationUrl.CAPSULE_SKIN_ALARM_URL.getUrl(),
+                new HttpEntity<>(request, headers),
+                Void.class
+            );
+        } catch (HttpClientErrorException e) {
+            throw new ExternalApiException(ErrorCode.EXTERNAL_API_ERROR);
+        }
+    }
+
+    public void sendFriendReqMessage(final Long friendId, final String ownerNickname) {
+        final FriendReqNotificationRequest request = notificationMapper.friendReqToMessage(friendId, ownerNickname);
+
+        try {
+            final HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            restTemplate.postForEntity(
+                NotificationUrl.FRIEND_REQ_ALARM_URL.getUrl(),
                 new HttpEntity<>(request, headers),
                 Void.class
             );
