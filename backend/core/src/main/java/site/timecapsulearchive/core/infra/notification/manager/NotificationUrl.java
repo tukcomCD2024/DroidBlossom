10f6fdfef9596848d28711c06552fdfaf0d250c2
GaBaljaintheroom <junsu1222@naver.com>
2024-03-13 20:52:54 +0900

    feat: url enum 타입 작성

---
+++ backend/core/src/main/java/site/timecapsulearchive/core/infra/notification/manager/NotificationUrl.java
@@ -0,0 +1,16 @@
+package site.timecapsulearchive.core.infra.notification.manager;
+
+public enum NotificationUrl {
+    CAPSULE_SKIN_ALARM_URL("https://notification.archive-timecapsule.kro.kr/api/notification/capsule_skin/send"),
+    FRIEND_REQ_ALARM_URL("https://notification.archive-timecapsule.kro.kr/api/notification/friend_req/send");
+
+    private String url;
+
+    NotificationUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+}
