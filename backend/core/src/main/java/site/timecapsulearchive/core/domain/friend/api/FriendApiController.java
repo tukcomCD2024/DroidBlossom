0ae3419067d4f6e2a8116c0f4459fb854a49efff
hong seokho <seokho7799@gmail.com>
2024-03-16 10:41:39 +0900

    fix : GetMapping 수정

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/friend/api/FriendApiController.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/friend/api/FriendApiController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,6 +44,7 @@ public class FriendApiController implements FriendApi {
         );
     }
 
+    @GetMapping
     @Override
     public ResponseEntity<ApiSpec<FriendsSliceResponse>> findFriends(
         @AuthenticationPrincipal final Long memberId,
