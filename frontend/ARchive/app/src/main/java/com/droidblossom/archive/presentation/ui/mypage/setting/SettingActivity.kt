f29b9bab01215e94c1ec5367092a6e54e2a9e002
hangunhee39 <ghee0831@naver.com>
2024-03-18 11:38:37 +0900

    feat: init 세팅엑티비티

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/mypage/setting/SettingActivity.kt
@@ -0,0 +1,12 @@
+package com.droidblossom.archive.presentation.ui.mypage.setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.droidblossom.archive.R
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
