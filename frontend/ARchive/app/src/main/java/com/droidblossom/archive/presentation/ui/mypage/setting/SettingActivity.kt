8f19a2b6b239b662f87d20bb242d22cf0d77854e
hangunhee39 <ghee0831@naver.com>
2024-03-18 12:00:13 +0900

    feat: 세팅 엑티비티 viewModel. databinding

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/mypage/setting/SettingActivity.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/mypage/setting/SettingActivity.kt
@@ -2,11 +2,27 @@ package com.droidblossom.archive.presentation.ui.mypage.setting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil.setContentView
 import com.droidblossom.archive.R
+import com.droidblossom.archive.databinding.ActivitySettingBinding
+import com.droidblossom.archive.databinding.FragmentMyPageBinding
+import com.droidblossom.archive.presentation.base.BaseActivity
+import com.droidblossom.archive.presentation.base.BaseFragment
+import com.droidblossom.archive.presentation.ui.mypage.MyPageViewModelImpl
+import dagger.hilt.android.AndroidEntryPoint
 
-class SettingActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class SettingActivity :  BaseActivity<SettingViewModelImpl, ActivitySettingBinding>(R.layout.activity_setting) {
+    override val viewModel: SettingViewModelImpl by viewModels()
+    override fun observeData() {}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting)
+        initView()
+    }
+
+    private fun initView(){
+
     }
 }
---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/mypage/setting/SettingViewModelImpl.kt
@@ -0,0 +1,12 @@
+package com.droidblossom.archive.presentation.ui.mypage.setting
+
+import com.droidblossom.archive.presentation.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingViewModelImpl @Inject constructor(
+
+) : BaseViewModel(), SettingViewModel {
+
+}
