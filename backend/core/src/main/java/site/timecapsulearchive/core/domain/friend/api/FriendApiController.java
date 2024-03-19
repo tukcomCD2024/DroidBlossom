47a4bcd1e1718a4961572dc77c471a671e27d89e
GaBaljaintheroom <junsu1222@naver.com>
2024-03-19 13:57:48 +0900

    feat: 친구 태그 검색 Controller 작성

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/friend/api/FriendApiController.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/friend/api/FriendApiController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import site.timecapsulearchive.core.domain.friend.data.reqeust.SearchFriendsRequest;
 import site.timecapsulearchive.core.domain.friend.data.response.FriendReqStatusResponse;
 import site.timecapsulearchive.core.domain.friend.data.response.FriendRequestsSliceResponse;
+import site.timecapsulearchive.core.domain.friend.data.response.FriendSearchResponse;
 import site.timecapsulearchive.core.domain.friend.data.response.FriendsSliceResponse;
 import site.timecapsulearchive.core.domain.friend.data.response.SearchFriendsResponse;
 import site.timecapsulearchive.core.domain.friend.service.FriendService;
@@ -109,5 +110,20 @@ public class FriendApiController implements FriendApi {
     public ResponseEntity<SearchFriendsResponse> searchMembersByPhones(
         SearchFriendsRequest request) {
         return null;
+    }
+
+    @PostMapping(value = "/search")
+    @Override
+    public ResponseEntity<ApiSpec<FriendSearchResponse>> searchFriendByTag(
+        @AuthenticationPrincipal Long memberId,
+        @RequestParam(value = "friend-tag") final String tag
+    ) {
+        return ResponseEntity.ok()
+            .body(
+                ApiSpec.success(
+                    SuccessCode.SUCCESS,
+                    friendService.searchFriend(memberId, tag)
+                )
+            );
     }
 }
