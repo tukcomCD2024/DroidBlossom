3f566b95052b04c0b5c03ee66675e4ce0d3286f0
hangunhee39 <ghee0831@naver.com>
2024-03-19 23:16:29 +0900

    faet : commont fragment

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/dialog/CommonDialogFragment.kt
@@ -0,0 +1,56 @@
+package com.droidblossom.archive.presentation.dialog
+
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import com.droidblossom.archive.R
+import com.droidblossom.archive.databinding.FragmentCommonDialogBinding
+import com.droidblossom.archive.presentation.base.BaseDialogFragment
+
+class CommonDialogFragment(
+    private val onClick: () -> Unit
+) : BaseDialogFragment<FragmentCommonDialogBinding>(R.layout.fragment_common_dialog) {
+
+    override fun onStart() {
+        super.onStart()
+        val dialog = dialog
+        if (dialog != null) {
+            val width = ViewGroup.LayoutParams.MATCH_PARENT
+            val height = ViewGroup.LayoutParams.WRAP_CONTENT
+            dialog.window?.setLayout(width, height)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.messageT.text = arguments?.getString("title") ?: ""
+        binding.rightBtn.text = arguments?.getString("rightBtnT") ?: "확인"
+
+        binding.leftBtn.setOnClickListener {
+            this.dismiss()
+        }
+
+        binding.rightBtn.setOnClickListener {
+            onClick()
+            this.dismiss()
+        }
+    }
+
+    companion object {
+
+        fun newIntent(
+            title: String,
+            rightBtnT: String,
+            onRightClick: () -> Unit
+        ): CommonDialogFragment {
+            val args = Bundle().apply {
+                putString("title", title)
+                putString("rightBtnT", rightBtnT)
+            }
+            return CommonDialogFragment(onRightClick).apply {
+                arguments = args
+            }
+        }
+    }
+}
