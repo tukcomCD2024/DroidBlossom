fdc4d68642c5071fa64505b173f811bc8fcdce34
hangunhee39 <ghee0831@naver.com>
2024-03-19 23:16:45 +0900

    feat : 로그아웃 구현

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/mypage/setting/SettingMainFragment.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/mypage/setting/SettingMainFragment.kt
@@ -2,6 +2,7 @@ package com.droidblossom.archive.presentation.ui.mypage.setting
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -10,12 +11,25 @@ import androidx.lifecycle.repeatOnLifecycle
 import com.droidblossom.archive.R
 import com.droidblossom.archive.databinding.FragmentSettingMainBinding
 import com.droidblossom.archive.presentation.base.BaseFragment
+import com.droidblossom.archive.presentation.dialog.CommonDialogFragment
+import com.droidblossom.archive.presentation.ui.auth.AuthActivity
+import com.droidblossom.archive.presentation.ui.home.dialog.CapsulePreviewDialogFragment
+import com.droidblossom.archive.util.DataStoreUtils
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SettingMainFragment :
     BaseFragment<SettingViewModelImpl, FragmentSettingMainBinding>(R.layout.fragment_setting_main) {
+
     override val viewModel: SettingViewModelImpl by viewModels()
+
+    @Inject
+    lateinit var dataStoreUtils: DataStoreUtils
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -45,7 +59,13 @@ class SettingMainFragment :
                         }
 
                         SettingViewModel.SettingMainEvent.GoLogout -> {
-
+                            val sheet = CommonDialogFragment.newIntent("정말 로그아웃 하시겠습니까?","로그아웃") {
+                                CoroutineScope(Dispatchers.IO).launch {
+                                    dataStoreUtils.resetTokenData()
+                                }
+                                AuthActivity.goAuth(requireContext())
+                            }
+                            sheet.show(parentFragmentManager, "logoutDialog")
                         }
 
                         SettingViewModel.SettingMainEvent.GoNotice -> {
