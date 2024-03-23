33d27e71839aaab4450261a26619118ae427636f
hong seokho <seokho7799@gmail.com>
2024-03-23 20:32:28 +0900

    feat : validation 추가

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/auth/data/request/EmailSignInRequest.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/auth/data/request/EmailSignInRequest.java
@@ -2,6 +2,7 @@ package site.timecapsulearchive.core.domain.auth.data.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "이메일 로그인 요청")
 public record EmailSignInRequest(
@@ -11,6 +12,7 @@ public record EmailSignInRequest(
     String email,
 
     @Schema(description = "비밀번호")
+    @NotNull
     String password
 ) {
 
--- backend/core/src/main/java/site/timecapsulearchive/core/domain/auth/data/request/EmailSignUpRequest.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/auth/data/request/EmailSignUpRequest.java
@@ -2,6 +2,7 @@ package site.timecapsulearchive.core.domain.auth.data.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "이메일 회원가입 요청")
 public record EmailSignUpRequest(
@@ -11,6 +12,7 @@ public record EmailSignUpRequest(
     String email,
 
     @Schema(description = "사용할 비밀번호")
+    @NotNull
     String password
 ) {
 
