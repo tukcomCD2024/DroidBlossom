9eb1edda5905126564e13ca25f91469d471b817a
hong seokho <seokho7799@gmail.com>
2024-03-23 23:56:00 +0900

    fix : NotBlank validation 추가

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/auth/data/request/EmailSignUpRequest.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/auth/data/request/EmailSignUpRequest.java
@@ -2,17 +2,18 @@ package site.timecapsulearchive.core.domain.auth.data.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 
 @Schema(description = "이메일 회원가입 요청")
 public record EmailSignUpRequest(
 
     @Schema(description = "사용할 이메일")
+    @NotBlank
     @Email
     String email,
 
     @Schema(description = "사용할 비밀번호")
-    @NotNull
+    @NotBlank
     String password
 ) {
 
