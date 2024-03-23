19ae27899807881c64266b47466d345690b35973
hong seokho <seokho7799@gmail.com>
2024-03-23 21:26:34 +0900

    feat : 중복 체크 쿼리 테스트 추가

---
+++ backend/core/src/test/java/site/timecapsulearchive/core/domain/member/repository/MemberQueryRepositoryTest.java
@@ -0,0 +1,66 @@
+package site.timecapsulearchive.core.domain.member.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.context.TestConstructor.AutowireMode;
+import org.springframework.transaction.annotation.Transactional;
+import site.timecapsulearchive.core.common.RepositoryTest;
+import site.timecapsulearchive.core.domain.member.entity.Member;
+import site.timecapsulearchive.core.domain.member.entity.SocialType;
+
+@TestConstructor(autowireMode = AutowireMode.ALL)
+class MemberQueryRepositoryTest extends RepositoryTest {
+
+    private final MemberQueryRepository memberQueryRepository;
+
+    MemberQueryRepositoryTest(EntityManager entityManager) {
+        this.memberQueryRepository = new MemberQueryRepository(
+            new JPAQueryFactory(entityManager));
+    }
+
+    @Transactional
+    @BeforeEach
+    void setup(@Autowired EntityManager entityManager) {
+        Member owner = getMember();
+        entityManager.persist(owner);
+    }
+
+    private Member getMember() {
+        return Member.builder()
+            .socialType(SocialType.GOOGLE)
+            .email(0 + "test@google.com")
+            .authId(0 + "test")
+            .profileUrl(0 + "test.com")
+            .build();
+    }
+
+    @Test
+    void 중복_이메일로_중복_체크_테스트() {
+        //given
+        String duplicatedEmail = "0test@google.com";
+
+        //when
+        Boolean isDuplicated = memberQueryRepository.checkEmailDuplication(duplicatedEmail);
+
+        //then
+        assertThat(isDuplicated).isTrue();
+    }
+
+    @Test
+    void 고유한_이메일로_중복_체크_테스트() {
+        //given
+        String uniqueEmail = "unique@google.com";
+
+        //when
+        Boolean isDuplicated = memberQueryRepository.checkEmailDuplication(uniqueEmail);
+
+        //then
+        assertThat(isDuplicated).isFalse();
+    }
+}
