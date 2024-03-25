302ef929528eec72b31f021f5dafb895339e3568
hangunhee39 <ghee0831@naver.com>
2024-03-26 04:25:48 +0900

    feat: 공지사항 완

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/mypage/setting/page/SettingNoticeFragment.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/mypage/setting/page/SettingNoticeFragment.kt
@@ -7,8 +7,11 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.droidblossom.archive.R
 import com.droidblossom.archive.databinding.FragmentSettingNoticeBinding
+import com.droidblossom.archive.domain.model.setting.Notice
+import com.droidblossom.archive.domain.model.setting.NoticeContent
 import com.droidblossom.archive.presentation.base.BaseFragment
 import com.droidblossom.archive.presentation.ui.mypage.setting.SettingViewModelImpl
+import com.droidblossom.archive.presentation.ui.mypage.setting.adapter.NoticeRVA
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,6 +21,10 @@ class SettingNoticeFragment :
     override val viewModel: SettingViewModelImpl by viewModels()
     lateinit var navController: NavController
 
+    private val noticeAdapter by lazy {
+        NoticeRVA()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.vm = viewModel
@@ -26,6 +33,27 @@ class SettingNoticeFragment :
     }
 
     private fun initView() {
+        binding.adapter.adapter = noticeAdapter
+        noticeAdapter.submitList(
+            listOf(
+                Notice(
+                    "아카이브 업데이트 : 1.1.0",
+                    "2024/03/01",
+                    listOf(
+                        NoticeContent("업데이트"),
+                        NoticeContent("했습니다 : 1.1.0")
+                    )
+                ),
+                Notice(
+                    "아카이브 업데이트 : 1.1.0",
+                    "2024/03/01",
+                    listOf(
+                        NoticeContent("업데이트"),
+                        NoticeContent("했습니다 : 1.1.0")
+                    )
+                )
+            )
+        )
         binding.backBtn.setOnClickListener {
             navController.popBackStack()
         }
