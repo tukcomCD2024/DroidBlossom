f5db71057ce5601e6b59073c388578be55080921
hangunhee39 <ghee0831@naver.com>
2024-03-26 02:06:13 +0900

    feat: 문의하기 (메일로 연결)

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/mypage/setting/SettingMainFragment.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/mypage/setting/SettingMainFragment.kt
@@ -1,6 +1,8 @@
 package com.droidblossom.archive.presentation.ui.mypage.setting
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
@@ -22,11 +24,13 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+
 @AndroidEntryPoint
 class SettingMainFragment :
     BaseFragment<SettingViewModelImpl, FragmentSettingMainBinding>(R.layout.fragment_setting_main) {
 
     override val viewModel: SettingViewModelImpl by viewModels()
+
     @Inject
     lateinit var dataStoreUtils: DataStoreUtils
     lateinit var navController: NavController
@@ -39,9 +43,9 @@ class SettingMainFragment :
 
     override fun observeData() {
         viewLifecycleOwner.lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED){
-                viewModel.settingMainEvents.collect{ event->
-                    when(event){
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.settingMainEvents.collect { event ->
+                    when (event) {
                         SettingViewModel.SettingMainEvent.Back -> {
                             (activity as SettingActivity).finish()
                         }
@@ -51,16 +55,21 @@ class SettingMainFragment :
                         }
 
                         SettingViewModel.SettingMainEvent.GoInquire -> {
-
+                            goEmailIntent()
                         }
 
                         SettingViewModel.SettingMainEvent.GoLicenses -> {
-                            startActivity(Intent(requireContext(), OssLicensesMenuActivity::class.java))
+                            startActivity(
+                                Intent(
+                                    requireContext(),
+                                    OssLicensesMenuActivity::class.java
+                                )
+                            )
                             OssLicensesMenuActivity.setActivityTitle("오픈소스 라이선스")
                         }
 
                         SettingViewModel.SettingMainEvent.GoLogout -> {
-                            val sheet = CommonDialogFragment.newIntent("정말 로그아웃 하시겠습니까?","로그아웃") {
+                            val sheet = CommonDialogFragment.newIntent("정말 로그아웃 하시겠습니까?", "로그아웃") {
                                 CoroutineScope(Dispatchers.IO).launch {
                                     dataStoreUtils.resetTokenData()
                                 }
@@ -90,5 +99,28 @@ class SettingMainFragment :
                 }
             }
         }
+    }
+
+    private fun goEmailIntent() {
+        val emailSelectorIntent = Intent(Intent.ACTION_SENDTO)
+        emailSelectorIntent.data = Uri.parse("mailto:")
+
+        val address = arrayOf("ARchive@address.com")
+
+        val emailIntent = Intent(Intent.ACTION_SEND)
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, address)
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "<ARchive 문의사항 보내기>")
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "")
+
+        emailIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        emailIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+        emailIntent.selector = emailSelectorIntent
+
+        startActivity(emailIntent)
+//        if (emailIntent.resolveActivity((activity as SettingActivity).packageManager) != null) {
+//            startActivity(emailIntent)
+//        } else {
+//            showToastMessage("메일을 연결할 앱이 없습니다.")
+//        }
     }
 }
