51c8170edc4848fdfbbbd39eb833d813d64de4d4
comst19 <comst19@naver.com>
2024-03-29 03:09:55 +0900

    feat: FriendService, 친구요청 api

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/source/remote/api/FriendService.kt
@@ -0,0 +1,17 @@
+package com.droidblossom.archive.data.source.remote.api
+
+import com.droidblossom.archive.data.dto.friend.request.FriendReqRequestDto
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.Response
+import com.droidblossom.archive.data.dto.ResponseBody
+import com.droidblossom.archive.data.dto.friend.response.FriendReqStatusResponseDto
+import retrofit2.http.Query
+
+interface FriendService {
+
+    @POST("friends/{friend_id/request}")
+    suspend fun postFriendsRequestApi(
+        @Query("friend_id") friendId : Double,
+    ) : Response<ResponseBody<FriendReqStatusResponseDto>>
+}
