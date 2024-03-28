f17a16d9636a28d82f8f95ea9eeff7920761d2b2
comst19 <comst19@naver.com>
2024-03-29 04:14:41 +0900

    feat: 친구 검색 tag, phone UseCase

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/usecase/friend/FriendsSearchUseCase.kt
@@ -0,0 +1,31 @@
+package com.droidblossom.archive.domain.usecase.friend
+
+import android.util.Log
+import com.droidblossom.archive.domain.model.friend.FriendsSearchPhoneRequest
+import com.droidblossom.archive.domain.model.friend.FriendsSearchRequest
+import com.droidblossom.archive.domain.repository.FriendRepository
+import com.droidblossom.archive.util.onException
+import com.droidblossom.archive.util.onFail
+import com.droidblossom.archive.util.onSuccess
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class FriendsSearchUseCase@Inject constructor(
+    private val repository: FriendRepository
+) {
+    suspend operator fun invoke(request : FriendsSearchRequest) =
+        flow {
+            try {
+                emit(repository.postFriendsSearch().onSuccess {
+
+                }.onFail {
+
+                }.onException {
+                    throw Exception(it)
+                })
+            }catch (e : Exception){
+                Log.d("예외확인", "$e")
+                e.printStackTrace()
+            }
+        }
+}
