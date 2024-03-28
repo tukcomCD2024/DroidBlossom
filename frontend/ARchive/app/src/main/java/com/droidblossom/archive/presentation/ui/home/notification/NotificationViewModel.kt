f890b5d64cd273d729e3e336f5710d3abc264a18
hangunhee39 <ghee0831@naver.com>
2024-03-28 16:46:51 +0900

    feat: 알림페이지 뷰모델, 데이터 바인딩

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/notification/NotificationViewModel.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/home/notification/NotificationViewModel.kt
@@ -1,4 +1,19 @@
 package com.droidblossom.archive.presentation.ui.home.notification
 
+import com.droidblossom.archive.domain.model.member.NotificationModel
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+
 interface NotificationViewModel {
+    val notificationEvent: SharedFlow<NotificationEvent>
+
+    val notifications: StateFlow<List<NotificationModel>>
+    val hasNextPage: StateFlow<Boolean>
+    val lastCreatedTime: StateFlow<String>
+
+    fun getNotificationPage()
+
+    sealed class NotificationEvent {
+        data class ShowToastMessage(val message: String) : NotificationEvent()
+    }
 }
