6c8784eba73aee26bd635c951d3da8312fcd7bad
comst19 <comst19@naver.com>
2024-03-29 03:25:13 +0900

    feat: postFriendsAcceptRequest()

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/repository/FriendRepositoryImpl.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/repository/FriendRepositoryImpl.kt
@@ -2,6 +2,8 @@ package com.droidblossom.archive.data.repository
 
 import com.droidblossom.archive.data.dto.ResponseBody
 import com.droidblossom.archive.data.dto.capsule_skin.response.CapsuleSkinsPageResponseDto
+import com.droidblossom.archive.data.dto.common.toModel
+import com.droidblossom.archive.data.dto.friend.request.FriendAcceptRequestDto
 import com.droidblossom.archive.data.dto.friend.request.FriendReqRequestDto
 import com.droidblossom.archive.data.dto.friend.response.FriendReqStatusResponseDto
 import com.droidblossom.archive.data.source.remote.api.FriendService
@@ -14,7 +16,11 @@ import javax.inject.Inject
 class FriendRepositoryImpl @Inject constructor(
     private val api : FriendService
 ) : FriendRepository {
-    override suspend fun postFriendFriendsRequest(request: FriendReqRequestDto): RetrofitResult<FriendReqStatusResponse> {
+    override suspend fun postFriendsRequest(request: FriendReqRequestDto): RetrofitResult<FriendReqStatusResponse> {
         return apiHandler({ api.postFriendsRequestApi(request.friendId) }) { response: ResponseBody<FriendReqStatusResponseDto> -> response.result.toModel() }
+    }
+
+    override suspend fun postFriendsAcceptRequest(request: FriendAcceptRequestDto): RetrofitResult<String> {
+        return apiHandler({ api.postFriendsAcceptRequestApi(request.friendId) }) {response: ResponseBody<String> -> response.result.toModel()}
     }
 }
--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/repository/FriendRepository.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/repository/FriendRepository.kt
@@ -1,10 +1,13 @@
 package com.droidblossom.archive.domain.repository
 
+import com.droidblossom.archive.data.dto.friend.request.FriendAcceptRequestDto
 import com.droidblossom.archive.data.dto.friend.request.FriendReqRequestDto
 import com.droidblossom.archive.domain.model.friend.FriendReqStatusResponse
 import com.droidblossom.archive.util.RetrofitResult
 
 interface FriendRepository {
 
-    suspend fun postFriendFriendsRequest(request: FriendReqRequestDto) : RetrofitResult<FriendReqStatusResponse>
+    suspend fun postFriendsRequest(request: FriendReqRequestDto) : RetrofitResult<FriendReqStatusResponse>
+
+    suspend fun postFriendsAcceptRequest(request: FriendAcceptRequestDto) : RetrofitResult<String>
 }
