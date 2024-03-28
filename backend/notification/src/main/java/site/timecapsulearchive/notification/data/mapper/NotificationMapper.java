6666b1e06a687dfcaa5b7b2cc97939d18e65f055
GaBaljaintheroom <junsu1222@naver.com>
2024-03-28 22:15:50 +0900

    feat: firebase 알림 전송

--- backend/notification/src/main/java/site/timecapsulearchive/notification/data/mapper/NotificationMapper.java
+++ backend/notification/src/main/java/site/timecapsulearchive/notification/data/mapper/NotificationMapper.java
@@ -2,7 +2,9 @@ package site.timecapsulearchive.notification.data.mapper;
 
 import org.springframework.stereotype.Component;
 import site.timecapsulearchive.notification.data.dto.CapsuleSkinNotificationSendDto;
+import site.timecapsulearchive.notification.data.dto.FriendNotificationDto;
 import site.timecapsulearchive.notification.data.request.CapsuleSkinNotificationSendRequest;
+import site.timecapsulearchive.notification.data.request.FriendNotificationRequest;
 import site.timecapsulearchive.notification.entity.Notification;
 import site.timecapsulearchive.notification.entity.NotificationCategory;
 
@@ -10,7 +12,7 @@ import site.timecapsulearchive.notification.entity.NotificationCategory;
 public class NotificationMapper {
 
     public CapsuleSkinNotificationSendDto capsuleSkinNotificationRequestToDto(
-        CapsuleSkinNotificationSendRequest request) {
+        final CapsuleSkinNotificationSendRequest request) {
         return CapsuleSkinNotificationSendDto.builder()
             .memberId(request.memberId())
             .status(request.status())
@@ -21,15 +23,37 @@ public class NotificationMapper {
             .build();
     }
 
-    public Notification capsuleSkinNotificationSendDtoToEntity(CapsuleSkinNotificationSendDto dto,
-        NotificationCategory notificationCategory) {
+    public Notification capsuleSkinNotificationSendDtoToEntity(
+        final CapsuleSkinNotificationSendDto dto,
+        final NotificationCategory category) {
         return Notification.builder()
             .memberId(dto.memberId())
             .title(dto.title())
             .text(dto.text())
             .imageUrl(dto.skinUrl())
-            .notificationCategory(notificationCategory)
+            .notificationCategory(category)
             .status(dto.status())
+            .build();
+    }
+
+    public FriendNotificationDto friendNotificationRequestToDto(
+        final FriendNotificationRequest request) {
+        return FriendNotificationDto.builder()
+            .memberId(request.memberId())
+            .status(request.status())
+            .title(request.title())
+            .text(request.text())
+            .build();
+    }
+
+    public Notification friendNotificationDtoToEntity(final FriendNotificationDto dto,
+        final NotificationCategory category) {
+        return Notification.builder()
+            .memberId(dto.memberId())
+            .title(dto.title())
+            .text(dto.text())
+            .status(dto.status())
+            .notificationCategory(category)
             .build();
     }
 }
