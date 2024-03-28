ebac1484a6d2f3425d85e4040514a43195c59726
comst19 <comst19@naver.com>
2024-03-29 03:11:16 +0900

    feat: 친구요청 UseCase

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/usecase/friend/FriendsRequestUseCase.kt
@@ -0,0 +1,31 @@
+package com.droidblossom.archive.domain.usecase.friend
+
+import android.util.Log
+import com.droidblossom.archive.domain.model.friend.FriendReqRequest
+import com.droidblossom.archive.domain.repository.FriendRepository
+import com.droidblossom.archive.util.onException
+import com.droidblossom.archive.util.onFail
+import com.droidblossom.archive.util.onSuccess
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class FriendsRequestUseCase @Inject constructor(
+    private val repository: FriendRepository
+) {
+
+    suspend operator fun invoke(request : FriendReqRequest) =
+        flow {
+            try {
+                emit(repository.postFriendFriendsRequest(request.toDto()).onSuccess {
+
+                }.onFail {
+
+                }.onException {
+                    throw Exception(it)
+                })
+            }catch (e : Exception){
+                Log.d("예외확인", "$e")
+                e.printStackTrace()
+            }
+        }
+}
