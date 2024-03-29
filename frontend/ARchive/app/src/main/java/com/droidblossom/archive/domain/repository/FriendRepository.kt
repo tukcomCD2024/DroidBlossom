57cf4c6645ade585aeb1288aa17c4654f0747f9c
hangunhee39 <ghee0831@naver.com>
2024-03-29 20:39:59 +0900

    feat: 친구 요청 거절 usecase

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/repository/FriendRepository.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/repository/FriendRepository.kt
@@ -13,12 +13,11 @@ import com.droidblossom.archive.util.RetrofitResult
 interface FriendRepository {
 
     suspend fun postFriendsRequest(request: FriendReqRequestDto) : RetrofitResult<FriendReqStatusResponse>
-
     suspend fun postFriendsAcceptRequest(request: FriendAcceptRequestDto) : RetrofitResult<String>
-
     suspend fun postFriendsSearch(request: FriendsSearchRequestDto) : RetrofitResult<FriendsSearchResponse>
     suspend fun postFriendsSearchPhone(request : FriendsSearchPhoneRequestDto) : RetrofitResult<FriendsSearchPhoneResponse>
     suspend fun getFriendsPage(size: Int ,createdAt : String) : RetrofitResult<FriendsPage>
     suspend fun getFriendsRequestsPage(size: Int, createdAt: String) : RetrofitResult<FriendsPage>
     suspend fun deleteFriend(friendId: Long) : RetrofitResult<String>
+    suspend fun deleteFriendDeny(friendId: Long) : RetrofitResult<String>
 }
