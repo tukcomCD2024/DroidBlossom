83f7f4a3ef6260ce647f2074d4c1a051fa11f0b1
hangunhee39 <ghee0831@naver.com>
2024-03-29 19:50:28 +0900

    feat : 친구 페이징 model/dto

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/dto/friend/response/FriendResponseDto.kt
@@ -0,0 +1,17 @@
+package com.droidblossom.archive.data.dto.friend.response
+
+import com.droidblossom.archive.domain.model.friend.Friend
+
+data class FriendResponseDto(
+    val createdAt: String,
+    val id: Int,
+    val nickname: String,
+    val profileUrl: String
+) {
+     fun toModel() = Friend(
+         createdAt = this.createdAt,
+         id = this.id,
+         nickname = this.nickname,
+         profileUrl = this.profileUrl
+     )
+}
---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/model/friend/Friend.kt
@@ -0,0 +1,8 @@
+package com.droidblossom.archive.domain.model.friend
+
+data class Friend(
+    val createdAt: String,
+    val id: Int,
+    val nickname: String,
+    val profileUrl: String
+)
