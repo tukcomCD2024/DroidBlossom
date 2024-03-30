f3007c437e843e6af563b570758368431cd578c9
hong seokho <seokho7799@gmail.com>
2024-03-30 10:25:58 +0900

    fix : flyway schema validation 오류 수정

--- backend/core/src/main/resources/db/migration/V18__member_friend_update.sql
+++ backend/core/src/main/resources/db/migration/V18__member_friend_update.sql
@@ -7,9 +7,9 @@ ALTER TABLE member_friend
     ADD COLUMN owner_id BIGINT;
 ALTER TABLE member_friend
     ADD COLUMN friend_id BIGINT;
-
+    
 ALTER TABLE member_friend
     ADD CONSTRAINT fk_member_friend_owner_id FOREIGN KEY (owner_id) REFERENCES member (member_id);
 
-ALTER TABLE member_friend
+ALTER TABLE  member_friend
     ADD CONSTRAINT fk_member_friend_friend_id FOREIGN KEY (friend_id) REFERENCES member (member_id);
--- backend/core/src/main/resources/db/migration/V21__member_update.sql
+++ backend/core/src/main/resources/db/migration/V21__member_update.sql
@@ -1,5 +1,2 @@
-ALTER TABLE member
-    ADD COLUMN password VARCHAR(255);
-ALTER TABLE member
-    MODIFY email VARCHAR(255) NOT NULL,
-    ADD CONSTRAINT unique_email UNIQUE (email);
+ALTER TABLE member ADD COLUMN password VARCHAR(255);
+ALTER TABLE member MODIFY email VARCHAR(255) NOT NULL, ADD CONSTRAINT unique_email UNIQUE (email);
