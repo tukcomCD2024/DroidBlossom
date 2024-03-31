f93be4d9fee78cd388f6a41622768245e5b95173
comst19 <comst19@naver.com>
2024-03-31 17:05:47 +0900

    feat: 소셜 프래그먼트의 뷰 페이저에 사용될 프래그먼트 - friend, group

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/social/page/SocialFriendFragment.kt
@@ -0,0 +1,21 @@
+package com.droidblossom.archive.presentation.ui.social.page
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.droidblossom.archive.R
+
+
+class SocialFriendFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_social_friend, container, false)
+    }
+
+}
