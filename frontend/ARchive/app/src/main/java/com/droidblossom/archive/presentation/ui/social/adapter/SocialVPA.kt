559003e9f48eecfca6304e56135376f159728178
comst19 <comst19@naver.com>
2024-03-31 17:06:42 +0900

    feat: 소셜 프래그먼트 ViewPager Adapter

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/social/adapter/SocialVPA.kt
@@ -0,0 +1,20 @@
+package com.droidblossom.archive.presentation.ui.social.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.droidblossom.archive.presentation.ui.social.page.SocialFriendFragment
+import com.droidblossom.archive.presentation.ui.social.page.SocialGroupFragment
+
+class SocialVPA(fragment: Fragment) : FragmentStateAdapter(fragment){
+
+    private val fragmentList = listOf(
+        SocialGroupFragment(),
+        SocialFriendFragment()
+    )
+
+    override fun getItemCount(): Int = fragmentList.size
+
+    override fun createFragment(position: Int): Fragment {
+        return fragmentList[position]
+    }
+}
