7ddf3f13d38afc698490821c50c86f8c267c0d5b
GaBaljaintheroom <junsu1222@naver.com>
2024-04-01 13:24:59 +0900

    refact : Pattern 객체 불변으로 변경

--- backend/core/src/main/java/site/timecapsulearchive/core/global/common/valid/ImageExtensionValidator.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/global/common/valid/ImageExtensionValidator.java
@@ -2,19 +2,21 @@ package site.timecapsulearchive.core.global.common.valid;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import java.util.Objects;
 import java.util.regex.Pattern;
 import site.timecapsulearchive.core.global.common.valid.annotation.Image;
 
 public class ImageExtensionValidator implements ConstraintValidator<Image, String> {
 
     private static final String AVAILABLE_IMAGE_EXTENSION_REGEX = "^\\S+\\.png$";
+    private static final Pattern pattern = Pattern.compile(AVAILABLE_IMAGE_EXTENSION_REGEX);
 
     @Override
     public boolean isValid(final String value, final ConstraintValidatorContext context) {
-        if (value == null || value.isBlank()) {
+        if (Objects.isNull(value) || value.isBlank()) {
             return false;
         }
 
-        return Pattern.matches(AVAILABLE_IMAGE_EXTENSION_REGEX, value);
+        return pattern.matcher(value).matches();
     }
 }
