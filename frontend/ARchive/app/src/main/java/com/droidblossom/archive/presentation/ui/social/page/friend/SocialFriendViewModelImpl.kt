cf8cb2ad44017904ebe174c51eedc0608f6cfd73
comst19 <comst19@naver.com>
2024-04-02 20:37:30 +0900

    feat: SocialFriendFragment 리사이클러뷰 연결

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/social/page/friend/SocialFriendViewModelImpl.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/social/page/friend/SocialFriendViewModelImpl.kt
@@ -1,12 +1,37 @@
 package com.droidblossom.archive.presentation.ui.social.page.friend
 
+import androidx.lifecycle.viewModelScope
 import com.droidblossom.archive.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SocialFriendViewModelImpl @Inject constructor(
 
 ): BaseViewModel(), SocialFriendViewModel {
+
+
+    private val _isSearchOpen = MutableStateFlow(false)
+    override val isSearchOpen: StateFlow<Boolean>
+        get() = _isSearchOpen
+
+    override fun openSearchFriendCapsule() {
+        viewModelScope.launch {
+            _isSearchOpen.emit(true)
+        }
+    }
+
+    override fun closeSearchFriendCapsule() {
+        viewModelScope.launch {
+            _isSearchOpen.emit(false)
+        }
+    }
+
+    override fun searchFriendCapsule(){
+
+    }
 
 }
