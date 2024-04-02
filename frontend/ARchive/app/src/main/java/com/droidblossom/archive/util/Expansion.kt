a0bc0ab79b7fbbea20a72331700ef3a918ea5811
comst19 <comst19@naver.com>
2024-04-02 20:29:14 +0900

    feat: searchview의 ui 로직에 의한 리사이클러뷰 제약 변경코드

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/util/Expansion.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/util/Expansion.kt
@@ -2,8 +2,12 @@ package com.droidblossom.archive.util
 
 import android.app.Activity
 import android.content.Intent
+import android.content.res.Resources
 import android.os.Build
+import android.util.TypedValue
+import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import java.io.Serializable
 
 fun <T: Serializable> Intent.intentSerializable(key: String, clazz: Class<T>): T? {
@@ -17,4 +21,17 @@ fun <T: Serializable> Intent.intentSerializable(key: String, clazz: Class<T>): T
 fun Activity.getStatusBarHeight(): Int {
     val resourceId = this.resources.getIdentifier("status_bar_height", "dimen", "android")
     return if (resourceId > 0) this.resources.getDimensionPixelSize(resourceId) else 0
+}
+
+fun ConstraintLayout.LayoutParams.updateTopConstraintsForSearch(
+    isSearchOpen: Boolean,
+    searchOpenView: View,
+    searchView: View,
+    additionalMarginDp: Float,
+    resources: Resources
+) {
+    this.topToBottom = if (isSearchOpen) searchOpenView.id else searchView.id
+    val additionalMargin = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP, additionalMarginDp, resources.displayMetrics).toInt()
+    this.topMargin = additionalMargin
 }
