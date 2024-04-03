7c6bb58f0929aa38d6f90dc0fe58db51537af6ab
hangunhee39 <ghee0831@naver.com>
2024-04-03 22:40:45 +0900

    feat: 주소록 fragment

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/mypage/friend/addfriend/SearchFriendNumberFragment.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/mypage/friend/addfriend/SearchFriendNumberFragment.kt
@@ -1,18 +1,137 @@
 package com.droidblossom.archive.presentation.ui.mypage.friend.addfriend
 
+import android.annotation.SuppressLint
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.droidblossom.archive.R
+import com.droidblossom.archive.databinding.FragmentFriendSearchNicknameBinding
+import com.droidblossom.archive.databinding.FragmentFriendSearchNumberBinding
+import com.droidblossom.archive.presentation.base.BaseFragment
+import com.droidblossom.archive.presentation.ui.mypage.friend.addfriend.adapter.AddFriendRVA
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
-class SearchFriendNumberFragment : Fragment() {
+@AndroidEntryPoint
+class SearchFriendNumberFragment :
+    BaseFragment<AddFriendViewModelImpl, FragmentFriendSearchNumberBinding>(R.layout.fragment_friend_search_number) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_friend_search_number, container, false)
+    override val viewModel: AddFriendViewModelImpl by viewModels()
+
+    lateinit var navController: NavController
+
+    private val addFriendRVA by lazy {
+        AddFriendRVA{ position ->
+            viewModel.checkAddFriendList(position)
+        }
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.vm = viewModel
+
+        navController = Navigation.findNavController(view)
+        initView()
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun initView() {
+
+        val layoutParams = binding.closeBtn.layoutParams as ViewGroup.MarginLayoutParams
+        layoutParams.topMargin += getStatusBarHeight()
+        binding.closeBtn.layoutParams = layoutParams
+
+        binding.recycleView.adapter = addFriendRVA
+
+        binding.closeBtn.setOnClickListener {
+            navController.popBackStack()
+        }
+
+        binding.searchOpenEditT.setOnEditorActionListener { _, i, _ ->
+            if (i == EditorInfo.IME_ACTION_DONE) {
+                if (!binding.searchOpenEditT.text.isNullOrEmpty()) {
+                    viewModel.searchTag()
+                }
+                true
+            }
+            false
+        }
+        binding.searchOpenEditT.addTextChangedListener {
+            viewModel.resetList()
+        }
+
+        binding.searchOpenBtnT.setOnClickListener {
+            val imm = requireActivity().getSystemService(InputMethodManager::class.java)
+            imm.hideSoftInputFromWindow(requireActivity().currentFocus?.windowToken, 0)
+            viewModel.searchTag()
+        }
+
+        binding.searchOpenEditT.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                viewModel.closeSearchNum()
+            }
+        }
+        binding.root.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                val focusedView = binding.searchOpenBtn
+                val outRect = Rect()
+                focusedView.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    focusedView.clearFocus()
+                    val imm = requireActivity().getSystemService(InputMethodManager::class.java)
+                    imm.hideSoftInputFromWindow(focusedView.windowToken, 0)
+                }
+            }
+            false
+        }
+
+        binding.recycleView.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                val focusedView = binding.searchOpenBtn
+                val outRect = Rect()
+                focusedView.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    val imm = requireActivity().getSystemService(InputMethodManager::class.java)
+                    imm.hideSoftInputFromWindow(focusedView.windowToken, 0)
+                }
+            }
+            false
+        }
+    }
+
+    override fun observeData() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.addEvent.collect { event ->
+                    when (event) {
+                        is AddFriendViewModel.AddEvent.ShowToastMessage -> {
+                            showToastMessage(event.message)
+                        }
+
+                        else -> {}
+                    }
+                }
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.addFriendListUI.collect{ friends ->
+                    addFriendRVA.submitList(friends)
+                }
+            }
+        }
     }
 }
