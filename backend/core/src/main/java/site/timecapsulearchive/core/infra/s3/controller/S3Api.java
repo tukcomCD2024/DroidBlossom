86e8b15a6bfe9bb14dfda3df1b0db123d1a9fbb4
GaBaljaintheroom <junsu1222@naver.com>
2024-04-04 01:34:54 +0900

    feat: 단일 이미지 미리 서명된 Put URL 반환 기능 구현

--- backend/core/src/main/java/site/timecapsulearchive/core/infra/s3/controller/S3Api.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/infra/s3/controller/S3Api.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import site.timecapsulearchive.core.global.common.response.ApiSpec;
+import site.timecapsulearchive.core.infra.s3.data.request.ImageS3PreSignedUrlRequest;
 import site.timecapsulearchive.core.infra.s3.data.request.S3PreSignedUrlRequest;
 import site.timecapsulearchive.core.infra.s3.data.response.S3PreSignedUrlResponse;
 
@@ -26,5 +27,22 @@ public interface S3Api {
     ResponseEntity<ApiSpec<S3PreSignedUrlResponse>> getS3PreSignedUrl(
         Long memberId,
         S3PreSignedUrlRequest request
+    );
+
+    @Operation(
+        summary = "단일 이미지 s3 업로드 미리 서명된 주소 요청",
+        description = "그룹 이미지를 업로드할 수 있는 미리 서명된 주소를 응답한다.",
+        security = {@SecurityRequirement(name = "user_token")},
+        tags = {"s3"}
+    )
+    @ApiResponses(value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "s3업로드에 필요한 주소 반환"
+        )
+    })
+    ResponseEntity<ApiSpec<String>> getImageS3PreSignedUrl(
+        Long memberId,
+        ImageS3PreSignedUrlRequest request
     );
 }
