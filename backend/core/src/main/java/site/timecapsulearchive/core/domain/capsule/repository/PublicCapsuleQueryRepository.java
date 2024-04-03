b8c87f6abfe349dd4ce18ebdaa25d29a2a226dcf
GaBaljaintheroom <junsu1222@naver.com>
2024-04-03 21:52:39 +0900

    feat : 친구가 만든 공개 캡슐 목록 쿼리 작성

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/capsule/repository/PublicCapsuleQueryRepository.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/capsule/repository/PublicCapsuleQueryRepository.java
@@ -11,12 +11,18 @@ import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.StringExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.ZonedDateTime;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Repository;
 import site.timecapsulearchive.core.domain.capsule.entity.CapsuleType;
 import site.timecapsulearchive.core.domain.capsule.generic_capsule.data.dto.CapsuleDetailDto;
 import site.timecapsulearchive.core.domain.capsule.generic_capsule.data.dto.CapsuleSummaryDto;
+import site.timecapsulearchive.core.domain.member.entity.QMember;
 
 @Repository
 @RequiredArgsConstructor
@@ -94,5 +100,58 @@ public class PublicCapsuleQueryRepository {
                 .where(capsule.id.eq(capsuleId).and(capsule.type.eq(CapsuleType.PUBLIC)))
                 .fetchOne()
         );
+    }
+
+    public Slice<CapsuleDetailDto> findPublicCapsulesDtoMadeByFriend(
+        final Long memberId,
+        final int size,
+        final ZonedDateTime createdAt
+    ) {
+
+        final QMember friend = new QMember("friend");
+        final List<CapsuleDetailDto> publicCapsuleDetailDtos = jpaQueryFactory
+            .select(
+                Projections.constructor(
+                    CapsuleDetailDto.class,
+                    capsule.id,
+                    capsuleSkin.imageUrl,
+                    capsule.dueDate,
+                    friend.nickname,
+                    friend.profileUrl,
+                    capsule.createdAt,
+                    capsule.address.fullRoadAddressName,
+                    capsule.title,
+                    capsule.content,
+                    groupConcatDistinct(image.imageUrl),
+                    groupConcatDistinct(video.videoUrl),
+                    capsule.isOpened,
+                    capsule.type
+                )
+            )
+            .from(capsule)
+            .join(memberFriend).on(memberFriend.owner.id.eq(memberId)
+                .and(memberFriend.friend.id.eq(capsule.member.id)))
+            .join(capsuleSkin).on(capsule.capsuleSkin.id.eq(capsuleSkin.id))
+            .leftJoin(image).on(capsule.id.eq(image.capsule.id))
+            .leftJoin(video).on(capsule.id.eq(video.capsule.id))
+            .where(capsule.createdAt.lt(createdAt)
+                .and(capsule.type.eq(CapsuleType.PUBLIC))
+            )
+            .groupBy(capsule.id)
+            .orderBy(capsule.createdAt.desc())
+            .limit(size + 1)
+            .fetch();
+
+        final boolean hasNext = canMoreRead(size, publicCapsuleDetailDtos.size());
+        if (hasNext) {
+            publicCapsuleDetailDtos.remove(size);
+        }
+
+        return new SliceImpl<>(publicCapsuleDetailDtos, Pageable.ofSize(size), hasNext);
+
+    }
+
+    private boolean canMoreRead(final int size, final int capsuleSize) {
+        return capsuleSize > size;
     }
 }
