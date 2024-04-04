108b78b083fffba09b810307c33f2a298c9e9b04
hangunhee39 <ghee0831@naver.com>
2024-04-04 13:47:30 +0900

    feat: 공개캡슐 디테일 repo

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/repository/PublicRepositoryImpl.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/repository/PublicRepositoryImpl.kt
@@ -2,9 +2,11 @@ package com.droidblossom.archive.data.repository
 
 import com.droidblossom.archive.data.dto.ResponseBody
 import com.droidblossom.archive.data.dto.common.CapsuleCreateRequestDto
+import com.droidblossom.archive.data.dto.common.CapsuleDetailResponseDto
 import com.droidblossom.archive.data.dto.common.CapsuleSummaryResponseDto
 import com.droidblossom.archive.data.dto.common.toModel
 import com.droidblossom.archive.data.source.remote.api.PublicService
+import com.droidblossom.archive.domain.model.common.CapsuleDetail
 import com.droidblossom.archive.domain.model.common.CapsuleSummaryResponse
 import com.droidblossom.archive.domain.repository.PublicRepository
 import com.droidblossom.archive.util.RetrofitResult
@@ -20,5 +22,9 @@ class PublicRepositoryImpl @Inject constructor(
 
     override suspend fun getPublicCapsuleSummary(capsuleId: Int): RetrofitResult<CapsuleSummaryResponse> {
         return apiHandler({ api.getPublicCapsuleSummaryApi(capsuleId) }) { response: ResponseBody<CapsuleSummaryResponseDto> -> response.result.toModel()}
+    }
+
+    override suspend fun getPublicCapsuleDetail(capsuleId: Long): RetrofitResult<CapsuleDetail> {
+        return apiHandler({ api.getPublicCapsuleDetailApi(capsuleId) }) { response: ResponseBody<CapsuleDetailResponseDto> -> response.result.toModel()}
     }
 }
--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/repository/PublicRepository.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/domain/repository/PublicRepository.kt
@@ -1,6 +1,7 @@
 package com.droidblossom.archive.domain.repository
 
 import com.droidblossom.archive.data.dto.common.CapsuleCreateRequestDto
+import com.droidblossom.archive.domain.model.common.CapsuleDetail
 import com.droidblossom.archive.domain.model.common.CapsuleSummaryResponse
 import com.droidblossom.archive.util.RetrofitResult
 
@@ -9,5 +10,7 @@ interface PublicRepository {
     suspend fun createPublicCapsule (request: CapsuleCreateRequestDto) : RetrofitResult<String>
 
     suspend fun getPublicCapsuleSummary (capsuleId: Int) : RetrofitResult<CapsuleSummaryResponse>
+
+    suspend fun getPublicCapsuleDetail (capsuleId: Long) : RetrofitResult<CapsuleDetail>
 
 }
