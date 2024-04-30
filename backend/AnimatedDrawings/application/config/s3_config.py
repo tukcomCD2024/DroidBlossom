be589fb40c38c347e05f69ac287d93f759b824b8
hong seokho <seokho7799@gmail.com>
2024-04-30 20:32:56 +0900

    fix : config 구조 변경

--- backend/AnimatedDrawings/application/config/s3_config.py
+++ backend/AnimatedDrawings/application/config/s3_config.py
@@ -2,19 +2,6 @@ from application.config.root_config import RootConfig
 
 
 class S3Config(RootConfig):
-    def __init__(self):
-        self._access_key_id = self._config_file['s3']['accessKey']
-        self._secret_access_key = self._config_file['s3']['secretKey']
-        self._s3_bucket_name = self._config_file['s3']['bucket']
-
-    @property
-    def access_key(self) -> str:
-        return self._access_key_id
-
-    @property
-    def secret_key(self) -> str:
-        return self._secret_access_key
-
-    @property
-    def s3_bucket_name(self) -> str:
-        return self._s3_bucket_name
+    ACCESS_KEY_ID = RootConfig.CONFIG_FILE['s3']['accessKey']
+    SECRET_ACCESS_KEY = RootConfig.CONFIG_FILE['s3']['secretKey']
+    S3_BUCKET_NAME = RootConfig.CONFIG_FILE['s3']['bucket']
