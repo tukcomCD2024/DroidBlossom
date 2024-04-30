1641fb53a41140b9b6fde44a1437f0da52ff9e49
hong seokho <seokho7799@gmail.com>
2024-05-01 08:16:44 +0900

    fix : 입력값 처리 수정

--- backend/AnimatedDrawings/application/animation_queue.py
+++ backend/AnimatedDrawings/application/animation_queue.py
@@ -1,31 +1,26 @@
-import argparse
-import json
-import socket
 import uuid
-from json.decoder import JSONDecodeError
+from ast import literal_eval
 
 from celery import chain
 from kombu import Exchange, Queue
-
-from application.task.tasks import create_animation, save_capsule_skin, \
-    send_notification
-from kombu_connection_pool import connection, connections
 
 from application.config.queue_config import QueueConfig
 from application.logging.logger_factory import LoggerFactory
 from application.model.motion import Motion
 from application.model.retarget import Retarget
+from application.task.tasks import create_animation, save_capsule_skin, \
+    send_notification
+from kombu_connection_pool import connection, connections
 
 
 class AnimationQueueController:
-    def __init__(self, output_file_path: str):
+    def __init__(self):
         self.queue_config = QueueConfig()
         self.require_keys = ['memberId', 'memberName', 'skinName', 'imageUrl',
                              'retarget', 'motionName']
         self.celery_work_queue_name = 'makeAnimation.queue'
         self.celery_success_queue_name = 'saveCapsuleSkin.queue'
         self.celery_send_notification_queue_name = 'sendNotification.queue'
-        self.output_file_path = output_file_path
         self.logger = LoggerFactory.get_logger(__name__)
 
     def run(self):
@@ -48,8 +43,8 @@ class AnimationQueueController:
 
     def callback(
         self,
-        body: str,
-        message
+        body: [dict, str, bytes],
+        message: any
     ) -> None:
         """
         queue에서 message consume 시 동작하는 callback
@@ -57,20 +52,20 @@ class AnimationQueueController:
         """
         try:
             self.logger.debug('메시지 수신 완료, 콜백 동작')
-            json_object = self.parse_json(body)
+            parsed_data = self.parse_body(body)
 
-            filename = f"capsuleSkin/{json_object['memberId']}/{uuid.uuid4()}.gif"
+            filename = f"capsuleSkin/{parsed_data['memberId']}/{uuid.uuid4()}.gif"
 
             chain(
-                create_animation.s(input_data=json_object,
+                create_animation.s(input_data=parsed_data,
                                    filename=filename)
                 .set(queue=self.celery_work_queue_name),
 
-                save_capsule_skin.s(input_data=json_object,
+                save_capsule_skin.s(input_data=parsed_data,
                                     filename=filename)
                 .set(queue=self.celery_success_queue_name),
 
-                send_notification.s(input_data=json_object,
+                send_notification.s(input_data=parsed_data,
                                     filename=filename)
                 .set(queue=self.celery_send_notification_queue_name)
             ).apply_async(
@@ -80,47 +75,35 @@ class AnimationQueueController:
             message.ack()
             self.logger.debug('celery에 작업 전달 완료')
         except Exception as e:
-            self.logger.exception('작업 큐 메시지 처리 오류', exc_info=e)
+            self.logger.exception('작업 큐 메시지 처리 오류 %r', e)
             message.reject()
 
-    def parse_json(self, body: str):
-        """
-        json bytes를 파싱해 dict로 반환하는 함수
-        :param body: queue로부터 넘어온 json bytes
-        :return: body에서 파싱된 dict
-
-        :raises JSONDecodeError: 유효하지 않은 json 형태인 경우
-        :raise TypeError: 잘못된 json 입력 타입인 경우
-        """
+    def parse_body(self, body: [dict, str, bytes]):
         try:
-            json_object = json.loads(body)
-            json_object['memberId'] = str(json_object['memberId'])
-            json_object['retarget'] = Retarget[json_object['retarget']].value
-            json_object['motionName'] = Motion[json_object['motionName']].value
+            if isinstance(body, str):
+                dict_data = literal_eval(body)
+            elif isinstance(body, dict):
+                dict_data = body
+            elif isinstance(body, bytes):
+                dict_data = literal_eval(body.decode('utf-8'))
+            else:
+                self.logger.error('처리할 수 없는 타입 오류')
+                raise TypeError('처리할 수 없는 타입입니다')
+
+            dict_data['retarget'] = Retarget[dict_data['retarget']].value
+            dict_data['motionName'] = Motion[dict_data['motionName']].value
 
             for key in self.require_keys:
-                if key not in json_object:
+                if key not in dict_data:
                     raise KeyError
 
-            return json_object
+            return dict_data
 
-        except (JSONDecodeError, KeyError, TypeError) as e:
-            self.logger.exception('작업 큐 메시지 json 파싱 오류', exc_info=e)
+        except (KeyError, TypeError) as e:
+            self.logger.exception('작업 큐 메시지 파싱 오류 %r', e)
             raise e
 
 
-def parse_args() -> str:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-o", "--output", help="log file path")
-    args = parser.parse_args()
-
-    if args.output:
-        return args.output
-
-    return ''
-
-
 if __name__ == '__main__':
-    output_log_path = parse_args()
-    application = AnimationQueueController(output_log_path)
+    application = AnimationQueueController()
     application.run()
