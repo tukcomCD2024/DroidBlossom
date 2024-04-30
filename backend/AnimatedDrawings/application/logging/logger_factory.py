b3c1bf3550631389f317e5e4a472f36053bb784b
hong seokho <seokho7799@gmail.com>
2024-04-30 20:35:17 +0900

    fix : 로거 팩토리 스펙 수정

--- backend/AnimatedDrawings/application/logging/logger_factory.py
+++ backend/AnimatedDrawings/application/logging/logger_factory.py
@@ -1,42 +1,30 @@
 import logging
 import sys
 
+from application.config.logger_config import LoggerConfig
+
 
 class LoggerFactory:
 
     @staticmethod
-    def get_logger(
-        name: str,
-        output_file_path: str = '/var/log/application.log',
-        level: int = logging.INFO,
-        format_string: str = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    ) -> logging.Logger:
+    def get_logger(name: str) -> logging.Logger:
         """
         기본 로거를 생성해준다.
         :param name: 로거의 이름
-        :param output_file_path: 로그를 기록할 파일의 경로
-        :param level: 로깅의 레벨
-        :param format_string: 로깅의 포맷팅
         :return: 설정된 로거를 반환한다.
         """
-        if output_file_path is None or output_file_path == '':
-            output_file_path = '/var/log/application.log'
-
         logger = logging.getLogger(name)
-        logger.setLevel(level)
+        logger.setLevel(LoggerConfig.LOGGING_LEVEL)
 
-        LoggerFactory._setup_handler(format_string, level, logger,
-                                     output_file_path)
+        LoggerFactory._setup_handler(LoggerConfig.FORMAT_STRING,
+                                     LoggerConfig.LOGGING_LEVEL,
+                                     logger,
+                                     LoggerConfig.APPLICATION_OUTPUT_FILE_PATH)
 
         return logger
 
     @staticmethod
-    def setup_logger(
-        logger: logging.Logger,
-        output_file_path: str = '/var/log/celeryd.log',
-        level: int = logging.WARNING,
-        format_string: str = '%(asctime)s - %(name)s - %(levelname)s'
-    ) -> None:
+    def setup_logger(logger: logging.Logger, output_file_path: str) -> None:
         """
         파라미터로 받은 로거에 포맷터를 설정한다.
         :param logger: 설정할 로거
@@ -44,7 +32,9 @@ class LoggerFactory:
         :param level: 로깅의 레벨
         :param format_string: 로깅의 포맷팅
         """
-        LoggerFactory._setup_handler(format_string, level, logger,
+        LoggerFactory._setup_handler(LoggerConfig.FORMAT_STRING,
+                                     LoggerConfig.LOGGING_LEVEL,
+                                     logger,
                                      output_file_path)
 
     @staticmethod
