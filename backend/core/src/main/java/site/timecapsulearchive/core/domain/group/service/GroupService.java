a8a96efed16d639fa869d0297839db0670116b0d
hong seokho <seokho7799@gmail.com>
2024-05-08 20:55:28 +0900

    feat : 그룹 삭제 서비스 추가

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/group/service/GroupService.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/group/service/GroupService.java
@@ -1,19 +1,21 @@
 package site.timecapsulearchive.core.domain.group.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import java.time.ZonedDateTime;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
+import site.timecapsulearchive.core.domain.capsule.group_capsule.repository.GroupCapsuleQueryRepository;
 import site.timecapsulearchive.core.domain.group.data.dto.GroupCreateDto;
 import site.timecapsulearchive.core.domain.group.data.dto.GroupDetailDto;
 import site.timecapsulearchive.core.domain.group.data.dto.GroupSummaryDto;
 import site.timecapsulearchive.core.domain.group.entity.Group;
 import site.timecapsulearchive.core.domain.group.entity.MemberGroup;
+import site.timecapsulearchive.core.domain.group.exception.GroupDeleteFailException;
 import site.timecapsulearchive.core.domain.group.exception.GroupNotFoundException;
 import site.timecapsulearchive.core.domain.group.repository.GroupQueryRepository;
 import site.timecapsulearchive.core.domain.group.repository.GroupRepository;
@@ -21,6 +23,7 @@ import site.timecapsulearchive.core.domain.group.repository.MemberGroupRepositor
 import site.timecapsulearchive.core.domain.member.entity.Member;
 import site.timecapsulearchive.core.domain.member.exception.MemberNotFoundException;
 import site.timecapsulearchive.core.domain.member.repository.MemberRepository;
+import site.timecapsulearchive.core.global.error.ErrorCode;
 import site.timecapsulearchive.core.infra.queue.manager.SocialNotificationManager;
 
 @Service
@@ -33,6 +36,7 @@ public class GroupService {
     private final TransactionTemplate transactionTemplate;
     private final SocialNotificationManager socialNotificationManager;
     private final GroupQueryRepository groupQueryRepository;
+    private final GroupCapsuleQueryRepository groupCapsuleQueryRepository;
 
     public void createGroup(final Long memberId, final GroupCreateDto dto) {
         final Member member = memberRepository.findMemberById(memberId)
@@ -83,5 +87,42 @@ public class GroupService {
         }
 
         return groupDetailDto;
+    }
+
+    /**
+     * 사용자가 그룹장인 그룹을 삭제한다.
+     * <br><u><b>주의</b></u> - 그룹 삭제 시 아래 조건에 해당하면 예외가 발생한다.
+     * <br>1. 그룹에 멤버가 존재하는 경우
+     * <br>2. 그룹 삭제를 요청한 사용자가 그룹장이 아닌 경우
+     * <br>3. 그룹에 그룹 캡슐이 남아있는 경우
+     * @param memberId 그룹에 속한 그룹장 아이디
+     * @param groupId 그룹 아이디
+     */
+    @Transactional
+    public void deleteGroup(final Long memberId, final Long groupId) {
+        Group group = groupRepository.findGroupById(groupId)
+            .orElseThrow(GroupNotFoundException::new);
+
+        List<MemberGroup> groupMembers = memberGroupRepository.findMemberGroupsByGroupId(groupId);
+        boolean groupMemberExist = groupMembers.size() > 1;
+        if (groupMemberExist) {
+            throw new GroupDeleteFailException(ErrorCode.GROUP_MEMBER_EXIST_ERROR);
+        }
+        groupMembers.forEach(memberGroupRepository::delete);
+
+        boolean isGroupOwner = groupMembers.stream()
+            .anyMatch(mg -> mg.getMember().getId().equals(memberId) && mg.getIsOwner());
+        if (!isGroupOwner) {
+            throw new GroupDeleteFailException(ErrorCode.NO_GROUP_AUTHORITY_ERROR);
+        }
+
+        Long groupCapsuleCount = groupCapsuleQueryRepository.findGroupCapsuleCountByGroupId(groupId)
+            .orElseThrow(GroupNotFoundException::new);
+        boolean groupCapsuleExist = groupCapsuleCount != 0;
+        if (groupCapsuleExist) {
+            throw new GroupDeleteFailException(ErrorCode.GROUP_CAPSULE_EXIST_ERROR);
+        }
+
+        groupRepository.delete(group);
     }
 }
