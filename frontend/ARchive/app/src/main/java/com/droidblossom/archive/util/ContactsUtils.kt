1d5e8467bd0108a1fe31905aa7a7edd79ec13432
hangunhee39 <ghee0831@naver.com>
2024-05-11 02:41:18 +0900

    feat: 내 전화번호 친추 못보내게

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/util/ContactsUtils.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/util/ContactsUtils.kt
@@ -1,17 +1,22 @@
 package com.droidblossom.archive.util
 
+import android.annotation.SuppressLint
 import android.content.ContentResolver
 import android.content.Context
 import android.provider.ContactsContract
+import android.telephony.TelephonyManager
 import android.util.Log
 import com.droidblossom.archive.data.dto.friend.request.PhoneBooks
 
 
 object ContactsUtils {
 
+    @SuppressLint("MissingPermission")
     fun getContacts(context : Context) : List<PhoneBooks> {
         val resolver: ContentResolver = context.contentResolver
         val phoneUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+
+        val tm = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
         val projection = arrayOf(
             ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
             ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
@@ -29,6 +34,9 @@ object ContactsUtils {
                 numberList.add(PhoneBooks(number,name))
                 Log.d("GetContact", "이름 : $name 번호 : $number")
             }
+        }
+        numberList.removeIf {
+            it.originPhone == tm.line1Number.replace("+82","0")
         }
         cursor!!.close()
         return numberList.toList()
