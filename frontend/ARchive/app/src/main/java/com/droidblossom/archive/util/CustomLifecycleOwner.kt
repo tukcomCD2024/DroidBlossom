ca01c32f35b15261f9f0d55a44dac28f1c6438f8
hangunhee39 <ghee0831@naver.com>
2024-05-13 15:22:03 +0900

    design : 캡슐 미리보기 스킨 비율

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/util/CustomLifecycleOwner.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/util/CustomLifecycleOwner.kt
@@ -6,12 +6,12 @@ import androidx.lifecycle.LifecycleRegistry
 
 class CustomLifecycleOwner : LifecycleOwner {
 
-    private val customLifecycle = LifecycleRegistry(this)
+    private val _customLifecycle = LifecycleRegistry(this)
 
     fun handleLifecycleEvent(event: Lifecycle.Event) {
-        customLifecycle.handleLifecycleEvent(event)
+        _customLifecycle.handleLifecycleEvent(event)
     }
 
     override val lifecycle: Lifecycle
-        get() = customLifecycle
+        get() = _customLifecycle
 }
