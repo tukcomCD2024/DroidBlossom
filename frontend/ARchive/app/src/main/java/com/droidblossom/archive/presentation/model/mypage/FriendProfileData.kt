6a21fa78e7f885043309abdf2e29159a2c8e253b
comst19 <comst19@naver.com>
2024-05-20 11:47:28 +0900

    design: 친구 상세 UI

---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/model/mypage/FriendProfileData.kt
@@ -0,0 +1,8 @@
+package com.droidblossom.archive.presentation.model.mypage
+
+data class FriendProfileData (
+    val profileId:Long,
+    val nickname : String,
+    val profileUrl : String,
+    val tag : String,
+)
---
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/presentation/ui/mypage/friend/detail/FriendDetailActivity.kt
@@ -0,0 +1,114 @@
+package com.droidblossom.archive.presentation.ui.mypage.friend.detail
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.ViewGroup
+import androidx.activity.viewModels
+import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.droidblossom.archive.R
+import com.droidblossom.archive.databinding.ActivityFriendDetailBinding
+import com.droidblossom.archive.presentation.base.BaseActivity
+import com.droidblossom.archive.presentation.ui.capsule.CapsuleDetailActivity
+import com.droidblossom.archive.presentation.ui.home.dialog.CapsulePreviewDialogFragment
+import com.droidblossom.archive.presentation.ui.mypage.adapter.CapsuleRVA
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class FriendDetailActivity :
+    BaseActivity<FriendDetailViewModelImpl, ActivityFriendDetailBinding>(R.layout.activity_friend_detail){
+
+    override val viewModel: FriendDetailViewModelImpl by viewModels<FriendDetailViewModelImpl>()
+
+    private val capsuleRVA: CapsuleRVA by lazy {
+        CapsuleRVA(
+            { id, type ->
+
+            },
+            { capsuleIndex, id, type ->
+
+
+            }
+        )
+    }
+
+
+    override fun observeData() {
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.friendDetailEvents.collect { event ->
+                    when(event){
+                        is FriendDetailViewModel.FriendDetailEvent.ShowToastMessage -> {
+
+                        }
+                    }
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.capsules.collect { capsules->
+                    capsuleRVA.submitList(capsules)
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.vm = viewModel
+
+
+        initView()
+        initRV()
+
+        val layoutParams = binding.appBarLayout.layoutParams as ViewGroup.MarginLayoutParams
+        layoutParams.topMargin += getStatusBarHeight()
+        binding.appBarLayout.layoutParams = layoutParams
+
+    }
+
+
+    private fun initView(){
+
+    }
+
+    private fun initRV(){
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            viewModel.getLatestCapsuleList()
+        }
+        binding.rv.adapter = capsuleRVA
+        binding.rv.setHasFixedSize(true)
+        binding.rv.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+
+                if (newState == RecyclerView.SCROLL_STATE_IDLE || newState == RecyclerView.SCROLL_STATE_DRAGGING) {
+                    val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+                    val totalItemCount = layoutManager.itemCount
+                    val lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition()
+
+                    if (totalItemCount - lastVisibleItemPosition <= 5) {
+                        viewModel.onScrollNearBottom()
+                    }
+                }
+            }
+        })
+    }
+
+    companion object {
+        private const val FRIEND_ID = "friend_id"
+        fun newIntent(context: Context, friendId: Long) =
+            Intent(context, FriendDetailActivity::class.java).apply {
+                putExtra(FRIEND_ID, friendId)
+            }
+    }
+}
