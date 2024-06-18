57bb6a1fdfd11295df132dfd60a2b28e26ce036f
comst19 <comst19@naver.com>
2024-06-18 12:52:26 +0900

    refact: 그룹 캡슐 멤버별 상태 확인 api 수정사항 반영

--- frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/dto/group_capsule/GroupMembersCapsuleOpenStatusResponseDto.kt
+++ frontend/ARchive/app/src/main/java/com/droidblossom/archive/data/dto/group_capsule/GroupMembersCapsuleOpenStatusResponseDto.kt
@@ -3,9 +3,9 @@ package com.droidblossom.archive.data.dto.group_capsule
 import com.droidblossom.archive.domain.model.group_capsule.GroupMembersCapsuleOpenStatusResponse
 
 data class GroupMembersCapsuleOpenStatusResponseDto(
-    val groupMemberCapsuleOpenStatus: List<GroupCapsuleMemberDto>
+    val groupMembers: List<GroupCapsuleMemberDto>
 ) {
     fun toModel() = GroupMembersCapsuleOpenStatusResponse(
-        groupMemberCapsuleOpenStatus = this.groupMemberCapsuleOpenStatus.map { it.toModel() }
+        groupMemberCapsuleOpenStatus = this.groupMembers.map { it.toModel() }
     )
 }
