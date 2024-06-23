822edd5a339a9a5b201c9c4c1f9f189fb9f2e303
GaBaljaintheroom <junsu1222@naver.com>
2024-06-24 00:07:03 +0900

    fix : request Valid 추가

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/member/data/reqeust/UpdateMemberDataRequest.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/member/data/reqeust/UpdateMemberDataRequest.java
@@ -1,15 +1,18 @@
 package site.timecapsulearchive.core.domain.member.data.reqeust;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import site.timecapsulearchive.core.domain.member.data.dto.UpdateMemberDataDto;
 
 @Schema(description = "회원 업데이트 포맷")
 public record UpdateMemberDataRequest(
 
     @Schema(description = "닉네임")
+    @NotNull(message = "사용자 닉네임은 필수 입니다.")
     String nickname,
 
     @Schema(description = "태그")
+    @NotNull(message = "사용자 태그는 필수 입니다.")
     String tag
 ) {
 
