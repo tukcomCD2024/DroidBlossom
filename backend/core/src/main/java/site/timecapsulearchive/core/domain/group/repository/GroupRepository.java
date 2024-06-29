17d96960e38513cbe732e35b638ad9bfbacf5974
hong seokho <seokho7799@gmail.com>
2024-06-29 17:23:03 +0900

    feat : soft delete 쿼리 추가

--- backend/core/src/main/java/site/timecapsulearchive/core/domain/group/repository/GroupRepository.java
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/group/repository/GroupRepository.java
@@ -1,6 +1,8 @@
 package site.timecapsulearchive.core.domain.group.repository;
 
+import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import site.timecapsulearchive.core.domain.group.entity.Group;
 
@@ -11,4 +13,7 @@ public interface GroupRepository extends Repository<Group, Long>, GroupQueryRepo
     Optional<Group> findGroupById(Long groupId);
 
     void delete(Group group);
+
+    @Query("select g from Group g join MemberGroup mg where mg.member.id = :memberId and mg.isOwner = true")
+    List<Group> findAllOwnerGroupsByMemberId(Long memberId);
 }
---
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/member/repository/NotificationRepository.java
@@ -0,0 +1,18 @@
+package site.timecapsulearchive.core.domain.member.repository;
+
+import java.time.ZonedDateTime;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+import site.timecapsulearchive.core.domain.member.entity.Notification;
+
+public interface NotificationRepository extends Repository<Notification, Long> {
+
+    @Query("UPDATE Notification n SET n.deletedAt = :deletedAt WHERE n.member.id = :memberId")
+    @Modifying
+    void deleteByMemberId(
+        @Param("memberId") Long memberId,
+        @Param("deletedAt") ZonedDateTime deletedAt
+    );
+}
