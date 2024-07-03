6fb1d7025e7cdfdbe7b12ef3db2158769d0a400a
hong seokho <seokho7799@gmail.com>
2024-07-03 11:14:24 +0900

    refact : 캡슐 스킨 레포지토리 개선
    - 하나의 레포지토리로도 사용가능하게 변경

---
+++ backend/core/src/main/java/site/timecapsulearchive/core/domain/capsuleskin/repository/CapsuleSkinQueryRepositoryImpl.java
@@ -0,0 +1,46 @@
+package site.timecapsulearchive.core.domain.capsuleskin.repository;
+
+import static site.timecapsulearchive.core.domain.capsuleskin.entity.QCapsuleSkin.capsuleSkin;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.ZonedDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.stereotype.Repository;
+import site.timecapsulearchive.core.domain.capsuleskin.data.dto.CapsuleSkinSummaryDto;
+import site.timecapsulearchive.core.global.util.SliceUtil;
+
+@Repository
+@RequiredArgsConstructor
+public class CapsuleSkinQueryRepositoryImpl implements CapsuleSkinQueryRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    public Slice<CapsuleSkinSummaryDto> findCapsuleSkinSliceByCreatedAtAndMemberId(
+        final Long memberId,
+        final int size,
+        final ZonedDateTime createdAt
+    ) {
+        final List<CapsuleSkinSummaryDto> capsuleSkins = jpaQueryFactory
+            .select(
+                Projections.constructor(
+                    CapsuleSkinSummaryDto.class,
+                    capsuleSkin.id,
+                    capsuleSkin.imageUrl,
+                    capsuleSkin.skinName,
+                    capsuleSkin.createdAt
+                )
+            )
+            .from(capsuleSkin)
+            .where(capsuleSkin.member.id.eq(memberId).and(capsuleSkin.createdAt.lt(createdAt)))
+            .orderBy(capsuleSkin.id.desc())
+            .limit(size + 1)
+            .fetch();
+
+        return SliceUtil.makeSlice(size, capsuleSkins);
+    }
+}
