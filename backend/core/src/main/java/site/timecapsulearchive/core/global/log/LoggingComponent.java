dbad7966860785a41cd3208f8abd5a5ff17e604c
GaBaljaintheroom <junsu1222@naver.com>
2024-08-11 15:35:25 +0900

    feat : AOP를 이용한 계층 로깅

---
+++ backend/core/src/main/java/site/timecapsulearchive/core/global/log/LoggingComponent.java
@@ -0,0 +1,30 @@
+package site.timecapsulearchive.core.global.log;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingComponent {
+
+    @Before("execution(* site.timecapsulearchive..*(..)) "
+        + "&& !within(site.timecapsulearchive.core.domain..api..*)"
+        + "&& !within(site.timecapsulearchive.core.global..*)")
+    public void doTraceBefore(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+
+        log.info("[before] {} args={}", joinPoint.getSignature(), args);
+    }
+
+    @After("execution(* site.timecapsulearchive..*(..)) "
+        + "&& !within(site.timecapsulearchive.core.domain..api..*)"
+        + "&& !within(site.timecapsulearchive.core.global..*)")
+    public void doTraceAfter(JoinPoint joinPoint) {
+        log.info("[after] {}", joinPoint.getSignature());
+    }
+}
